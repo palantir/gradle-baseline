59b8be678590c13f697cdad826cfdfec4f478f2f
Robert Fink <rfink@palantir.com>
2017-03-11 20:42:59 +0000

    Add baseline-errorprone project with slf4j check
    Thanks to @markelliot for providing the implementation

---
+++ baseline-errorprone/src/main/java/com/palantir/errorprone/slf4j/Slf4jConstantLogMessage.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ */
+
+package com.palantir.errorprone.slf4j;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.Category;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.matchers.CompileTimeConstantExpressionMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import java.util.List;
+import java.util.regex.Pattern;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "Slf4jConstantLogMessage",
+        category = Category.ONE_OFF,
+        severity = SeverityLevel.ERROR,
+        summary = "Allow only compile-time constant slf4j log message strings.")
+public final class Slf4jConstantLogMessage extends BugChecker implements MethodInvocationTreeMatcher {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Matcher<ExpressionTree> LOG_METHOD =
+            Matchers.anyOf(
+                    MethodMatchers.instanceMethod()
+                            .onDescendantOf("org.slf4j.Logger")
+                            .withNameMatching(Pattern.compile("trace|debug|info|warn|error")));
+
+    private final Matcher<ExpressionTree> compileTimeConstExpressionMatcher =
+            new CompileTimeConstantExpressionMatcher();
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        if (!LOG_METHOD.matches(tree, state)) {
+            return Description.NO_MATCH;
+        }
+
+        List<? extends ExpressionTree> args = tree.getArguments();
+        ExpressionTree messageArg = ASTHelpers.isCastable(
+                ASTHelpers.getType(tree.getArguments().get(0)),
+                state.getTypeFromString("org.slf4j.Marker"),
+                state)
+                ? args.get(1)
+                : args.get(0);
+
+        if (compileTimeConstExpressionMatcher.matches(messageArg, state)) {
+            return Description.NO_MATCH;
+        }
+
+        return buildDescription(tree).setMessage("slf4j log statement uses a non-constant expression").build();
+    }
+
+}
---
+++ baseline-errorprone/src/test/java/com/palantir/errorprone/slf4j/Slf4jConstantLogMessageTests.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ */
+
+package com.palantir.errorprone.slf4j;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class Slf4jConstantLogMessageTests {
+
+    private CompilationTestHelper compilationHelper;
+
+    @Before
+    public void before() {
+        compilationHelper = CompilationTestHelper.newInstance(Slf4jConstantLogMessage.class, getClass());
+    }
+
+    private void test(String log) throws Exception {
+        compilationHelper
+                .addSourceLines(
+                        "Test.java",
+                        "import org.slf4j.Logger;",
+                        "import org.slf4j.LoggerFactory;",
+                        "import org.slf4j.Marker;",
+                        "import java.util.Iterator;",
+                        "class Test {",
+                        "  private static final Logger log = LoggerFactory.getLogger(Test.class);",
+                        "  void f(String param) {",
+                        "    // BUG: Diagnostic contains: non-constant expression",
+                        "    " + log,
+                        "  }",
+                        "",
+                        "  class DummyMarker implements Marker {",
+                        "    public String getName() { return null; }",
+                        "    public void add(Marker reference) {}",
+                        "    public boolean remove(Marker reference) { return true; }",
+                        "    public boolean hasChildren() { return false; }",
+                        "    public boolean hasReferences() { return false; }",
+                        "    public Iterator<Marker> iterator() { return null; }",
+                        "    public boolean contains(Marker other) { return false; }",
+                        "    public boolean contains(String name) { return false; }",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void testNonCompileTimeConstantExpression() throws Exception {
+        test("log.trace(\"constant\" + param);");
+        test("log.debug(\"constant\" + param);");
+        test("log.info(\"constant\" + param);");
+        test("log.warn(\"constant\" + param);");
+        test("log.error(\"constant\" + param);");
+        test("log.trace(new DummyMarker(), \"constant\" + param);");
+        test("log.debug(new DummyMarker(), \"constant\" + param);");
+        test("log.info(new DummyMarker(), \"constant\" + param);");
+        test("log.warn(new DummyMarker(), \"constant\" + param);");
+        test("log.error(new DummyMarker(), \"constant\" + param);");
+    }
+
+    @Test
+    public void negative() throws Exception {
+        compilationHelper
+                .addSourceLines(
+                        "Test.java",
+                        "import org.slf4j.Logger;",
+                        "import org.slf4j.LoggerFactory;",
+                        "import org.slf4j.Marker;",
+                        "import java.util.Iterator;",
+                        "class Test {",
+                        "  private static final Logger log = LoggerFactory.getLogger(Test.class);",
+                        "  private static final String compileTimeConstant = \"constant\";",
+                        "  void f() {",
+                        "    // log.<>(String)",
+                        "    log.trace(\"constant\");",
+                        "    log.debug(\"constant\");",
+                        "    log.info(\"constant\");",
+                        "    log.warn(\"constant\");",
+                        "    log.error(\"constant\");",
+                        "",
+                        "    // log.<>(String, Object)",
+                        "    log.trace(\"constant {}\", \"extra\");",
+                        "    log.debug(\"constant {}\", \"extra\");",
+                        "    log.info(\"constant {}\", \"extra\");",
+                        "    log.warn(\"constant {}\", \"extra\");",
+                        "    log.error(\"constant {}\", \"extra\");",
+                        "",
+                        "    // log.<>(String, Object, Object)",
+                        "    log.trace(\"constant {} {}\", \"extra\", \"extra2\");",
+                        "    log.debug(\"constant {} {}\", \"extra\", \"extra2\");",
+                        "    log.info(\"constant {} {}\", \"extra\", \"extra2\");",
+                        "    log.warn(\"constant {} {}\", \"extra\", \"extra2\");",
+                        "    log.error(\"constant {} {}\", \"extra\", \"extra2\");",
+                        "",
+                        "    // log.<>(String, Object...)",
+                        "    log.trace(\"constant {} {} {}\", \"extra\", \"extra2\", \"extra3\");",
+                        "    log.debug(\"constant {} {} {}\", \"extra\", \"extra2\", \"extra3\");",
+                        "    log.info(\"constant {} {} {}\", \"extra\", \"extra2\", \"extra3\");",
+                        "    log.warn(\"constant {} {} {}\", \"extra\", \"extra2\", \"extra3\");",
+                        "    log.error(\"constant {} {} {}\", \"extra\", \"extra2\", \"extra3\");",
+                        "",
+                        "    log.trace(\"constant\" + compileTimeConstant);",
+                        "    log.debug(\"constant\" + compileTimeConstant);",
+                        "    log.info(\"constant\" + compileTimeConstant);",
+                        "    log.warn(\"constant\" + compileTimeConstant);",
+                        "    log.error(\"constant\" + compileTimeConstant);",
+                        "",
+                        "    log.trace(\"constant {}\" + compileTimeConstant, \"extra\");",
+                        "    log.debug(\"constant {}\" + compileTimeConstant, \"extra\");",
+                        "    log.info(\"constant {}\" + compileTimeConstant, \"extra\");",
+                        "    log.warn(\"constant {}\" + compileTimeConstant, \"extra\");",
+                        "    log.error(\"constant {}\" + compileTimeConstant, \"extra\");",
+                        "",
+                        "    // log.<>(Marker, String)",
+                        "    log.trace(new DummyMarker(), \"constant\");",
+                        "    log.debug(new DummyMarker(), \"constant\");",
+                        "    log.info(new DummyMarker(), \"constant\");",
+                        "    log.warn(new DummyMarker(), \"constant\");",
+                        "    log.error(new DummyMarker(), \"constant\");",
+                        "  }",
+                        "",
+                        "  class DummyMarker implements Marker {",
+                        "    public String getName() { return null; }",
+                        "    public void add(Marker reference) {}",
+                        "    public boolean remove(Marker reference) { return true; }",
+                        "    public boolean hasChildren() { return false; }",
+                        "    public boolean hasReferences() { return false; }",
+                        "    public Iterator<Marker> iterator() { return null; }",
+                        "    public boolean contains(Marker other) { return false; }",
+                        "    public boolean contains(String name) { return false; }",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+}
