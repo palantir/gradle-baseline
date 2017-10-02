f7d2076364524fd46418d8d6a9b4fad3c2a31210
Callum Rogers <CRogers@users.noreply.github.com>
2017-10-02 11:39:11 +0100

    Configure the intellij checkstyle plugin to use the correct checkstyle version (#212)

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineIdea.groovy
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineIdea.groovy
@@ -113,6 +113,7 @@ class BaselineIdea extends AbstractBaselinePlugin {
               <option name="configuration">
                 <map>
                   <entry key="active-configuration" value="${checkstyleFile}:Baseline Checkstyle" />
+                  <entry key="checkstyle-version" value="${BaselineCheckstyle.DEFAULT_CHECKSTYLE_VERSION}" />
                   <entry key="check-nonjava-files" value="false" />
                   <entry key="check-test-classes" value="true" />
                   <entry key="location-0" value="${checkstyleFile}:Baseline Checkstyle" />
