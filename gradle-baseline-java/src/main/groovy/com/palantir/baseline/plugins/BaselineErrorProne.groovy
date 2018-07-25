b22a41d6432e8def2425f1f6e36c905fc86a8b33
Robert Kruszewski <robertk@palantir.com>
2018-07-25 16:10:36 +0100

    afterevaluate

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineErrorProne.groovy
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineErrorProne.groovy
@@ -17,6 +17,7 @@
 package com.palantir.baseline.plugins
 
 import net.ltgt.gradle.errorprone.ErrorPronePlugin
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
@@ -32,18 +33,19 @@ class BaselineErrorProne extends AbstractBaselinePlugin {
             errorprone "com.palantir.baseline:baseline-error-prone:${extractVersionString()}"
         }
 
-        project.tasks.withType(JavaCompile) {
-            options.compilerArgumentProviders +=
-                    {["-Xbootclasspath/p:${project.getConfigurations().getByName("errorprone").getAsPath()}"]}
-            options.compilerArgs += [
-                    "-XepDisableWarningsInGeneratedCode",
-                    "-Xep:EqualsHashCode:ERROR",
-                    "-Xep:EqualsIncompatibleType:ERROR",
-            ]
-        }
+        project.afterEvaluate { p ->
+            p.tasks.withType(JavaCompile) {
+                options.compilerArgs += [
+                        "-Xbootclasspath/p:${project.getConfigurations().getByName("errorprone").getAsPath()}",
+                        "-XepDisableWarningsInGeneratedCode",
+                        "-Xep:EqualsHashCode:ERROR",
+                        "-Xep:EqualsIncompatibleType:ERROR",
+                ]
+            }
 
-        project.tasks.withType(Test) {
-            jvmArgumentProviders {["-Xbootclasspath/p:${project.getConfigurations().getByName("errorprone").getAsPath()}"]}
+            p.tasks.withType(Test) {
+                jvmArgs "-Xbootclasspath/p:${project.getConfigurations().getByName("errorprone").getAsPath()}"
+            }
         }
     }
 
