e15e293db9b2304435065798e612c9f62432a726
Dan Fox <dfox@palantir.com>
2018-08-02 02:24:36 +0100

    apply nebula.dependency-recommender and set up extension

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineVersionsProps.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineVersionsProps.java
@@ -16,9 +16,13 @@
 
 package com.palantir.baseline.plugins;
 
+import com.google.common.collect.ImmutableMap;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import netflix.nebula.dependency.recommender.DependencyRecommendationsPlugin;
+import netflix.nebula.dependency.recommender.RecommendationStrategies;
+import netflix.nebula.dependency.recommender.provider.RecommendationProviderContainer;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -28,11 +32,29 @@ public class BaselineVersionsProps implements Plugin<Project> {
     public void apply(Project project) {
         warnIfAppliedToSubproject(project);
 
-        File versionsPropsFile = createVersionsPropsFileIfNecessary(project);
+        File versionsPropsFile = rootVersionsPropsFile(project);
+
+        project.getRootProject().allprojects(proj -> {
+            // apply plugin: "nebula.dependency-recommender"
+            proj.getPluginManager().apply(DependencyRecommendationsPlugin.class);
+
+            // get dependencyRecommendations extension
+            RecommendationProviderContainer extension = proj.getExtensions().getByType(
+                    RecommendationProviderContainer.class);
+
+            extension.setStrategy(RecommendationStrategies.OverrideTransitives); // default is 'ConflictResolved'
+            
+            extension.propertiesFile(ImmutableMap.of("file", versionsPropsFile));
+
+            // allow nested projects to specify their own nested versions.props file
+            if (proj != proj.getRootProject() && proj.file("versions.props").exists()) {
+                extension.propertiesFile(ImmutableMap.of("file", proj.file("versions.props")));
+            }
+        });
     }
 
-    private static File createVersionsPropsFileIfNecessary(Project project) {
-        File file = project.file("versions.props");
+    private static File rootVersionsPropsFile(Project project) {
+        File file = project.getRootProject().file("versions.props");
         if (!file.canRead()) {
             try {
                 Files.createFile(file.toPath());
@@ -50,5 +72,4 @@ public class BaselineVersionsProps implements Plugin<Project> {
                     project.getName());
         }
     }
-
 }
