ac34b4057aea3fa3a750c971b488e36904d61f5c
Dan Fox <dfox@palantir.com>
2018-08-20 20:35:52 +0100

    More defensive BaselineFormat plugin

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
@@ -17,6 +17,7 @@
 package com.palantir.baseline.plugins;
 
 import com.diffplug.gradle.spotless.SpotlessExtension;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.gradle.api.Project;
 
@@ -32,9 +33,16 @@ class BaselineFormat extends AbstractBaselinePlugin {
 
         project.getPluginManager().apply("com.diffplug.gradle.spotless");
 
+        Path configFile = Paths.get(getConfigDir(), "spotless", "spotless.eclipse.xml");
+        if (!configFile.toFile().exists()) {
+            project.getLogger().error("Config file must exist, try ./gradlew baselineUpdateConfig: {}", configFile);
+            return;
+        }
+
         project.getExtensions().getByType(SpotlessExtension.class).java(java -> {
+            // TODO(dfox): apply this to all source sets, not just 'main' and 'test'
             java.target("src/main/java/**/*.java", "src/main/test/**/*.java");
-            java.eclipse().configFile(Paths.get(getConfigDir(), "spotless", "spotless.eclipse.xml"));
+            java.eclipse().configFile(configFile);
             java.removeUnusedImports();
             java.trimTrailingWhitespace();
             java.indentWithSpaces(4);
