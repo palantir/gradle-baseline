5359c17eea2293a6486f1e27787b8e6f7273a340
Dan Fox <dfox@palantir.com>
2018-08-20 23:30:26 +0100

    Test to prove default dependency works

--- gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineConfigIntegrationTest.groovy
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineConfigIntegrationTest.groovy
@@ -48,13 +48,20 @@ class BaselineConfigIntegrationTest extends AbstractPluginTest {
         directory('.baseline').list().toList().toSet() == ['checkstyle', 'copyright', 'eclipse', 'idea'].toSet()
     }
 
-    def 'Fails if no configuration dependency is specified'() {
+    def './gradlew baselineUpdateConfig should still work even if no configuration dependency is specified'() {
         when:
         buildFile << standardBuildFile
 
+        // forcing is necessary here because Implementation-Version is only available after publish, not during tests
+        buildFile << """
+        configurations.baseline {
+            resolutionStrategy { force 'com.palantir.baseline:gradle-baseline-java-config:${projectVersion}' }
+        }
+        """
+
         then:
-        with('baselineUpdateConfig').buildAndFail().output.contains(
-                "Could not find com.palantir.baseline:gradle-baseline-java-config:.")
+        with('baselineUpdateConfig').build()
+        !directory('.baseline').list().toList().isEmpty()
     }
 
     def 'Fails if too many configuration dependencies are specified'() {
