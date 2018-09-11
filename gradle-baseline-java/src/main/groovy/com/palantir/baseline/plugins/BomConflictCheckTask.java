596f7257534dfbfe3d133d21b43b74b744f656db
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2018-09-11 19:12:18 +0100

    whitespace

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BomConflictCheckTask.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BomConflictCheckTask.java
@@ -63,7 +63,7 @@ public class BomConflictCheckTask extends DefaultTask {
     @TaskAction
     public final void checkBomConflict() {
         List<Conflict> conflicts = Lists.newArrayList();
-        Set<String> artifacts =  getResolvedArtifacts();
+        Set<String> artifacts = getResolvedArtifacts();
         Map<String, String> recommendations = getMavenBomRecommendations();
         Set<String> bomDeps = getProject().getConfigurations()
                 .getByName(DependencyRecommendationsPlugin.NEBULA_RECOMMENDER_BOM)
