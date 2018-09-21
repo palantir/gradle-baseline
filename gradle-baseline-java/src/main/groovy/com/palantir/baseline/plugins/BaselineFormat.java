5b6731e080ed65411765fa2c2ec9d7a23b4a3cf0
Qinfeng Chen <qchen@palantir.com>
2018-09-21 16:02:43 +0100

    remove eclipse settings and keep non contentious settings

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
@@ -36,17 +36,12 @@ class BaselineFormat extends AbstractBaselinePlugin {
 
         project.getPluginManager().apply("com.diffplug.gradle.spotless");
 
-        Path configFile = Paths.get(getConfigDir(), "spotless", "spotless.eclipse.xml");
-        if (!configFile.toFile().exists()) {
-            project.getLogger().error("Config file must exist, try ./gradlew baselineUpdateConfig: {}", configFile);
-            return;
-        }
-
         project.getExtensions().getByType(SpotlessExtension.class).java(java -> {
             // TODO(dfox): apply this to all source sets, not just 'main' and 'test'
             java.target("src/main/java/**/*.java", "src/main/test/**/*.java");
-            java.eclipse().configFile(configFile);
             java.removeUnusedImports();
+            // use empty string to specify one group for all non-static imports
+            java.importOrder("");
             java.trimTrailingWhitespace();
             java.indentWithSpaces(4);
             java.endWithNewline();
