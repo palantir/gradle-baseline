d80b522b55e212cf42c2aaf77c6a50c7f17212a0
Qinfeng Chen <qinfchen@users.noreply.github.com>
2018-09-25 16:10:01 +0100

    [fix] applies the baseline-format plugin only when java plugin is applied (#383)

---
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineFormatIntegrationTest.groovy
@@ -0,0 +1,73 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
+class BaselineFormatIntegrationTest extends AbstractPluginTest {
+
+    def standardBuildFile = '''
+        plugins {
+            id 'java'
+            id 'com.palantir.baseline-format'
+        }
+    '''.stripIndent()
+
+    def noJavaBuildFile = '''
+        plugins {
+            id 'com.palantir.baseline-format'
+        }
+    '''.stripIndent()
+
+    def validJavaFile = '''
+    package test;
+    import com.java.tests;
+    public class Test { void test() {} }
+        '''.stripIndent()
+
+    def 'can apply plugin'() {
+        when:
+        buildFile << standardBuildFile
+
+        then:
+        with('format', '--stacktrace').build()
+    }
+
+    def 'cannot run format task when java plugin is missing'() {
+        when:
+        buildFile << noJavaBuildFile
+
+        then:
+        with('format', '--stacktrace').buildAndFail()
+    }
+
+    def 'format task fixes styles'() {
+        when:
+        buildFile << standardBuildFile
+        file('src/main/java/test/Test.java') << validJavaFile
+
+        then:
+        BuildResult result = with('format').build();
+        result.task(":format").outcome == TaskOutcome.SUCCESS
+        result.task(":spotlessApply").outcome == TaskOutcome.SUCCESS
+        file('src/main/java/test/Test.java').text == '''
+            package test;
+            public class Test { void test() {} }
+        '''.stripIndent()
+    }
+}
---
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineFormatTest.groovy
@@ -0,0 +1,45 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline
+
+import static org.junit.Assert.assertTrue
+
+import com.palantir.baseline.plugins.BaselineFormat
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class BaselineFormatTest extends Specification {
+    private Project project
+
+    def setup() {
+        project = ProjectBuilder.builder().build()
+        project.plugins.apply 'java'
+        project.plugins.apply BaselineFormat
+        project.evaluate()
+    }
+
+    def spotlessPluginApplied() {
+        expect:
+        assertTrue project.plugins.hasPlugin("com.diffplug.gradle.spotless")
+    }
+
+    def baselineFormatCreatesFormatTask() {
+        expect:
+        project.tasks.format
+    }
+}
