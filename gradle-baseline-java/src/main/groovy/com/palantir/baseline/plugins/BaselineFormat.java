6d9e45362a9840663f6c4303637b7039d8c72b37
Dan Sanduleac <dsanduleac@palantir.com>
2018-10-11 14:15:44 +0100

    oops wrong one

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
@@ -30,10 +30,10 @@ class BaselineFormat extends AbstractBaselinePlugin {
         project.getPluginManager().withPlugin("java", plugin -> {
             project.getPluginManager().apply("com.diffplug.gradle.spotless");
 
-            project.getConvention().getPlugin(SpotlessExtension.class).java(java -> {
+            project.getExtensions().getByType(SpotlessExtension.class).java(java -> {
                 Object[] allJavaFiles = project
-                        .getExtensions()
-                        .getByType(JavaPluginConvention.class)
+                        .getConvention()
+                        .getPlugin(JavaPluginConvention.class)
                         .getSourceSets()
                         .stream()
                         .map(SourceSet::getJava)
