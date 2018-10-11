9bcdfb8ee8725c97be4a9cbb4ee41aa04212949a
Dan Sanduleac <dsanduleac@palantir.com>
2018-10-11 16:18:58 +0100

    lazy FileCollection

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
@@ -19,8 +19,8 @@ package com.palantir.baseline.plugins;
 import com.diffplug.gradle.spotless.SpotlessExtension;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.tasks.SourceSet;
 
 class BaselineFormat extends AbstractBaselinePlugin {
 
@@ -31,13 +31,13 @@ class BaselineFormat extends AbstractBaselinePlugin {
             project.getPluginManager().apply("com.diffplug.gradle.spotless");
 
             project.getExtensions().getByType(SpotlessExtension.class).java(java -> {
-                Object[] allJavaFiles = project
+                // Configure a lazy FileCollection then pass it as the target
+                ConfigurableFileCollection allJavaFiles = project.files();
+                project
                         .getConvention()
                         .getPlugin(JavaPluginConvention.class)
                         .getSourceSets()
-                        .stream()
-                        .map(SourceSet::getAllJava)
-                        .toArray();
+                        .all(sourceSet -> allJavaFiles.from(sourceSet.getAllJava()));
 
                 java.target(allJavaFiles);
                 java.removeUnusedImports();
