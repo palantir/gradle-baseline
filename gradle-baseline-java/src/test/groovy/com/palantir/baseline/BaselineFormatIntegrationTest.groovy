42bd071ea0232556b71e57798e5351007a8385dd
Joe Ellis <ellis125@gmail.com>
2018-10-16 16:44:10 -0400

    [fix] Ignore errors in generated code (#428)
    * Ignore errors in generated code
    
    * test
    
    * check path not source set
    
    * ignore based on source set name
    
    * fix condition
    
    * kick build
    
    * kick it again

--- gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineFormatIntegrationTest.groovy
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineFormatIntegrationTest.groovy
@@ -38,7 +38,7 @@ class BaselineFormatIntegrationTest extends AbstractPluginTest {
     package test;
     import com.java.tests;
     public class Test { void test() {} }
-        '''.stripIndent()
+    '''.stripIndent()
 
     def 'can apply plugin'() {
         when:
@@ -106,5 +106,22 @@ class BaselineFormatIntegrationTest extends AbstractPluginTest {
             package test;
             public class Test { void test() {} }
         '''.stripIndent()
+    }
+
+    def 'format ignores generated files'() {
+        when:
+        buildFile << standardBuildFile
+        buildFile << '''
+            sourceSets { generated }
+        '''.stripIndent()
+        file('src/generated/java/test/Test.java') << '''
+            package test;
+            import java.lang.Void;
+            public class Test { Void test() {} }
+        '''.stripIndent()
+
+        then:
+        BuildResult result = with('spotlessJavaCheck').build();
+        result.task(":spotlessJava").outcome == TaskOutcome.SUCCESS
     }
 }
