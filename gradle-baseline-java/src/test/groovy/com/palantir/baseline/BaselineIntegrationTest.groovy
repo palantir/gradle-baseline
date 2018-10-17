61bed0c1f86f79010fc6f7ffe51a6919afd02176
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2018-10-17 15:31:33 +0100

    Test that we can apply the baseline plugin on gradle 5 (#433)

---
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineIntegrationTest.groovy
@@ -0,0 +1,48 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline
+
+
+import spock.lang.Unroll
+
+class BaselineIntegrationTest extends AbstractPluginTest {
+
+    def standardBuildFile() {
+        """
+        plugins {
+            id 'com.palantir.baseline'
+        }
+    
+        repositories {
+            jcenter()
+        }
+        """.stripIndent()
+    }
+
+    @Unroll("Can apply on #gradleVersion")
+    def canApplyOnGradle() {
+        buildFile << standardBuildFile()
+        multiProject.addSubproject("java-project", "apply plugin: 'java'")
+        multiProject.addSubproject("other-project")
+
+        expect:
+        with().withGradleVersion(gradleVersion).build()
+
+        where:
+        gradleVersion << ['4.10.2', '5.0-milestone-1']
+    }
+}
