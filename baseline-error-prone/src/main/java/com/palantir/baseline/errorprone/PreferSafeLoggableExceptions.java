91fdbcdccf5189008bccfc8947d7177d3d7959f2
iamdanfox <iamdanfox@gmail.com>
2018-10-24 15:30:43 +0100

    [feature] errorprone check for SafeLoggable exceptions (#440)

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferSafeLoggableExceptions.java
@@ -0,0 +1,88 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableMap;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.CompileTimeConstantExpressionMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.NewClassTree;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "PreferSafeLoggableExceptions",
+        category = BugPattern.Category.ONE_OFF,
+        severity = BugPattern.SeverityLevel.SUGGESTION,
+        summary = "Throw SafeLoggable exceptions to ensure the exception message will not be redacted")
+public final class PreferSafeLoggableExceptions extends BugChecker implements BugChecker.NewClassTreeMatcher {
+
+    private static final long serialVersionUID = 1L;
+    private final Matcher<ExpressionTree> compileTimeConstExpressionMatcher =
+            new CompileTimeConstantExpressionMatcher();
+
+    // https://github.com/palantir/safe-logging/tree/develop/preconditions/src/main/java/com/palantir/logsafe/exceptions
+    @Override
+    public Description matchNewClass(NewClassTree tree, VisitorState state) {
+        List<? extends ExpressionTree> args = tree.getArguments();
+        Optional<? extends ExpressionTree> messageArg = args.stream()
+                .filter(arg -> ASTHelpers.isSameType(
+                        ASTHelpers.getType(arg),
+                        state.getTypeFromString("java.lang.String"),
+                        state))
+                .reduce((one, two) -> one);
+
+        if (!messageArg.isPresent()) {
+            return Description.NO_MATCH;
+        }
+
+        if (!compileTimeConstExpressionMatcher.matches(messageArg.get(), state)) {
+            // ignore exceptions with non-constant messages to minimise the hits
+            return Description.NO_MATCH;
+        }
+
+        Map<Class<?>, String> map = ImmutableMap.of(
+                IllegalArgumentException.class, "SafeIllegalArgumentException",
+                IllegalStateException.class, "SafeIllegalStateException",
+                IOException.class, "SafeIoException",
+                NullPointerException.class, "SafeNullPointerException",
+                RuntimeException.class, "SafeRuntimeException");
+
+        return map.entrySet().stream()
+                .filter(entry -> Matchers.isSameType(entry.getKey()).matches(tree.getIdentifier(), state))
+                .map(entry -> buildDescription(tree)
+                        .setMessage("Prefer " + entry.getValue() + " from com.palantir.safe-logging:preconditions")
+                        .addFix(SuggestedFix.builder()
+                                .replace(tree.getIdentifier(), entry.getValue())
+                                .addImport("com.palantir.logsafe.exceptions." + entry.getValue())
+                                .build())
+                        .build())
+                .findAny()
+                .orElse(Description.NO_MATCH);
+    }
+}
---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/PreferSafeLoggableExceptionsTest.java
@@ -0,0 +1,138 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PreferSafeLoggableExceptionsTest {
+
+    private CompilationTestHelper compilationHelper;
+
+    @Before
+    public void before() {
+        compilationHelper = CompilationTestHelper.newInstance(PreferSafeLoggableExceptions.class, getClass());
+    }
+
+    @Test
+    public void illegal_argument_exception() {
+        compilationHelper.addSourceLines(
+                "Bean.java",
+                "class Bean {",
+                "// BUG: Diagnostic contains: Prefer SafeIllegalArgumentException",
+                "Exception foo = new IllegalArgumentException(\"Foo\");",
+                "}").doTest();
+    }
+
+    @Test
+    public void auto_fix_illegal_argument_exception() {
+        BugCheckerRefactoringTestHelper.newInstance(new PreferSafeLoggableExceptions(), getClass())
+                .addInputLines(
+                        "Bean.java",
+                        "class Bean {",
+                        "  Exception foo = new IllegalArgumentException(\"Foo\");",
+                        "}",
+                        "")
+                .addOutputLines(
+                        "Bean.java",
+                        "import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;",
+                        "",
+                        "class Bean {",
+                        "  Exception foo = new SafeIllegalArgumentException(\"Foo\");",
+                        "}",
+                        "")
+                .doTest(BugCheckerRefactoringTestHelper.TestMode.TEXT_MATCH);
+    }
+
+    @Test
+    public void illegal_argument_exception_without_message_doesnt_match() {
+        compilationHelper.addSourceLines(
+                "Bean.java",
+                "class Bean {",
+                "Exception foo = new IllegalArgumentException(new RuntimeException());",
+                "}").doTest();
+    }
+
+    @Test
+    public void auto_fix_illegal_state_exception() {
+        BugCheckerRefactoringTestHelper.newInstance(new PreferSafeLoggableExceptions(), getClass())
+                .addInputLines(
+                        "Bean.java",
+                        "class Bean {",
+                        "  Exception foo = new IllegalArgumentException(\"Foo\");",
+                        "}",
+                        "")
+                .addOutputLines(
+                        "Bean.java",
+                        "import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;",
+                        "",
+                        "class Bean {",
+                        "  Exception foo = new SafeIllegalArgumentException(\"Foo\");",
+                        "}",
+                        "")
+                .doTest(BugCheckerRefactoringTestHelper.TestMode.TEXT_MATCH);
+    }
+
+    @Test
+    public void illegal_state_exception() {
+        compilationHelper.addSourceLines(
+                "Bean.java",
+                "class Bean {",
+                "// BUG: Diagnostic contains: Prefer SafeIllegalStateException",
+                "Exception foo = new IllegalStateException(\"Foo\");",
+                "}").doTest();
+    }
+
+    @Test
+    public void illegal_state_exception_without_message_doesnt_match() {
+        compilationHelper.addSourceLines(
+                "Bean.java",
+                "class Bean {",
+                "Exception foo = new IllegalStateException(new RuntimeException());",
+                "}").doTest();
+    }
+
+    @Test
+    public void illegal_state_exception_with_non_constant_message_doesnt_match() {
+        compilationHelper.addSourceLines(
+                "Bean.java",
+                "class Bean {",
+                "Exception foo = new IllegalStateException(\"I am a non-constant string\" + Math.random());",
+                "}").doTest();
+    }
+
+    @Test
+    public void io_exception() {
+        compilationHelper.addSourceLines(
+                "Bean.java",
+                "class Bean {",
+                "// BUG: Diagnostic contains: Prefer SafeIoException",
+                "Exception foo = new java.io.IOException(\"Foo\");",
+                "}").doTest();
+    }
+
+    @Test
+    public void io_exception_without_message_doesnt_match() {
+        compilationHelper.addSourceLines(
+                "Bean.java",
+                "class Bean {",
+                "Exception foo = new java.io.IOException(new RuntimeException());",
+                "}").doTest();
+    }
+}
