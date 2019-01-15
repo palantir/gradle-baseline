3d0f804696d417ac633a2def386fa81c5ff65bc9
Dan Sanduleac <dsanduleac@palantir.com>
2019-01-15 14:23:38 +0000

    import

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineErrorProne.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineErrorProne.java
@@ -31,7 +31,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.plugins.ExtensionAware;
-import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.compile.JavaCompile;
 import org.gradle.api.tasks.javadoc.Javadoc;
 import org.gradle.api.tasks.testing.Test;
@@ -65,7 +64,7 @@ public final class BaselineErrorProne implements Plugin<Project> {
                         ((ExtensionAware) javaCompile.getOptions()).getExtensions()
                                 .configure(ErrorProneOptions.class, errorProneOptions -> {
                                     errorProneOptions.check("Slf4jLogsafeArgs", CheckSeverity.OFF);
-                                    errorProneOptions.check("Slf4jConstantLogMessage", CheckSeverity.OFF);
+                                    errorProneOptions.check("PreferSafeLoggableExceptions", CheckSeverity.OFF);
                                 }));
             });
 
