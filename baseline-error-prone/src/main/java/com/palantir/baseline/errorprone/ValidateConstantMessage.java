1a9e272dfb2d180c967d458d7a9659032db4f426
David Dunn <2678226+dsd987@users.noreply.github.com>
2019-02-22 10:47:55 -0500

    Fix incorrect argument selection in ValidateConstantMessage and expand test functionality. (#546)
    ## Before this PR
    The ValidateConstantMessage check would flag on the last argument that is a string rather than on the message string. Additionally, the grouping of test cases under the single positive test does not correctly test cases after the first one.
    
    ## After this PR
    The ValidateConstantMessage check now knows the argument index of all message argument in all calls so it can be correctly identified and checked. Test cases are now all completely separated in their own tests.

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/ValidateConstantMessage.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/ValidateConstantMessage.java
@@ -17,6 +17,7 @@
 package com.palantir.baseline.errorprone;
 
 import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableMap;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.Category;
 import com.google.errorprone.BugPattern.SeverityLevel;
@@ -30,7 +31,6 @@ import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
 import java.util.List;
-import java.util.Optional;
 
 @AutoService(BugChecker.class)
 @BugPattern(
@@ -49,20 +49,54 @@ public final class ValidateConstantMessage extends BugChecker implements BugChec
     private final Matcher<ExpressionTree> compileTimeConstExpressionMatcher =
             new CompileTimeConstantExpressionMatcher();
 
+    private static final ImmutableMap<String, Integer> VALIDATE_METHODS_MESSAGE_ARGS =
+            ImmutableMap.<String, Integer>builder()
+                    .put("exclusiveBetween", 4)
+                    .put("finite", 2)
+                    .put("inclusiveBetween", 4)
+                    .put("isAssignableFrom", 3)
+                    .put("isInstanceOf", 3)
+                    .put("isTrue", 2)
+                    .put("matchesPattern", 3)
+                    .put("noNullElements", 2)
+                    .put("notBlank", 2)
+                    .put("notEmpty", 2)
+                    .put("notNaN", 2)
+                    .put("notNull", 2)
+                    .put("validIndex", 3)
+                    .put("validState", 2)
+                    .put("allElementsOfType", 3) // commons-lang 2.x only
+                    .build();
+
     @Override
     public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
         if (!VALIDATE_METHODS.matches(tree, state)) {
             return Description.NO_MATCH;
         }
 
+        String methodName = ASTHelpers.getSymbol(tree).name.toString();
+        if (!VALIDATE_METHODS_MESSAGE_ARGS.containsKey(methodName)) {
+            return Description.NO_MATCH;
+        }
+
+        int messageArgNumber = VALIDATE_METHODS_MESSAGE_ARGS.get(methodName);
         List<? extends ExpressionTree> args = tree.getArguments();
 
-        Optional<? extends ExpressionTree> messageArg = args.stream()
-                .filter(arg -> ASTHelpers.isSameType(ASTHelpers.getType(arg),
-                        state.getTypeFromString("java.lang.String"), state))
-                .reduce((one, two) -> two);
+        if (args.size() < messageArgNumber) {
+            return Description.NO_MATCH;
+        }
 
-        if (!messageArg.isPresent() || compileTimeConstExpressionMatcher.matches(messageArg.get(), state)) {
+        ExpressionTree messageArg = args.get(messageArgNumber - 1);
+        boolean isStringType = ASTHelpers.isSameType(
+                ASTHelpers.getType(messageArg),
+                state.getTypeFromString("java.lang.String"),
+                state);
+        boolean isConstantString = compileTimeConstExpressionMatcher.matches(messageArg, state);
+        if (!isStringType || isConstantString) {
+            return Description.NO_MATCH;
+        }
+
+        if (TestCheckUtils.isTestCode(state)) {
             return Description.NO_MATCH;
         }
 
