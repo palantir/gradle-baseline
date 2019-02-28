palantir/gradle-baseline

commit 938bd7e28713da8b80aa2f35d6d35f4c7a56560d

[improvement] ./gradlew idea deletes redundant ipr files (#550)
fixes https://github.com/palantir/gradle-baseline/issues/538

## Before this PR

While adopting gradle-consistent-versions (which sometimes requires renaming the root project), people have been confused by leftover *.ipr files.

## After this PR

Running ./gradlew idea will result in exactly on ipr file.

gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineIdea.groovy
@@ -19,7 +19,9 @@ package com.palantir.baseline.plugins
 import groovy.xml.XmlUtil
 import java.nio.file.Files
 import java.nio.file.Paths
+import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.plugins.ide.idea.IdeaPlugin
 import org.gradle.plugins.ide.idea.model.IdeaModel
@@ -56,6 +58,21 @@ class BaselineIdea extends AbstractBaselinePlugin {
             markResourcesDirs(ideaModuleModel);
             moveProjectReferencesToEnd(ideaModuleModel);
         }
+
+        // If someone renames a project, leftover {ipr,iml,ipr} files may still exist on disk and
+        // confuse users, so we proactively clean them up. Intentionally using an Action<Task> to allow up-to-dateness.
+        Action<Task> cleanup = new Action<Task>() {
+            void execute(Task t) {
+                project.delete(project.fileTree(
+                        dir: project.getProjectDir(), include: '*.ipr', exclude: "${project.name}.ipr"));
+                project.delete(project.fileTree(
+                        dir: project.getProjectDir(), include: '*.iml', exclude: "${project.name}.iml"))
+                project.delete(project.fileTree(
+                        dir: project.getProjectDir(), include: '*.iws', exclude: "${project.name}.iws"))
+            }
+        }
+
+        project.getTasks().findByName("idea").doLast(cleanup);
     }
 
     /**

gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineIdeaIntegrationTest.groovy
@@ -236,4 +236,22 @@ class BaselineIdeaIntegrationTest extends AbstractPluginTest {
         BuildResult result = with('--stacktrace', '--info', 'idea').build()
         assert result.tasks(TaskOutcome.SUCCESS).collect { it.path }.contains(':idea')
     }
+
+    def 'deletes redundant iml,ipr,iws files'() {
+        when:
+        buildFile << standardBuildFile
+        File real = new File(projectDir, projectDir.name + ".ipr")
+        File iws = createFile('foo.ipr')
+        File iml = createFile('foo.iml')
+        File ipr = createFile('foo.iws')
+
+        then:
+        !real.exists()
+        iws.exists() && iml.exists() && ipr.exists()
+
+        with('idea').build()
+
+        real.exists()
+        !iws.exists() && !iml.exists() && !ipr.exists()
+    }
 }

commit 39530ee29b05a609a44f1b52a6d21be345763f19

[fix] Fix cleanup of old idea project files (#559)
## Before this PR

Regression introduced in #550 means that `.iml` files might be removed if the idea extension were configured in such a way as for them to differ from the project name.
Even if not explicitly configured, if a subproject had the same name as the root project, the IDEA plugin would give it a name like `${project.name}-${project.name}.iml` as mentioned in https://github.com/palantir/gradle-baseline/pull/555#issuecomment-467851332, which differs from the name the cleanup task was instructed to exclude.

## After this PR

Decide based on the outputs of the actual tasks creating these ipr / iws / iml files what the name that they would use is, and exclude just that name, rather than assuming it's going to be e.g. `${project.name}.iml`.

Fixes #557.

gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineIdea.groovy
@@ -16,13 +16,19 @@
 
 package com.palantir.baseline.plugins
 
+import groovy.transform.CompileStatic
 import groovy.xml.XmlUtil
 import java.nio.file.Files
 import java.nio.file.Paths
 import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.file.FileTreeElement
+import org.gradle.api.specs.Spec
 import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.plugins.ide.idea.GenerateIdeaModule
+import org.gradle.plugins.ide.idea.GenerateIdeaProject
+import org.gradle.plugins.ide.idea.GenerateIdeaWorkspace
 import org.gradle.plugins.ide.idea.IdeaPlugin
 import org.gradle.plugins.ide.idea.model.IdeaModel
 
@@ -32,8 +38,9 @@ class BaselineIdea extends AbstractBaselinePlugin {
         this.project = project
 
         project.plugins.apply IdeaPlugin
-        project.afterEvaluate {
+        IdeaModel ideaModuleModel = project.extensions.getByType(IdeaModel)
 
+        project.afterEvaluate {
             // Configure Idea project
             IdeaModel ideaRootModel = project.rootProject.extensions.findByType(IdeaModel)
             if (ideaRootModel) {
@@ -53,26 +60,36 @@ class BaselineIdea extends AbstractBaselinePlugin {
             }
 
             // Configure Idea module
-            IdeaModel ideaModuleModel = project.extensions.findByType(IdeaModel)
             addJdkVersion(ideaModuleModel)
-            markResourcesDirs(ideaModuleModel);
-            moveProjectReferencesToEnd(ideaModuleModel);
+            markResourcesDirs(ideaModuleModel)
+            moveProjectReferencesToEnd(ideaModuleModel)
         }
 
         // If someone renames a project, leftover {ipr,iml,ipr} files may still exist on disk and
         // confuse users, so we proactively clean them up. Intentionally using an Action<Task> to allow up-to-dateness.
         Action<Task> cleanup = new Action<Task>() {
             void execute(Task t) {
+                if (t.project.rootProject == t.project) {
+                    def iprFile = t.project.tasks.withType(GenerateIdeaProject).find().outputFile
+                    def iwsFile = t.project.tasks.withType(GenerateIdeaWorkspace).find().outputFile
+                    project.delete(project.fileTree(
+                            dir: project.getProjectDir(), include: '*.ipr', exclude: isFile(iprFile)))
+                    project.delete(project.fileTree(
+                            dir: project.getProjectDir(), include: '*.iws', exclude: isFile(iwsFile)))
+                }
+
+                def imlFile = t.project.tasks.withType(GenerateIdeaModule).find().outputFile
                 project.delete(project.fileTree(
-                        dir: project.getProjectDir(), include: '*.ipr', exclude: "${project.name}.ipr"));
-                project.delete(project.fileTree(
-                        dir: project.getProjectDir(), include: '*.iml', exclude: "${project.name}.iml"))
-                project.delete(project.fileTree(
-                        dir: project.getProjectDir(), include: '*.iws', exclude: "${project.name}.iws"))
+                        dir: project.getProjectDir(), include: '*.iml', exclude: isFile(imlFile)))
             }
         }
 
-        project.getTasks().findByName("idea").doLast(cleanup);
+        project.getTasks().findByName("idea").doLast(cleanup)
+    }
+
+    @CompileStatic
+    static Spec<FileTreeElement> isFile(File file) {
+        { FileTreeElement details -> details.file == file } as Spec<FileTreeElement>
     }
 
     /**

gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineIdeaIntegrationTest.groovy
@@ -28,6 +28,9 @@ class BaselineIdeaIntegrationTest extends AbstractPluginTest {
             id 'java'
             id 'com.palantir.baseline-idea'
         }
+        allprojects {
+            apply plugin: 'com.palantir.baseline-idea'
+        }
     '''.stripIndent()
 
     def setup() {
@@ -253,5 +256,42 @@ class BaselineIdeaIntegrationTest extends AbstractPluginTest {
 
         real.exists()
         !iws.exists() && !iml.exists() && !ipr.exists()
+    }
+
+    def 'does not delete subproject iml has different name than subproject'() {
+        def rootProjectName = projectDir.name
+
+        buildFile << standardBuildFile
+        File real = new File(projectDir, rootProjectName + ".ipr")
+        File iws = createFile('foo.ipr')
+        File iml = createFile('foo.iml')
+        File ipr = createFile('foo.iws')
+
+        def subprojectDir = multiProject.addSubproject('bar', '''
+            idea {
+                module {
+                    name = 'something-else'
+                }
+            }
+        '''.stripIndent())
+        def subprojectIml = new File(subprojectDir, 'something-else.iml')
+
+        expect:
+        !real.exists()
+        iws.exists() && iml.exists() && ipr.exists()
+
+        when:
+        with('idea').build()
+
+        then:
+        real.exists()
+        subprojectIml.exists() // expect 'idea' to have created it
+
+        when:
+        def otherSubprojectIml = createFile('some-other-name.iml', subprojectDir)
+        with('idea').build()
+
+        then:
+        !otherSubprojectIml.exists()
     }
 }