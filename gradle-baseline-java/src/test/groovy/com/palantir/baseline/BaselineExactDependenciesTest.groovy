6f5bbb3f0717c4b34ce718f73f884a6374a12b0b
Dan Fox <dfox@palantir.com>
2019-03-05 22:41:48 +0000

    test checkUnusedDependencies and checkImplicitDependencies

--- gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineExactDependenciesTest.groovy
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineExactDependenciesTest.groovy
@@ -16,6 +16,9 @@
 
 package com.palantir.baseline
 
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
 class BaselineExactDependenciesTest extends AbstractPluginTest {
 
     def standardBuildFile = '''
@@ -25,7 +28,7 @@ class BaselineExactDependenciesTest extends AbstractPluginTest {
         }
     '''.stripIndent()
 
-    def validJavaFile = '''
+    def minimalJavaFile = '''
     package pkg;
     public class Foo { void foo() {} }
     '''.stripIndent()
@@ -33,9 +36,67 @@ class BaselineExactDependenciesTest extends AbstractPluginTest {
     def 'both tasks vacuously pass with no dependencies'() {
         when:
         buildFile << standardBuildFile
-        file('src/main/java/pkg/Foo.java') << validJavaFile
+        file('src/main/java/pkg/Foo.java') << minimalJavaFile
 
         then:
         with('checkUnusedDependencies', 'checkImplicitDependencies', '--stacktrace').build()
+    }
+
+    def 'tasks are not run as part of ./gradlew check'() {
+        when:
+        buildFile << standardBuildFile
+        file('src/main/java/pkg/Foo.java') << minimalJavaFile
+
+        then:
+        BuildResult result = with('check').build()
+        result.task(':checkUnusedDependencies') == null
+        result.task(':checkImplicitDependencies ') == null
+    }
+
+    def 'checkUnusedDependencies fails when no classes are referenced'() {
+        when:
+        buildFile << standardBuildFile
+        buildFile << """
+        repositories {
+            mavenCentral()
+        }
+        dependencies {
+            compile 'com.google.guava:guava:27.0.1-jre'
+        }
+        """
+        file('src/main/java/pkg/Foo.java') << minimalJavaFile
+
+        then:
+        BuildResult result = with('checkUnusedDependencies', '--stacktrace').buildAndFail()
+        result.task(':classes').getOutcome() == TaskOutcome.SUCCESS
+        result.task(':checkUnusedDependencies').getOutcome() == TaskOutcome.FAILED
+        result.output.contains("Found 1 dependencies unused during compilation")
+    }
+
+    def 'checkImplicitDependencies fails when a class is imported without being declared as a dependency'() {
+        when:
+        buildFile << standardBuildFile
+        buildFile << """
+        repositories {
+            mavenCentral()
+        }
+        dependencies {
+            compile 'com.fasterxml.jackson.datatype:jackson-datatype-guava:2.9.8' // pulls in guava transitively
+        }
+        """
+        file('src/main/java/pkg/Foo.java') << '''
+        package pkg;
+        public class Foo {
+            void foo() {
+                com.google.common.collect.ImmutableList.of();
+            }
+        }
+        '''.stripIndent()
+
+        then:
+        BuildResult result = with('checkImplicitDependencies', '--stacktrace').buildAndFail()
+        result.task(':classes').getOutcome() == TaskOutcome.SUCCESS
+        result.task(':checkImplicitDependencies').getOutcome() == TaskOutcome.FAILED
+        result.output.contains("Found 1 implicit dependencies")
     }
 }
