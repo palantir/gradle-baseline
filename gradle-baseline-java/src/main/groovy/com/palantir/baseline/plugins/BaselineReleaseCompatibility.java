22f8ac0167cba1f34356768c1ca3a53f93070dcb
Dan Fox <dfox@palantir.com>
2019-03-25 22:09:02 +0000

    Rename META-INF

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineReleaseCompatibility.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineReleaseCompatibility.java
@@ -22,7 +22,6 @@ import java.util.Optional;
 import org.gradle.api.JavaVersion;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.compile.JavaCompile;
-import org.gradle.process.CommandLineArgumentProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
