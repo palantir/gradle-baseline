a487f30530ea370c283a4472b9548f96ea9e749d
Dan Fox <dfox@palantir.com>
2019-03-25 22:18:40 +0000

    Integration test verifies Optional.isEmpty() fails compilation

---
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineReleaseCompatibilityIntegrationTest.groovy
@@ -0,0 +1,56 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
+class BaselineReleaseCompatibilityIntegrationTest extends AbstractPluginTest {
+
+    def standardBuildFile = '''
+        plugins {
+            id 'java'
+            id 'com.palantir.baseline-release-compatibility'
+        }
+
+        sourceCompatibility = 1.8
+
+        repositories {
+            mavenLocal()
+            jcenter()
+        }
+    '''.stripIndent()
+
+    def invalidJavaFile = '''
+        package test;
+        public class Invalid {
+            void demo() {
+                java.util.Optional.of(1).isEmpty(); // this method was added in Java9
+            }
+        }
+    '''.stripIndent()
+
+    def 'compileJava fails when features from Java9 are used'() {
+        when:
+        buildFile << standardBuildFile
+        file('src/main/java/test/Invalid.java').text = invalidJavaFile
+
+        then:
+        BuildResult result = with('compileJava').buildAndFail()
+        result.task(":compileJava").outcome == TaskOutcome.FAILED
+    }
+}
