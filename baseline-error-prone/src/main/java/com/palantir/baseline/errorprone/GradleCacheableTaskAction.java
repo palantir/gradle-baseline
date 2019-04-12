fd1524c1c1274e2ba5c8b15f103bf2243cafd88b
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2019-04-12 21:57:19 +0100

    [feature] Gradle task action errorprone check (#608)
    ## Before this PR
    
    Devs could accidentally pass a lambda to `Task.doFirst` or `Task.doLast` actions in gradle plugins. This makes the task non-cacheable, as pointed out in https://github.com/gradle/gradle/issues/5510.
    
    
    ## After this PR
    
    ==COMMIT_MSG=
    Add an errorprone rule `GradleCacheableTaskAction` that prevents passing a lambda to `Task.doFirst` or `Task.doLast` when implementing gradle plugins.
    ==COMMIT_MSG==

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/GradleCacheableTaskAction.java
@@ -0,0 +1,61 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.LambdaExpressionTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.LambdaExpressionTree;
+import java.util.regex.Pattern;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "GradleCacheableTaskAction",
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = SeverityLevel.ERROR,
+        summary = "Forbid gradle task actions (doFirst, doLast) to be implemented by lambdas.")
+public final class GradleCacheableTaskAction extends BugChecker implements LambdaExpressionTreeMatcher {
+
+    private static final long serialVersionUID = 1L;
+    private static final Matcher<ExpressionTree> IS_ACTION = Matchers.isSubtypeOf("org.gradle.api.Action");
+
+    private static final Matcher<ExpressionTree> TASK_ACTION = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.gradle.api.Task")
+            .withNameMatching(Pattern.compile("doFirst|doLast"));
+
+    private static final Matcher<ExpressionTree> IS_TASK_ACTION =
+            Matchers.allOf(Matchers.parentNode(Matchers.methodInvocation(TASK_ACTION)), IS_ACTION);
+
+    @Override
+    public Description matchLambdaExpression(LambdaExpressionTree tree, VisitorState state) {
+        if (!IS_TASK_ACTION.matches(tree, state)) {
+            return Description.NO_MATCH;
+        }
+        return buildDescription(tree)
+                .setMessage("Gradle task actions are not cacheable when implemented by lambdas")
+                .build();
+    }
+}
---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/GradleCacheableTaskActionTest.java
@@ -0,0 +1,74 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GradleCacheableTaskActionTest {
+
+    private static final String errorMsg = "BUG: Diagnostic contains: "
+            + "Gradle task actions are not cacheable when implemented by lambdas";
+
+    private CompilationTestHelper compilationHelper;
+
+    @Before
+    public void before() {
+        compilationHelper = CompilationTestHelper.newInstance(GradleCacheableTaskAction.class, getClass());
+    }
+
+    @Test
+    public void failsOnDoFirstLambda() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Task;",
+                        "import org.gradle.api.Project;",
+                        "import org.gradle.api.Plugin;",
+                        "class Foo implements Plugin<Project> {",
+                        "  public final void apply(Project project) {",
+                        "    project.getTasks().register(\"foo\", task -> {",
+                        "      // " + errorMsg,
+                        "      task.doFirst(t -> System.out.println(\"im a lambda\"));",
+                        "    });",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void allowsDoFirstAnonymousClass() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Task;",
+                        "import org.gradle.api.Action;",
+                        "import org.gradle.api.Project;",
+                        "import org.gradle.api.Plugin;",
+                        "class Foo implements Plugin<Project> {",
+                        "  public final void apply(Project project) {",
+                        "    project.getTasks().register(\"foo\", task -> {",
+                        "      task.doFirst(new Action<Task>() {",
+                        "          public void execute(Task task) {}",
+                        "      });",
+                        "    });",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+}
