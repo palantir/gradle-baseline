22ad31040e050b66855ad081961057cfa744bcbe
Dan Sanduleac <dsanduleac@palantir.com>
2019-04-12 21:37:11 +0100

    rewrite from point of view of lambda

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/GradleCacheableTaskAction.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/GradleCacheableTaskAction.java
@@ -17,22 +17,17 @@
 package com.palantir.baseline.errorprone;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.Iterables;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.SeverityLevel;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
-import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
-import com.google.errorprone.matchers.ChildMultiMatcher.MatchType;
+import com.google.errorprone.bugpatterns.BugChecker.LambdaExpressionTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.matchers.Matchers;
 import com.google.errorprone.matchers.method.MethodMatchers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.LambdaExpressionTree;
-import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.Tree;
-import com.sun.source.tree.Tree.Kind;
 import java.util.regex.Pattern;
 
 @AutoService(BugChecker.class)
@@ -42,27 +37,24 @@ import java.util.regex.Pattern;
         linkType = BugPattern.LinkType.CUSTOM,
         severity = SeverityLevel.ERROR,
         summary = "Forbid gradle actions (doFirst, doLast) to be implemented by lambdas.")
-public final class GradleCacheableTaskAction extends BugChecker implements MethodInvocationTreeMatcher {
+public final class GradleCacheableTaskAction extends BugChecker implements LambdaExpressionTreeMatcher {
 
     private static final long serialVersionUID = 1L;
-    private static final Matcher<Tree> IS_ACTION = Matchers.isSubtypeOf("org.gradle.api.Action");
+    private static final Matcher<ExpressionTree> IS_ACTION = Matchers.isSubtypeOf("org.gradle.api.Action");
 
     private static final Matcher<ExpressionTree> TASK_ACTION = MethodMatchers.instanceMethod()
             .onDescendantOf("org.gradle.api.Task")
             .withNameMatching(Pattern.compile("doFirst|doLast"));
 
-    private static final Matcher<MethodInvocationTree> TASK_ACTION_WITH_LAMBDA = Matchers.allOf(
-            TASK_ACTION,
-            Matchers.hasArguments(MatchType.LAST, Matchers.allOf(IS_ACTION, Matchers.kindIs(Kind.LAMBDA_EXPRESSION))));
+    private static final Matcher<ExpressionTree> IS_TASK_ACTION =
+            Matchers.allOf(Matchers.parentNode(Matchers.methodInvocation(TASK_ACTION)), IS_ACTION);
 
     @Override
-    public Description matchMethodInvocation(
-            MethodInvocationTree tree, VisitorState state) {
-        if (!TASK_ACTION_WITH_LAMBDA.matches(tree, state)) {
+    public Description matchLambdaExpression(LambdaExpressionTree tree, VisitorState state) {
+        if (!IS_TASK_ACTION.matches(tree, state)) {
             return Description.NO_MATCH;
         }
-        LambdaExpressionTree lambdaArg = (LambdaExpressionTree) Iterables.getLast(tree.getArguments());
-        return buildDescription(lambdaArg)
+        return buildDescription(tree)
                 .setMessage("Gradle task actions are not cacheable when implemented by lambdas")
                 .build();
     }
