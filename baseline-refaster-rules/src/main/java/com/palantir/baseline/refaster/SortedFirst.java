6da58328e19cda1db1163f0489b91ba848b1de7d
Nicholas Gates <gatesn@users.noreply.github.com>
2019-06-27 08:15:11 -0400

    [improvement] Refaster rule compilation (#661)
    ## After this PR
    Passing `-PrefasterApply` to the `compileJava` tasks will apply any refaster rules discovered in the `refaster` configuration.
    
    Includes some default baseline refaster rules.

---
+++ baseline-refaster-rules/src/main/java/com/palantir/baseline/refaster/SortedFirst.java
@@ -0,0 +1,40 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.refaster;
+
+import com.google.errorprone.refaster.annotation.AfterTemplate;
+import com.google.errorprone.refaster.annotation.BeforeTemplate;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Example refaster rule as shown at https://errorprone.info/docs/refaster.
+ */
+public final class SortedFirst<T> {
+
+    @BeforeTemplate
+    Optional<T> before(Stream<T> stream, Comparator<? super T> comparator) {
+        return stream.sorted(comparator).findFirst();
+    }
+
+    @AfterTemplate
+    Optional<T> after(Stream<T> stream, Comparator<? super T> comparator) {
+        return stream.min(comparator);
+    }
+
+}
---
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineErrorProneRefasterIntegrationTest.groovy
@@ -0,0 +1,113 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline
+
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
+/**
+ * This test depends on ./gradlew :baseline-error-prone:publishToMavenLocal
+ */
+class BaselineErrorProneRefasterIntegrationTest extends AbstractPluginTest {
+
+    def standardBuildFile = '''
+        plugins {
+            id 'java'
+            id 'com.palantir.baseline-error-prone'
+            id 'org.inferred.processors' version '1.3.0'
+        }
+        repositories {
+            mavenLocal()
+            jcenter()
+        }
+    '''.stripIndent()
+
+    def 'compileJava with refaster fixes CollectionsIsEmpty'() {
+        when:
+        buildFile << standardBuildFile
+        file('src/main/java/test/Test.java') << '''
+        package test;
+        import java.util.ArrayList;
+        public class Test {
+            boolean empty = new ArrayList<>().size() == 0;
+        }
+        '''.stripIndent()
+
+        then:
+        BuildResult result = with('compileJava', '-i', '-PrefasterApply').build()
+        result.task(":compileJava").outcome == TaskOutcome.SUCCESS
+        file('src/main/java/test/Test.java').text == '''
+        package test;
+        import java.util.ArrayList;
+        public class Test {
+            boolean empty = new ArrayList<>().isEmpty();
+        }
+        '''.stripIndent()
+    }
+
+    def 'compileJava with refaster fixes SortedFirst'() {
+        when:
+        buildFile << standardBuildFile
+        file('src/main/java/test/Test.java') << '''
+        package test;
+        import java.util.*;
+        import java.util.stream.Stream; 
+        public class Test {
+            Optional<Integer> i = Arrays.asList(5, -10, 7, -18, 23).stream()
+                .sorted(Comparator.reverseOrder())
+                .findFirst();
+        }
+        '''.stripIndent()
+
+        then:
+        BuildResult result = with('compileJava', '-i', '-PrefasterApply').build()
+        result.task(":compileJava").outcome == TaskOutcome.SUCCESS
+        file('src/main/java/test/Test.java').text == '''
+        package test;
+        import java.util.*;
+        import java.util.stream.Stream; 
+        public class Test {
+            Optional<Integer> i = Arrays.asList(5, -10, 7, -18, 23).stream().min(Comparator.reverseOrder());
+        }
+        '''.stripIndent()
+    }
+
+    def 'compileJava with refaster fixes Utf8Length'() {
+        when:
+        buildFile << standardBuildFile
+        file('src/main/java/test/Test.java') << '''
+        package test;
+        import java.nio.charset.StandardCharsets;
+        public class Test {
+            int i = "hello world".getBytes(StandardCharsets.UTF_8).length;
+        }
+        '''.stripIndent()
+
+        then:
+        BuildResult result = with('compileJava', '-i', '-PrefasterApply').build()
+        result.task(":compileJava").outcome == TaskOutcome.SUCCESS
+        file('src/main/java/test/Test.java').text == '''
+        package test;
+        import com.google.common.base.Utf8;
+        import java.nio.charset.StandardCharsets;
+        public class Test {
+            int i = Utf8.encodedLength("hello world");
+        }
+        '''.stripIndent()
+    }
+}
