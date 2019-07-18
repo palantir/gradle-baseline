f83a0191a89b7acebcf3edcc6f8793049c0c5a7f
Nicholas Gates <gatesn@users.noreply.github.com>
2019-07-18 15:48:27 +0100

    Test helper for refaster checks (#697)

---
+++ baseline-refaster-rules/src/test/java/com/palantir/baseline/refaster/Utf8LengthTest.java
@@ -0,0 +1,41 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.refaster;
+
+import org.junit.Test;
+
+public class Utf8LengthTest {
+
+    @Test
+    public void test() {
+        RefasterTestHelper
+                .forRefactoring(Utf8Length.class)
+                .withInputLines(
+                        "Test",
+                         "import java.nio.charset.StandardCharsets;",
+                        "public class Test {",
+                        "  int i = \"hello world\".getBytes(StandardCharsets.UTF_8).length;",
+                        "}")
+                .hasOutputLines(
+                        "import com.google.common.base.Utf8;",
+                        "import java.nio.charset.StandardCharsets;",
+                        "public class Test {",
+                        "  int i = Utf8.encodedLength(\"hello world\");",
+                        "}");
+    }
+
+}
---
+++ baseline-refaster-testing/src/main/java/com/palantir/baseline/refaster/CompilerUtility.java
@@ -0,0 +1,88 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.refaster;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.CharStreams;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.tools.javac.api.JavacTaskImpl;
+import com.sun.tools.javac.api.JavacTool;
+import com.sun.tools.javac.util.Context;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Locale;
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+
+final class CompilerUtility {
+
+    private CompilerUtility() {}
+
+    static CompilerResult compile(JavaFileObject javaFileObject) {
+        JavaCompiler compiler = JavacTool.create();
+        DiagnosticCollector<JavaFileObject> diagnosticsCollector = new DiagnosticCollector<>();
+        StandardJavaFileManager fileManager =
+                compiler.getStandardFileManager(diagnosticsCollector, Locale.ENGLISH, StandardCharsets.UTF_8);
+
+        JavacTaskImpl task = (JavacTaskImpl) compiler.getTask(
+                CharStreams.nullWriter(),
+                fileManager,
+                diagnosticsCollector,
+                ImmutableList.of(),
+                null,
+                ImmutableList.of(javaFileObject));
+
+        Iterable<? extends CompilationUnitTree> trees;
+        try {
+            trees = task.parse();
+            task.analyze();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return new CompilerResult() {
+            @Override
+            public Context context() {
+                return task.getContext();
+            }
+
+            @Override
+            public List<CompilationUnitTree> compilationUnits() {
+                return ImmutableList.copyOf(trees);
+            }
+
+            @Override
+            public List<Diagnostic<? extends JavaFileObject>> diagnostics() {
+                return diagnosticsCollector.getDiagnostics();
+            }
+        };
+    }
+
+    interface CompilerResult {
+
+        Context context();
+
+        List<CompilationUnitTree> compilationUnits();
+
+        List<Diagnostic<? extends JavaFileObject>> diagnostics();
+
+    }
+
+}
