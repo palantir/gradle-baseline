6c652244bd1a08c77e29b0dd3febfccccf7a3a38
Dan Sanduleac <dsanduleac@palantir.com>
2019-08-07 12:16:16 +0100

    fix up eagerOrElse template, suppress the check

--- baseline-refaster-rules/src/main/java/com/palantir/baseline/refaster/OptionalOrElseSupplier.java
+++ baseline-refaster-rules/src/main/java/com/palantir/baseline/refaster/OptionalOrElseSupplier.java
@@ -22,9 +22,10 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 public class OptionalOrElseSupplier<T> {
+    @SuppressWarnings("OptionalOrElseMethodInvocation")
     @BeforeTemplate
     final T eagerOrElse(Optional<T> optional, Supplier<T> supplier) {
-        return optional.orElseGet(() -> supplier.get());
+        return optional.orElse(supplier.get());
     }
 
     @AfterTemplate
