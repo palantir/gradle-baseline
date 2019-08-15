a02ba6df65edb4d722a30719f778521ee17c23d6
Carter Kozak <ckozak@ckozak.net>
2019-08-15 15:55:11 -0400

    Error prone validation that Stream.sort is invoked on comparable streams (#753)
    Error prone validation that Stream.sort is invoked on comparable streams

---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/NonComparableStreamSortTests.java
@@ -0,0 +1,62 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NonComparableStreamSortTests {
+
+    private CompilationTestHelper compilationHelper;
+
+    @Before
+    public void before() {
+        compilationHelper = CompilationTestHelper.newInstance(NonComparableStreamSort.class, getClass());
+    }
+
+    @Test
+    public void should_fail_on_non_comparable_stream() {
+        compilationHelper.addSourceLines(
+                "Test.java",
+                "import java.util.List;",
+                "import java.util.ArrayList;",
+                "class Test {",
+                "   public static final void main(String[] args) {",
+                "       List<Object> list = new ArrayList<>();",
+                "       // BUG: Stream.sorted() should only be called on streams of Comparable types.",
+                "       list.stream().sorted();",
+                "   }",
+                "}"
+        ).doTest();
+    }
+
+    @Test
+    public void should_on_comparable_stream() {
+        compilationHelper.addSourceLines(
+                "Test.java",
+                "import java.util.List;",
+                "import java.util.ArrayList;",
+                "class Test {",
+                "   public static final void main(String[] args) {",
+                "       List<String> list = new ArrayList<>();",
+                "       list.stream().sorted();",
+                "   }",
+                "}"
+        ).doTest();
+    }
+}
