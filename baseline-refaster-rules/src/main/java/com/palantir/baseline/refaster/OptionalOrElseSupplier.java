c8c1d0d71a8f5ada257fafe4076142f73ba643b3
forozco <forozco@palantir.com>
2019-08-29 15:18:23 +0100

    fix

--- baseline-refaster-rules/src/main/java/com/palantir/baseline/refaster/OptionalOrElseSupplier.java
+++ baseline-refaster-rules/src/main/java/com/palantir/baseline/refaster/OptionalOrElseSupplier.java
@@ -24,7 +24,7 @@ import java.util.function.Supplier;
 public class OptionalOrElseSupplier<T> {
     @BeforeTemplate
     final T eagerOrElse(Optional<T> optional, Supplier<T> supplier) {
-        return optional.orElseGet(() -> supplier.get());
+        return optional.orElse(supplier.get());
     }
 
     @AfterTemplate
