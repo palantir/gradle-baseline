23cf92f922534b34957330a664f5cd3f6162580a
Dan Sanduleac <dsanduleac@palantir.com>
2019-09-11 15:55:27 -0400

    add multicatch test

---
+++ gradle-baseline-java/src/test/resources/com/palantir/baseline/formatter-in/java/src/main/java/test/Multicatch.java
@@ -0,0 +1,12 @@
+package test;
+
+class Multicatch {
+    void test() {
+        try {
+            // whatever
+        } catch (ClassNotFoundException | ClassCastException | NoSuchMethodException
+                | IllegalAccessException | InvocationTargetException e) {
+            // ignore
+        }
+    }
+}
