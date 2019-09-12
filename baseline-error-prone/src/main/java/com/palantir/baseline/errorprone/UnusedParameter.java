a7d71e42ac6a9b5a4f1f2211f0036a07492f226b
forozco <forozco@palantir.com>
2019-09-12 15:11:41 -0400

    comment

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/UnusedParameter.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/UnusedParameter.java
@@ -95,7 +95,10 @@ import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 
-/** Bugpattern to detect unused declarations. */
+/**
+ * Copy from {@Link UnusedVariable } except we consider all parameter for unused analysis.
+ * We removed the `onlyCheckForReassignments` filter
+ * */
 @AutoService(BugChecker.class)
 @BugPattern(
         name = "UnusedParameter",
