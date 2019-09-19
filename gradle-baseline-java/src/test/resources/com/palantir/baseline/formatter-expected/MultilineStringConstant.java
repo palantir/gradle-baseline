0336e9ee1d2f3e7c061b3ed5f458646141667a6d
Carter Kozak <ckozak@palantir.com>
2019-09-19 11:17:05 -0400

    Test for multiline string constant with NLS comments
    Oh noes!

---
+++ gradle-baseline-java/src/test/resources/com/palantir/baseline/formatter-expected/MultilineStringConstant.java
@@ -0,0 +1,26 @@
+package test;
+
+class MultilineStringConstant {
+
+    // NON-NLS comments are required for i18n, it's important they are kept with their strings.
+    private static final String MULTIPLE_LINE_NON_NLS =
+            "field_0," + //$NON-NLS-1$
+                    "field_1," //$NON-NLS-1$
+                    +
+                    "field_2,"
+                    +
+                    "field_3,"
+                    +
+                    "field_4";
+
+    private static final String MULTIPLE_LINE_NO_COMMENT =
+            "field_0," +
+                    "field_1,"
+                    +
+                    "field_2,"
+                    +
+                    "field_3,"
+                    +
+                    "field_4";
+
+}
