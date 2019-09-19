9a76d2423001b3e0f86231dd73019f84fcc73b6c
forozco <forozco@palantir.com>
2019-09-19 18:04:56 -0400

    StrictUnusedVariable maintains side effects

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/StrictUnusedVariable.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/StrictUnusedVariable.java
@@ -409,10 +409,7 @@ public final class StrictUnusedVariable extends BugChecker implements BugChecker
             return ImmutableList.of();
         }
         ElementKind varKind = varSymbol.getKind();
-        boolean encounteredSideEffects = false;
         SuggestedFix.Builder fix = SuggestedFix.builder().setShortDescription("remove unused variable");
-        SuggestedFix.Builder removeSideEffectsFix =
-                SuggestedFix.builder().setShortDescription("remove unused variable and any side effects");
         for (TreePath usagePath : usagePaths) {
             StatementTree statement = (StatementTree) usagePath.getLeaf();
             if (statement.getKind() == Tree.Kind.VARIABLE) {
@@ -422,17 +419,14 @@ public final class StrictUnusedVariable extends BugChecker implements BugChecker
                 VariableTree variableTree = (VariableTree) statement;
                 ExpressionTree initializer = variableTree.getInitializer();
                 if (hasSideEffect(initializer) && TOP_LEVEL_EXPRESSIONS.contains(initializer.getKind())) {
-                    encounteredSideEffects = true;
                     if (varKind == ElementKind.FIELD) {
                         String newContent =
                                 String.format(
                                         "%s{ %s; }",
                                         varSymbol.isStatic() ? "static " : "", state.getSourceForNode(initializer));
                         fix.merge(SuggestedFixes.replaceIncludingComments(usagePath, newContent, state));
-                        removeSideEffectsFix.replace(statement, "");
                     } else {
                         fix.replace(statement, String.format("%s;", state.getSourceForNode(initializer)));
-                        removeSideEffectsFix.replace(statement, "");
                     }
                 } else if (isEnhancedForLoopVar(usagePath)) {
                     String modifiers =
@@ -440,19 +434,14 @@ public final class StrictUnusedVariable extends BugChecker implements BugChecker
                                     variableTree.getModifiers() == null
                                             ? null
                                             : state.getSourceForNode(variableTree.getModifiers()));
-                    String newContent =
-                            String.format(
-                                    "%s%s unused",
-                                    modifiers.isEmpty() ? "" : (modifiers + " "), variableTree.getType());
+                    String newContent = String.format(
+                            "%s%s unused", modifiers.isEmpty() ? "" : (modifiers + " "), variableTree.getType());
                     // The new content for the second fix should be identical to the content for the first
                     // fix in this case because we can't just remove the enhanced for loop variable.
                     fix.replace(variableTree, newContent);
-                    removeSideEffectsFix.replace(variableTree, newContent);
                 } else {
                     String replacement = needsBlock(usagePath) ? "{}" : "";
                     fix.merge(SuggestedFixes.replaceIncludingComments(usagePath, replacement, state));
-                    removeSideEffectsFix.merge(
-                            SuggestedFixes.replaceIncludingComments(usagePath, replacement, state));
                 }
                 continue;
             } else if (statement.getKind() == Tree.Kind.EXPRESSION_STATEMENT) {
@@ -468,26 +457,20 @@ public final class StrictUnusedVariable extends BugChecker implements BugChecker
                                         ((JCTree.JCAssignOp) tree).getExpression().getStartPosition(),
                                         "");
                         fix.merge(replacement);
-                        removeSideEffectsFix.merge(replacement);
                         continue;
                     }
                 } else if (tree instanceof AssignmentTree) {
                     if (hasSideEffect(((AssignmentTree) tree).getExpression())) {
-                        encounteredSideEffects = true;
                         fix.replace(
                                 tree.getStartPosition(), ((JCTree.JCAssign) tree).getExpression().getStartPosition(), "");
-                        removeSideEffectsFix.replace(statement, "");
                         continue;
                     }
                 }
             }
             String replacement = needsBlock(usagePath) ? "{}" : "";
             fix.replace(statement, replacement);
-            removeSideEffectsFix.replace(statement, replacement);
         }
-        return encounteredSideEffects
-                ? ImmutableList.of(removeSideEffectsFix.build(), fix.build())
-                : ImmutableList.of(fix.build());
+        return ImmutableList.of(fix.build());
     }
 
     private static ImmutableList<SuggestedFix> buildUnusedParameterFixes(
