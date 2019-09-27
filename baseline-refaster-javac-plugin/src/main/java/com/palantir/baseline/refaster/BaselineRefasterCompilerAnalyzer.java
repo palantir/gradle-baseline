91c2b71b4c2d16e19fd20b62131a973eb5ac279b
Carter Kozak <ckozak@palantir.com>
2019-09-27 14:34:45 -0400

    fmt

--- baseline-refaster-javac-plugin/src/main/java/com/palantir/baseline/refaster/BaselineRefasterCompilerAnalyzer.java
+++ baseline-refaster-javac-plugin/src/main/java/com/palantir/baseline/refaster/BaselineRefasterCompilerAnalyzer.java
@@ -32,8 +32,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TaskListener that receives compilation of a Refaster rule class and outputs a combined serialized analyzer
