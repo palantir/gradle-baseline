5746b6edc95b2fdf9af3a4eb9065d41d58cb6fa4
Dan Fox <dfox@palantir.com>
2019-09-30 19:15:05 +0100

    AssertJ rules for some inequalities

---
+++ baseline-refaster-rules/src/test/java/com/palantir/baseline/refaster/AssertjInequalitiesTest.java
@@ -0,0 +1,80 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.refaster;
+
+import static org.assertj.core.api.Assumptions.assumeThat;
+
+import org.junit.Test;
+
+public class AssertjInequalitiesTest {
+
+    @Test
+    public void less_than() {
+        assumeThat(System.getProperty("java.specification.version"))
+                .describedAs("Refaster does not currently support fluent refactors on java 11")
+                .isEqualTo("1.8");
+        RefasterTestHelper
+                .forRefactoring(AssertjLessThan.class)
+                .withInputLines(
+                        "Test",
+                        "import static org.assertj.core.api.Assertions.assertThat;",
+                        "import java.util.List;",
+                        "public class Test {",
+                        "  void int_istrue(int a, int b) {",
+                        "    assertThat(a < b).isTrue();",
+                        "    assertThat(a >= b).isFalse();",
+                        "  }",
+                        "}")
+                .hasOutputLines(
+                        "import static org.assertj.core.api.Assertions.assertThat;",
+                        "import java.util.List;",
+                        "public class Test {",
+                        "  void int_istrue(int a, int b) {",
+                        "    assertThat(a).isLessThan(b);",
+                        "    assertThat(a).isLessThan(b);",
+                        "  }",
+                        "}");
+    }
+
+    @Test
+    public void less_than_with_description() {
+        assumeThat(System.getProperty("java.specification.version"))
+                .describedAs("Refaster does not currently support fluent refactors on java 11")
+                .isEqualTo("1.8");
+        RefasterTestHelper
+                .forRefactoring(AssertjLessThanWithDescription.class)
+                .withInputLines(
+                        "Test",
+                        "import static org.assertj.core.api.Assertions.assertThat;",
+                        "import java.util.List;",
+                        "public class Test {",
+                        "  void int_istrue(int a, int b, String desc) {",
+                        "    assertThat(a < b).describedAs(desc).isTrue();",
+                        "    assertThat(a >= b).describedAs(desc).isFalse();",
+                        "  }",
+                        "}")
+                .hasOutputLines(
+                        "import static org.assertj.core.api.Assertions.assertThat;",
+                        "import java.util.List;",
+                        "public class Test {",
+                        "  void int_istrue(int a, int b, String desc) {",
+                        "    assertThat(a).describedAs(desc).isLessThan(b);",
+                        "    assertThat(a).describedAs(desc).isLessThan(b);",
+                        "  }",
+                        "}");
+    }
+}
