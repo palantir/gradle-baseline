9fd50445a007c24cb038c87ea53bfd355b1948e6
Dan Fox <dfox@palantir.com>
2019-09-30 19:45:06 +0100

    AssertjArrayEquals

---
+++ baseline-refaster-rules/src/test/java/com/palantir/baseline/refaster/AssertjArrayEqualsTest.java
@@ -0,0 +1,54 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.refaster;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class AssertjArrayEqualsTest {
+    @Test
+    public void sanity_check() {
+        byte[] bytes = {1, 2, 3};
+        // both of these work, but I think isEqualTo reads a bit more nicely
+        assertThat(bytes).isEqualTo(new byte[]{1, 2, 3});
+        assertThat(bytes).containsExactly(new byte[]{1, 2, 3});
+    }
+
+    @Test
+    public void array_of_bytes() {
+        RefasterTestHelper
+                .forRefactoring(AssertjArrayEquals.class)
+                .withInputLines(
+                        "Test",
+                        "import static org.assertj.core.api.Assertions.assertThat;",
+                        "import java.util.Arrays;",
+                        "public class Test {",
+                        "  void f(byte[] a, byte[] b) {",
+                        "    assertThat(Arrays.equals(a,b)).isTrue();",
+                        "  }",
+                        "}")
+                .hasOutputLines(
+                        "import static org.assertj.core.api.Assertions.assertThat;",
+                        "import java.util.Arrays;",
+                        "public class Test {",
+                        "  void f(byte[] a, byte[] b) {",
+                        "    assertThat(a).isEqualTo(b);",
+                        "  }",
+                        "}");
+    }
+}
