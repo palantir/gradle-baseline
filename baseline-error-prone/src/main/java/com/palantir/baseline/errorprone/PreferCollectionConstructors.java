ecc4ebea48389582b532e42146a413e6bb3298d0
Andreas Lundblad <andreas.lundblad@gmail.com>
2019-10-03 17:07:07 -0700

    Added errorprone checks for Guavas static factory methods.

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferCollectionConstructors.java
@@ -0,0 +1,293 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.fixes.SuggestedFixes;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.code.Types;
+import com.sun.tools.javac.tree.JCTree;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "PreferCollectionConstructors",
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = BugPattern.SeverityLevel.WARNING,
+        summary = "Since Java 7 the standard collection constructors should be used instead of the static factory "
+                + "methods provided by Guava.")
+public final class PreferCollectionConstructors extends BugChecker implements BugChecker.MethodInvocationTreeMatcher {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Matcher<ExpressionTree> NEW_ARRAY_LIST =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Lists")
+                    .named("newArrayList");
+
+    private static final Matcher<ExpressionTree> NEW_LINKED_LIST =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Lists")
+                    .named("newLinkedList");
+
+    private static final Matcher<ExpressionTree> NEW_COPY_ON_WRITE_ARRAY_LIST =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Lists")
+                    .named("newCopyOnWriteArrayList");
+
+    private static final Matcher<ExpressionTree> NEW_ARRAY_LIST_WITH_CAPACITY =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Lists")
+                    .namedAnyOf("newArrayListWithCapacity", "newArrayListWithExpectedSize")
+                    .withParameters("int");
+
+    private static final Matcher<ExpressionTree> NEW_CONCURRENT_MAP =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newConcurrentMap")
+                    .withParameters();
+
+    private static final Matcher<ExpressionTree> NEW_HASH_MAP =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newHashMap");
+
+    private static final Matcher<ExpressionTree> NEW_HASH_MAP_WITH_EXPECTED_SIZE =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newHashMapWithExpectedSize")
+                    .withParameters("int");
+
+    private static final Matcher<ExpressionTree> NEW_TREE_MAP =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newTreeMap");
+
+    private static final Matcher<ExpressionTree> NEW_COPY_ON_WRITE_ARRAY_SET =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Sets")
+                    .named("newCopyOnWriteArraySet");
+
+    private static final Matcher<ExpressionTree> NEW_LINKED_HASH_SET =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Sets")
+                    .named("newLinkedHashSet");
+
+    private static final Matcher<ExpressionTree> NEW_LINKED_HASH_SET_WITH_EXPECTED_SIZE =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Sets")
+                    .named("newLinkedHashSetWithExpectedSize")
+                    .withParameters("int");
+
+    private static final Matcher<ExpressionTree> NEW_TREE_SET =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Sets")
+                    .named("newTreeSet");
+
+    private static final Matcher<ExpressionTree> NEW_HASH_SET =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Sets")
+                    .named("newHashSet");
+
+    private static final Matcher<ExpressionTree> NEW_HASH_SET_WITH_EXPECTED_SIZE =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Sets")
+                    .named("newHashSetWithExpectedSize")
+                    .withParameters("int");
+
+    private static final Matcher<ExpressionTree> NEW_LINKED_HASH_MAP =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newLinkedHashMap");
+
+    private static final Matcher<ExpressionTree> NEW_LINKED_HASH_MAP_WITH_EXPECTED_SIZE =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newLinkedHashMapWithExpectedSize")
+                    .withParameters("int");
+
+    private static final Matcher<ExpressionTree> NEW_ENUM_MAP =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newEnumMap");
+
+    private static final Matcher<ExpressionTree> NEW_IDENTITY_HASH_MAP =
+            MethodMatchers.staticMethod()
+                    .onClass("com.google.common.collect.Maps")
+                    .named("newIdentityHashMap");
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        // Param types used to figure out which overload is called
+        List<Type> paramTypes = ((JCTree.JCExpression) tree.getMethodSelect()).type.asMethodType().argtypes;
+
+        // Argument types used to differentiate between an Iterable and a Collection
+        List<Type> argTypes = ((JCTree.JCMethodInvocation) tree).args
+                .stream().map((JCTree.JCExpression e) -> e.type).collect(Collectors.toList());
+
+        // For convenience
+        ExpressionTree arg = tree.getArguments().size() == 1 ? tree.getArguments().get(0) : null;
+
+        if (NEW_ARRAY_LIST.matches(tree, state)
+                && (paramTypes.isEmpty() || isIterableParamCollectionArg(state, paramTypes, argTypes))) {
+            return buildSingleArgFixSuggestion(tree, state, ArrayList.class, arg);
+        }
+        if (NEW_ARRAY_LIST_WITH_CAPACITY.matches(tree, state)) {
+            return buildSingleArgFixSuggestion(tree, state, ArrayList.class, arg);
+        }
+        if (NEW_LINKED_LIST.matches(tree, state)
+                && (paramTypes.isEmpty() || isIterableParamCollectionArg(state, paramTypes, argTypes))) {
+            return buildSingleArgFixSuggestion(tree, state, LinkedList.class, arg);
+        }
+        if (NEW_COPY_ON_WRITE_ARRAY_LIST.matches(tree, state)
+                && (paramTypes.isEmpty() || isIterableParamCollectionArg(state, paramTypes, argTypes))) {
+            return buildSingleArgFixSuggestion(tree, state, CopyOnWriteArrayList.class, arg);
+        }
+        if (NEW_CONCURRENT_MAP.matches(tree, state)) {
+            return buildSingleArgFixSuggestion(tree, state, ConcurrentHashMap.class, arg);
+        }
+        if (NEW_HASH_MAP.matches(tree, state)
+                && (paramTypes.isEmpty() || checkParamTypes(state, paramTypes, Map.class))) {
+            return buildSingleArgFixSuggestion(tree, state, HashMap.class, arg);
+        }
+        if (NEW_HASH_MAP_WITH_EXPECTED_SIZE.matches(tree, state)) {
+            return buildSingleArgFixSuggestion(tree, state, HashMap.class, arg);
+        }
+        if (NEW_COPY_ON_WRITE_ARRAY_SET.matches(tree, state)
+                && (paramTypes.isEmpty() || isIterableParamCollectionArg(state, paramTypes, argTypes))) {
+            return buildSingleArgFixSuggestion(tree, state, CopyOnWriteArraySet.class, arg);
+        }
+        if (NEW_LINKED_HASH_SET.matches(tree, state)
+                && (paramTypes.isEmpty() || isIterableParamCollectionArg(state, paramTypes, argTypes))) {
+            return buildSingleArgFixSuggestion(tree, state, LinkedHashSet.class, arg);
+        }
+        if (NEW_LINKED_HASH_SET_WITH_EXPECTED_SIZE.matches(tree, state)) {
+            return buildSingleArgFixSuggestion(tree, state, LinkedHashSet.class, arg);
+        }
+        if (NEW_TREE_SET.matches(tree, state)
+                && (paramTypes.isEmpty()
+                        || checkParamTypes(state, paramTypes, Comparator.class)
+                        || isIterableParamCollectionArg(state, paramTypes, argTypes))) {
+            return buildSingleArgFixSuggestion(tree, state, TreeSet.class, arg);
+        }
+        if (NEW_HASH_SET.matches(tree, state)
+                && (paramTypes.isEmpty() || isIterableParamCollectionArg(state, paramTypes, argTypes))) {
+            return buildSingleArgFixSuggestion(tree, state, HashSet.class, arg);
+        }
+        if (NEW_HASH_SET_WITH_EXPECTED_SIZE.matches(tree, state)) {
+            return buildSingleArgFixSuggestion(tree, state, HashSet.class, arg);
+        }
+        if (NEW_TREE_MAP.matches(tree, state)
+                && (paramTypes.isEmpty()
+                        || checkParamTypes(state, paramTypes, SortedMap.class)
+                        || checkParamTypes(state, paramTypes, Comparator.class))) {
+            return buildSingleArgFixSuggestion(tree, state, TreeMap.class, arg);
+        }
+        if (NEW_LINKED_HASH_MAP.matches(tree, state)
+                && (paramTypes.isEmpty() || checkParamTypes(state, paramTypes, Map.class))) {
+            return buildSingleArgFixSuggestion(tree, state, LinkedHashMap.class, arg);
+        }
+        if (NEW_LINKED_HASH_MAP_WITH_EXPECTED_SIZE.matches(tree, state)) {
+            return buildSingleArgFixSuggestion(tree, state, LinkedHashMap.class, arg);
+        }
+        if (NEW_ENUM_MAP.matches(tree, state)
+                && (paramTypes.isEmpty()
+                        || checkParamTypes(state, paramTypes, Class.class)
+                        || checkParamTypes(state, paramTypes, Map.class))) {
+            return buildSingleArgFixSuggestion(tree, state, EnumMap.class, arg);
+        }
+        if (NEW_IDENTITY_HASH_MAP.matches(tree, state)) {
+            return buildSingleArgFixSuggestion(tree, state, IdentityHashMap.class, arg);
+        }
+        return Description.NO_MATCH;
+    }
+
+    private Description buildSingleArgFixSuggestion(
+            MethodInvocationTree tree,
+            VisitorState state,
+            Class<?> collectionClass,
+            ExpressionTree argTree) {
+        SuggestedFix.Builder fixBuilder = SuggestedFix.builder();
+        String collectionType = SuggestedFixes.qualifyType(state, fixBuilder, collectionClass.getName());
+        String typeArgs = tree.getTypeArguments()
+                .stream()
+                .map(state::getSourceForNode)
+                .collect(Collectors.joining(", "));
+        String arg = argTree == null ? "" : state.getSourceForNode(argTree);
+        String replacement = "new " + collectionType + "<" + typeArgs + ">(" + arg + ")";
+        return buildDescription(tree)
+                .setMessage("The factory method call should be replaced with a constructor call.")
+                .addFix(fixBuilder.replace(tree, replacement).build())
+                .build();
+    }
+
+    private boolean isIterableParamCollectionArg(VisitorState state, List<Type> params, List<Type> args) {
+        return checkParamTypes(state, params, Iterable.class)
+                && checkArgTypes(state, args, Collection.class);
+    }
+
+    private boolean checkParamTypes(VisitorState state, List<Type> params, Class<?>... expected) {
+        return checkTypes(state, false, params, expected);
+    }
+
+    private boolean checkArgTypes(VisitorState state, List<Type> args, Class<?>... expected) {
+        return checkTypes(state, true, args, expected);
+    }
+
+    private boolean checkTypes(VisitorState state, boolean subtypes, List<Type> typeList1, Class<?>... typeList2) {
+        Types types = state.getTypes();
+        if (typeList1.size() != typeList2.length) {
+            return false;
+        }
+        for (int i = 0; i < typeList1.size(); i++) {
+            Type t1 = types.erasure(typeList1.get(i));
+            Type t2 = types.erasure(state.getTypeFromString(typeList2[i].getName()));
+            if ((!subtypes && !types.isSameType(t1, t2)) || (subtypes && !types.isSubtype(t1, t2))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/PreferCollectionConstructorsTest.java
@@ -0,0 +1,339 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+@Execution(ExecutionMode.CONCURRENT)
+public final class PreferCollectionConstructorsTest {
+
+    @Test
+    public void testNewArrayListRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newArrayList()",
+                "new ArrayList<>()",
+                "java.util.ArrayList");
+    }
+
+    @Test
+    public void testNewArrayListWithCollectionsRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newArrayList(new java.util.ArrayList<>())",
+                "new ArrayList<>(new java.util.ArrayList<>())",
+                "java.util.ArrayList");
+    }
+
+    @Test
+    public void testNewArrayListWithCapacityRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newArrayListWithCapacity(123)",
+                "new ArrayList<>(123)",
+                "java.util.ArrayList");
+    }
+
+    @Test
+    public void testNewArrayListWithExpectedSizeRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newArrayListWithExpectedSize(123)",
+                "new ArrayList<>(123)",
+                "java.util.ArrayList");
+    }
+
+    @Test
+    public void testNewLinkedListRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newLinkedList()",
+                "new LinkedList<>()",
+                "java.util.LinkedList");
+    }
+
+    @Test
+    public void testNewLinkedListWithCollectionsRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newLinkedList(new java.util.ArrayList<>())",
+                "new LinkedList<>(new java.util.ArrayList<>())",
+                "java.util.LinkedList");
+    }
+
+    @Test
+    public void testNewCopyOnWriteListRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newCopyOnWriteArrayList()",
+                "new CopyOnWriteArrayList<>()",
+                "java.util.concurrent.CopyOnWriteArrayList");
+    }
+
+    @Test
+    public void testNewCopyOnWriteArrayListWithCollectionsRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newCopyOnWriteArrayList(new java.util.ArrayList<>())",
+                "new CopyOnWriteArrayList<>(new java.util.ArrayList<>())",
+                "java.util.concurrent.CopyOnWriteArrayList");
+    }
+
+    @Test
+    public void testNewConcurrentMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newConcurrentMap()",
+                "new ConcurrentHashMap<>()",
+                "java.util.concurrent.ConcurrentHashMap");
+    }
+
+    @Test
+    public void testNewEnumMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newEnumMap(java.util.concurrent.TimeUnit.class)",
+                "new EnumMap<>(java.util.concurrent.TimeUnit.class)",
+                "java.util.EnumMap");
+    }
+
+    @Test
+    public void testNewEnumMapWithMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newEnumMap(new java.util.HashMap<java.util.concurrent.TimeUnit, String>())",
+                "new EnumMap<>(new java.util.HashMap<java.util.concurrent.TimeUnit, String>())",
+                "java.util.EnumMap");
+    }
+
+    @Test
+    public void testNewHashMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newHashMap()",
+                "new HashMap<>()",
+                "java.util.HashMap");
+    }
+
+    @Test
+    public void testNewHashMapWithMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newHashMap(new java.util.HashMap<>())",
+                "new HashMap<>(new java.util.HashMap<>())",
+                "java.util.HashMap");
+    }
+
+    @Test
+    public void testNewHashMapWithExpectedSizeRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newHashMapWithExpectedSize(123)",
+                "new HashMap<>(123)",
+                "java.util.HashMap");
+    }
+
+    @Test
+    public void testNewIdentityHashMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newIdentityHashMap()",
+                "new IdentityHashMap<>()",
+                "java.util.IdentityHashMap");
+    }
+
+    @Test
+    public void testNewLinkedHashMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newLinkedHashMap()",
+                "new LinkedHashMap<>()",
+                "java.util.LinkedHashMap");
+    }
+
+    @Test
+    public void testNewLinkedHashMapWithMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newLinkedHashMap(new java.util.HashMap<>())",
+                "new LinkedHashMap<>(new java.util.HashMap<>())",
+                "java.util.LinkedHashMap");
+    }
+
+    @Test
+    public void testNewLinkedHashMapWithExpectedSizeRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newLinkedHashMapWithExpectedSize(123)",
+                "new LinkedHashMap<>(123)",
+                "java.util.LinkedHashMap");
+    }
+
+    @Test
+    public void testNewTreeMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newTreeMap()",
+                "new TreeMap<>()",
+                "java.util.TreeMap");
+    }
+
+    @Test
+    public void testNewTreeMapWithMapRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newTreeMap(new java.util.TreeMap<>())",
+                "new TreeMap<>(new java.util.TreeMap<>())",
+                "java.util.TreeMap");
+    }
+
+    @Test
+    public void testNewTreeMapWithComparatorRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.newTreeMap(java.util.Comparator.<String>naturalOrder())",
+                "new TreeMap<>(java.util.Comparator.<String>naturalOrder())",
+                "java.util.TreeMap");
+    }
+
+    @Test
+    public void testNewCopyOnWriteArraySetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newCopyOnWriteArraySet()",
+                "new CopyOnWriteArraySet<>()",
+                "java.util.concurrent.CopyOnWriteArraySet");
+    }
+
+    @Test
+    public void testNewCopyOnWriteArraySetWithSetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newCopyOnWriteArraySet(new java.util.HashSet<>())",
+                "new CopyOnWriteArraySet<>(new java.util.HashSet<>())",
+                "java.util.concurrent.CopyOnWriteArraySet");
+    }
+
+    @Test
+    public void testNewHashSetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newHashSet()",
+                "new HashSet<>()",
+                "java.util.HashSet");
+    }
+
+    @Test
+    public void testNewHashSetWithSetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newHashSet(new java.util.HashSet<>())",
+                "new HashSet<>(new java.util.HashSet<>())",
+                "java.util.HashSet");
+    }
+
+    @Test
+    public void testNewHashSetWithExpectedSizeRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newHashSetWithExpectedSize(123)",
+                "new HashSet<>(123)",
+                "java.util.HashSet");
+    }
+
+    @Test
+    public void testNewLinkedHashSetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newLinkedHashSet()",
+                "new LinkedHashSet<>()",
+                "java.util.LinkedHashSet");
+    }
+
+    @Test
+    public void testNewLinkedHashSetWithSetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newLinkedHashSet(new java.util.HashSet<>())",
+                "new LinkedHashSet<>(new java.util.HashSet<>())",
+                "java.util.LinkedHashSet");
+    }
+
+    @Test
+    public void testNewLinkedHashSetWithExpectedSizeRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newLinkedHashSetWithExpectedSize(123)",
+                "new LinkedHashSet<>(123)",
+                "java.util.LinkedHashSet");
+    }
+
+    @Test
+    public void testNewTreeSetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newTreeSet()",
+                "new TreeSet<>()",
+                "java.util.TreeSet");
+    }
+
+    @Test
+    public void testNewTreeSetWithSetRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newTreeSet(new java.util.HashSet<>())",
+                "new TreeSet<>(new java.util.HashSet<>())",
+                "java.util.TreeSet");
+    }
+
+    @Test
+    public void testNewTreeSetWithComparatorRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.newTreeSet(java.util.Comparator.naturalOrder())",
+                "new TreeSet<>(java.util.Comparator.naturalOrder())",
+                "java.util.TreeSet");
+    }
+
+    @Test
+    public void testWithOneTypeArgRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.<String>newHashSet()",
+                "new HashSet<String>()",
+                "java.util.HashSet");
+    }
+
+    @Test
+    public void testWithTwoTypeArgsRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Maps.<String, Integer>newTreeMap()",
+                "new TreeMap<String, Integer>()",
+                "java.util.TreeMap");
+    }
+
+    @Test
+    public void testWithVarargRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Lists.newArrayList(\"a\", \"b\", \"c\")",
+                "Lists.newArrayList(\"a\", \"b\", \"c\")");
+    }
+
+    @Test
+    public void testWithExplicitVarargInvocationRewrite() {
+        testStaticFactoryMethodRewrite(
+                "Sets.<java.util.Set<String>>newHashSet(new java.util.HashSet<String>())",
+                "Sets.<java.util.Set<String>>newHashSet(new java.util.HashSet<String>())");
+    }
+
+    private void testStaticFactoryMethodRewrite(String before, String after, String... addedImports) {
+        List<String> imports = Arrays.asList(
+                "import com.google.common.collect.Iterables;",
+                "import com.google.common.collect.Lists;",
+                "import com.google.common.collect.Maps;",
+                "import com.google.common.collect.Sets;");
+
+        List<String> inputLines = new ArrayList<>(imports);
+        inputLines.add("class Test {{ " + before + "; }}");
+
+        List<String> outputLines = new ArrayList<>(imports);
+        for (String addedImport : addedImports) {
+            outputLines.add("import " + addedImport + ";");
+        }
+        outputLines.add("class Test {{ " + after + "; }}");
+
+        BugCheckerRefactoringTestHelper.newInstance(new PreferCollectionConstructors(), getClass())
+                .addInputLines("Test.java", inputLines.toArray(new String[0]))
+                .addOutputLines("Test.java", outputLines.toArray(new String[0]))
+                .doTest(BugCheckerRefactoringTestHelper.TestMode.TEXT_MATCH);
+    }
+}
