5bbb3669a990aa9a6f14278e46432422518b6704
Andreas Lundblad <andreas.lundblad@gmail.com>
2019-10-05 10:18:57 -0700

    Reduced cyclomatic complexity by introducing a map.

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferCollectionConstructors.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferCollectionConstructors.java
@@ -17,7 +17,8 @@
 package com.palantir.baseline.errorprone;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
@@ -28,7 +29,6 @@ import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.matchers.method.MethodMatchers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
-import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.tree.JCTree.JCExpression;
 import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
@@ -41,6 +41,8 @@ import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
@@ -90,13 +92,13 @@ public final class PreferCollectionConstructors extends BugChecker implements Bu
                     .named("newLinkedList")
                     .withParameters("java.lang.Iterable");
 
-    private static final Matcher<ExpressionTree> NEW_COW_ARRAY_LIST =
+    private static final Matcher<ExpressionTree> NEW_COPY_ON_WRITE_ARRAY_LIST =
             MethodMatchers.staticMethod()
                     .onClass("com.google.common.collect.Lists")
                     .named("newCopyOnWriteArrayList")
                     .withParameters();
 
-    private static final Matcher<ExpressionTree> NEW_COW_ARRAY_LIST_WITH_ITERABLE =
+    private static final Matcher<ExpressionTree> NEW_COPY_ON_WRITE_ARRAY_LIST_WITH_ITERABLE =
             MethodMatchers.staticMethod()
                     .onClass("com.google.common.collect.Lists")
                     .named("newCopyOnWriteArrayList")
@@ -144,13 +146,13 @@ public final class PreferCollectionConstructors extends BugChecker implements Bu
                     .named("newTreeMap")
                     .withParameters("java.util.SortedMap");
 
-    private static final Matcher<ExpressionTree> NEW_COW_ARRAY_SET =
+    private static final Matcher<ExpressionTree> NEW_COPY_ON_WRITE_ARRAY_SET =
             MethodMatchers.staticMethod()
                     .onClass("com.google.common.collect.Sets")
                     .named("newCopyOnWriteArraySet")
                     .withParameters();
 
-    private static final Matcher<ExpressionTree> NEW_COW_ARRAY_SET_WITH_ITERABLE =
+    private static final Matcher<ExpressionTree> NEW_COPY_ON_WRITE_ARRAY_SET_WITH_ITERABLE =
             MethodMatchers.staticMethod()
                     .onClass("com.google.common.collect.Sets")
                     .named("newCopyOnWriteArraySet")
@@ -251,136 +253,93 @@ public final class PreferCollectionConstructors extends BugChecker implements Bu
                     .onClass("com.google.common.collect.Maps")
                     .named("newIdentityHashMap");
 
+    private static final Map<Matcher<ExpressionTree>, Class<?>> classMap =
+            ImmutableMap.<Matcher<ExpressionTree>, Class<?>>builder()
+                    .put(NEW_ARRAY_LIST, ArrayList.class)
+                    .put(NEW_ARRAY_LIST_WITH_ITERABLE, ArrayList.class)
+                    .put(NEW_ARRAY_LIST_WITH_CAPACITY, ArrayList.class)
+                    .put(NEW_LINKED_LIST, LinkedList.class)
+                    .put(NEW_LINKED_LIST_WITH_ITERABLE, LinkedList.class)
+                    .put(NEW_COPY_ON_WRITE_ARRAY_LIST, CopyOnWriteArrayList.class)
+                    .put(NEW_COPY_ON_WRITE_ARRAY_LIST_WITH_ITERABLE, CopyOnWriteArrayList.class)
+                    .put(NEW_CONCURRENT_MAP, ConcurrentHashMap.class)
+                    .put(NEW_HASH_MAP, HashMap.class)
+                    .put(NEW_HASH_MAP_WITH_MAP, HashMap.class)
+                    .put(NEW_HASH_MAP_WITH_EXPECTED_SIZE, HashMap.class)
+                    .put(NEW_COPY_ON_WRITE_ARRAY_SET, CopyOnWriteArraySet.class)
+                    .put(NEW_COPY_ON_WRITE_ARRAY_SET_WITH_ITERABLE, CopyOnWriteArraySet.class)
+                    .put(NEW_LINKED_HASH_SET, LinkedHashSet.class)
+                    .put(NEW_LINKED_HASH_SET_WITH_ITERABLE, LinkedHashSet.class)
+                    .put(NEW_LINKED_HASH_SET_WITH_EXPECTED_SIZE, LinkedHashSet.class)
+                    .put(NEW_TREE_SET, TreeSet.class)
+                    .put(NEW_TREE_SET_WITH_COMPARATOR, TreeSet.class)
+                    .put(NEW_TREE_SET_WITH_ITERABLE, TreeSet.class)
+                    .put(NEW_HASH_SET, HashSet.class)
+                    .put(NEW_HASH_SET_WITH_ITERABLE, HashSet.class)
+                    .put(NEW_HASH_SET_WITH_EXPECTED_SIZE, HashSet.class)
+                    .put(NEW_TREE_MAP, TreeMap.class)
+                    .put(NEW_TREE_MAP_WITH_SORTED_MAP, TreeMap.class)
+                    .put(NEW_TREE_MAP_WITH_COMPARATOR, TreeMap.class)
+                    .put(NEW_LINKED_HASH_MAP, LinkedHashMap.class)
+                    .put(NEW_LINKED_HASH_MAP_WITH_MAP, LinkedHashMap.class)
+                    .put(NEW_LINKED_HASH_MAP_WITH_EXPECTED_SIZE, LinkedHashMap.class)
+                    .put(NEW_ENUM_MAP, EnumMap.class)
+                    .put(NEW_ENUM_MAP_WITH_CLASS, EnumMap.class)
+                    .put(NEW_ENUM_MAP_WITH_MAP, EnumMap.class)
+                    .put(NEW_IDENTITY_HASH_MAP, IdentityHashMap.class)
+                    .build();
+
+    private static final Set<Matcher<ExpressionTree>> requiresCollectionArg = ImmutableSet.of(
+            NEW_ARRAY_LIST_WITH_ITERABLE,
+            NEW_LINKED_LIST_WITH_ITERABLE,
+            NEW_COPY_ON_WRITE_ARRAY_LIST_WITH_ITERABLE,
+            NEW_COPY_ON_WRITE_ARRAY_SET_WITH_ITERABLE,
+            NEW_LINKED_HASH_SET_WITH_ITERABLE,
+            NEW_TREE_SET_WITH_ITERABLE,
+            NEW_HASH_SET_WITH_ITERABLE);
+
     @Override
     public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
-        // Argument type used to differentiate between an Iterable and a Collection
-        List<JCExpression> args = ((JCMethodInvocation) tree).args;
-        Type firstArgType = args.isEmpty() ? null : args.get(0).type;
 
-        // For convenience
-        ExpressionTree firstArg = Iterables.getFirst(tree.getArguments(), null);
+        Class<?> collectionClass = findCollectionClassToUse(state, tree);
+        if (collectionClass == null) {
+            return Description.NO_MATCH;
+        }
 
-        if (NEW_ARRAY_LIST.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, ArrayList.class, firstArg);
-        }
-        if (NEW_ARRAY_LIST_WITH_ITERABLE.matches(tree, state) && isCollectionArg(state, firstArgType)) {
-            return buildConstructorFixSuggestion(tree, state, ArrayList.class, firstArg);
-        }
-        if (NEW_ARRAY_LIST_WITH_CAPACITY.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, ArrayList.class, firstArg);
-        }
-        if (NEW_LINKED_LIST.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedList.class, firstArg);
-        }
-        if (NEW_LINKED_LIST_WITH_ITERABLE.matches(tree, state) && isCollectionArg(state, firstArgType)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedList.class, firstArg);
-        }
-        if (NEW_COW_ARRAY_LIST.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, CopyOnWriteArrayList.class, firstArg);
-        }
-        if (NEW_COW_ARRAY_LIST_WITH_ITERABLE.matches(tree, state) && isCollectionArg(state, firstArgType)) {
-            return buildConstructorFixSuggestion(tree, state, CopyOnWriteArrayList.class, firstArg);
-        }
-        if (NEW_CONCURRENT_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, ConcurrentHashMap.class, firstArg);
-        }
-        if (NEW_HASH_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, HashMap.class, firstArg);
-        }
-        if (NEW_HASH_MAP_WITH_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, HashMap.class, firstArg);
-        }
-        if (NEW_HASH_MAP_WITH_EXPECTED_SIZE.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, HashMap.class, firstArg);
-        }
-        if (NEW_COW_ARRAY_SET.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, CopyOnWriteArraySet.class, firstArg);
-        }
-        if (NEW_COW_ARRAY_SET_WITH_ITERABLE.matches(tree, state) && isCollectionArg(state, firstArgType)) {
-            return buildConstructorFixSuggestion(tree, state, CopyOnWriteArraySet.class, firstArg);
-        }
-        if (NEW_LINKED_HASH_SET.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedHashSet.class, firstArg);
-        }
-        if (NEW_LINKED_HASH_SET_WITH_ITERABLE.matches(tree, state) && isCollectionArg(state, firstArgType)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedHashSet.class, firstArg);
-        }
-        if (NEW_LINKED_HASH_SET_WITH_EXPECTED_SIZE.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedHashSet.class, firstArg);
-        }
-        if (NEW_TREE_SET.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, TreeSet.class, firstArg);
-        }
-        if (NEW_TREE_SET_WITH_COMPARATOR.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, TreeSet.class, firstArg);
-        }
-        if (NEW_TREE_SET_WITH_ITERABLE.matches(tree, state) && isCollectionArg(state, firstArgType)) {
-            return buildConstructorFixSuggestion(tree, state, TreeSet.class, firstArg);
-        }
-        if (NEW_HASH_SET.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, HashSet.class, firstArg);
-        }
-        if (NEW_HASH_SET_WITH_ITERABLE.matches(tree, state) && isCollectionArg(state, firstArgType)) {
-            return buildConstructorFixSuggestion(tree, state, HashSet.class, firstArg);
-        }
-        if (NEW_HASH_SET_WITH_EXPECTED_SIZE.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, HashSet.class, firstArg);
-        }
-        if (NEW_TREE_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, TreeMap.class, firstArg);
-        }
-        if (NEW_TREE_MAP_WITH_SORTED_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, TreeMap.class, firstArg);
-        }
-        if (NEW_TREE_MAP_WITH_COMPARATOR.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, TreeMap.class, firstArg);
-        }
-        if (NEW_LINKED_HASH_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedHashMap.class, firstArg);
-        }
-        if (NEW_LINKED_HASH_MAP_WITH_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedHashMap.class, firstArg);
-        }
-        if (NEW_LINKED_HASH_MAP_WITH_EXPECTED_SIZE.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, LinkedHashMap.class, firstArg);
-        }
-        if (NEW_ENUM_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, EnumMap.class, firstArg);
-        }
-        if (NEW_ENUM_MAP_WITH_CLASS.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, EnumMap.class, firstArg);
-        }
-        if (NEW_ENUM_MAP_WITH_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, EnumMap.class, firstArg);
-        }
-        if (NEW_IDENTITY_HASH_MAP.matches(tree, state)) {
-            return buildConstructorFixSuggestion(tree, state, IdentityHashMap.class, firstArg);
-        }
-        return Description.NO_MATCH;
-    }
-
-    private Description buildConstructorFixSuggestion(
-            MethodInvocationTree tree,
-            VisitorState state,
-            Class<?> collectionClass,
-            ExpressionTree argTree) {
         SuggestedFix.Builder fixBuilder = SuggestedFix.builder();
         String collectionType = SuggestedFixes.qualifyType(state, fixBuilder, collectionClass.getName());
         String typeArgs = tree.getTypeArguments()
                 .stream()
                 .map(state::getSourceForNode)
                 .collect(Collectors.joining(", "));
-        String arg = argTree == null ? "" : state.getSourceForNode(argTree);
+        String arg = tree.getArguments().isEmpty() ? "" : state.getSourceForNode(tree.getArguments().get(0));
         String replacement = "new " + collectionType + "<" + typeArgs + ">(" + arg + ")";
         return buildDescription(tree)
-                .setMessage("The factory method call should be replaced with a constructor call. " +
-                        "See https://git.io/JeCT6 for more information.")
+                .setMessage("The factory method call should be replaced with a constructor call. "
+                        + "See https://git.io/JeCT6 for more information.")
                 .addFix(fixBuilder.replace(tree, replacement).build())
                 .build();
     }
 
-    private boolean isCollectionArg(VisitorState state, Type type) {
+    private Class<?> findCollectionClassToUse(VisitorState state, ExpressionTree tree) {
+        boolean firstArgIsCollection = isFirstArgCollection(state, tree);
+        for (Map.Entry<Matcher<ExpressionTree>, Class<?>> entry : classMap.entrySet()) {
+            Matcher<ExpressionTree> matcher = entry.getKey();
+            if (matcher.matches(tree, state) && (!requiresCollectionArg.contains(matcher) || firstArgIsCollection)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    private boolean isFirstArgCollection(VisitorState state, ExpressionTree tree) {
+        List<JCExpression> args = ((JCMethodInvocation) tree).args;
+        if (args.isEmpty()) {
+            return false;
+        }
         Types types = state.getTypes();
-        return types.isSubtype(types.erasure(type), types.erasure(state.getTypeFromString("java.util.Collection")));
+        return types.isSubtype(
+                types.erasure(args.get(0).type),
+                types.erasure(state.getTypeFromString("java.util.Collection")));
     }
 }
