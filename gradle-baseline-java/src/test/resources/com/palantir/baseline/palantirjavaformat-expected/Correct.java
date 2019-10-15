bf700335663880d6893c1c7d88a122e7b16571af
Dan Fox <dfox@palantir.com>
2019-10-15 14:47:16 +0100

    Update p-j-f tests

--- gradle-baseline-java/src/test/resources/com/palantir/baseline/palantirjavaformat-expected/Correct.java
+++ gradle-baseline-java/src/test/resources/com/palantir/baseline/palantirjavaformat-expected/Correct.java
@@ -4,10 +4,8 @@ public class Test {
     void test() {
         int x = 1;
         System.out.println("Hello");
-        Optional.of("hello")
-                .orElseGet(
-                        () -> {
-                            return "Hello World";
-                        });
+        Optional.of("hello").orElseGet(() -> {
+            return "Hello World";
+        });
     }
 }
