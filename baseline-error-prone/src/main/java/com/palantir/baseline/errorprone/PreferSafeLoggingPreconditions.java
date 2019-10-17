bc7a2bc30c92babcf35edcb9125b3f4b9fa56349
Dan Sanduleac <dsanduleac@palantir.com>
2019-10-17 16:49:20 +0100

    autofix strings for already changed files

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferSafeLoggingPreconditions.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferSafeLoggingPreconditions.java
@@ -43,8 +43,9 @@ import java.util.regex.Pattern;
         providesFix = BugPattern.ProvidesFix.REQUIRES_HUMAN_ATTENTION,
         severity = BugPattern.SeverityLevel.WARNING,
         summary =
-                "Precondition and similar checks with a constant message and no parameters should use equivalent "
-                        + "checks from com.palantir.logsafe.Preconditions for standardization as functionality is the same.")
+                "Precondition and similar checks with a constant message and no parameters should use equivalent"
+                        + " checks from com.palantir.logsafe.Preconditions for standardization as functionality is the"
+                        + " same.")
 public final class PreferSafeLoggingPreconditions extends BugChecker implements BugChecker.MethodInvocationTreeMatcher {
 
     private static final long serialVersionUID = 1L;
