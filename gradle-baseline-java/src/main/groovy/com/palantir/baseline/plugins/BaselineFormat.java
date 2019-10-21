374fbda71cdf71fdf260d1c2e7fea328ef5a2138
Callum Rogers <callumr@palantir.com>
2019-10-21 16:10:22 +0100

    Format diff task

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineFormat.java
@@ -18,20 +18,25 @@ package com.palantir.baseline.plugins;
 
 import com.diffplug.gradle.spotless.SpotlessExtension;
 import com.diffplug.spotless.FormatterFunc;
+import com.palantir.javaformat.java.FormatDiffCli;
 import com.palantir.javaformat.java.Formatter;
 import com.palantir.javaformat.java.JavaFormatterOptions;
 import com.palantir.javaformat.java.JavaFormatterOptions.Style;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.IOException;
+import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskProvider;
 import org.gradle.api.tasks.compile.JavaCompile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 class BaselineFormat extends AbstractBaselinePlugin {
 
@@ -45,6 +50,10 @@ class BaselineFormat extends AbstractBaselinePlugin {
         this.project = project;
 
         project.getPluginManager().withPlugin("java", plugin -> {
+            if (palantirJavaFormatterEnabled(project)) {
+                project.getTasks().register("formatDiff", FormatDiff.class);
+            }
+
             project.getPluginManager().apply("com.diffplug.gradle.spotless");
             Path eclipseXml = eclipseConfigFile(project);
 
@@ -105,6 +114,13 @@ class BaselineFormat extends AbstractBaselinePlugin {
                 });
             });
         });
+    }
+
+    public static class FormatDiff extends DefaultTask {
+        @TaskAction
+        public final void formatDiff() throws IOException, InterruptedException {
+            FormatDiffCli.formatDiff(getProject().getProjectDir().toPath());
+        }
     }
 
     static boolean eclipseFormattingEnabled(Project project) {
