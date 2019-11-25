300435db6ff2d2d0a99dd2eb2c68497cc99cdbd5
Carter Kozak <ckozak@ckozak.net>
2019-11-25 10:54:00 -0500

    Replace refaster `StreamEmpty` with error prone `StreamOfEmpty`
    Error prone allows us to provide more precise fixes with less
    risk of patterns being applied to unexpected blocks of code.

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/StreamOfEmpty.java
@@ -0,0 +1,52 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import java.util.stream.Stream;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "StreamOfEmpty",
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = BugPattern.SeverityLevel.SUGGESTION,
+        summary = "Stream.of() should be replaced with Stream.empty() to avoid unnecessary varargs allocation.")
+public final class StreamOfEmpty extends BugChecker implements BugChecker.MethodInvocationTreeMatcher {
+
+    private static final Matcher<ExpressionTree> MATCHER = MethodMatchers.staticMethod()
+            .onClass(Stream.class.getName())
+            .withSignature("<T>of(T...)");
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        if (MATCHER.matches(tree, state) && tree.getArguments().isEmpty()) {
+            return buildDescription(tree)
+                    .addFix(MoreSuggestedFixes.renameInvocationRetainingTypeArguments(tree, "empty", state))
+                    .build();
+        }
+        return Description.NO_MATCH;
+    }
+}
