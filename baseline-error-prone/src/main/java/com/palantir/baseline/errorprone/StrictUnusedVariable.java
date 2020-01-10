b3049c67e2a5c61efc62011478242bb3d0dbe64c
Carter Kozak <ckozak@ckozak.net>
2020-01-10 09:56:02 -0500

    Fix #1159: StrictUnusedVariable respects UnusedVariable suppression

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/StrictUnusedVariable.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/StrictUnusedVariable.java
@@ -106,7 +106,7 @@ import javax.lang.model.element.Name;
 @AutoService(BugChecker.class)
 @BugPattern(
         name = "StrictUnusedVariable",
-        altNames = {"unused", "StrictUnusedVariable"},
+        altNames = {"unused", "UnusedVariable"},
         link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
         linkType = BugPattern.LinkType.CUSTOM,
         summary = "Unused.",
