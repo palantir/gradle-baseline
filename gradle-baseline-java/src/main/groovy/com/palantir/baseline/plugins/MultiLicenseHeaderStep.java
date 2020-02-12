c151b9929129bc76c67e13b908c60dfa59c91b1c
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2020-02-12 13:24:41 +0000

    Separate additional copyright (#1232)

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/MultiLicenseHeaderStep.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/MultiLicenseHeaderStep.java
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */
+
+/*
  * Original copyright:
  *
  * Copyright 2016 DiffPlug
