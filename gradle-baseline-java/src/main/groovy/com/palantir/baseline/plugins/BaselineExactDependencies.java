55c7de5004e9ca2b83dffc74506f898d25a91b41
Dan Sanduleac <dsanduleac@palantir.com>
2020-02-26 12:57:06 +0000

    Revert "Don't resolve compileOnly / annotationProcessor, instead look at runtimeClasspath"
    This reverts commit 02553f86

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineExactDependencies.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineExactDependencies.java
@@ -63,13 +63,19 @@ public final class BaselineExactDependencies implements Plugin<Project> {
                     .getPlugin(JavaPluginConvention.class)
                     .getSourceSets()
                     .getByName(SourceSet.MAIN_SOURCE_SET_NAME);
-            Configuration runtimeClasspath =
-                    project.getConfigurations().getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME);
+            Configuration compileClasspath =
+                    project.getConfigurations().getByName(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME);
+            Configuration compileOnlyClasspath =
+                    project.getConfigurations().getByName(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME);
+            Configuration annotationProcessorClasspath =
+                    project.getConfigurations().getByName(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME);
 
             project.getTasks().create("checkUnusedDependencies", CheckUnusedDependenciesTask.class, task -> {
                 task.dependsOn(JavaPlugin.CLASSES_TASK_NAME);
                 task.setSourceClasses(mainSourceSet.getOutput().getClassesDirs());
-                task.dependenciesConfiguration(runtimeClasspath);
+                task.dependenciesConfiguration(compileClasspath);
+                task.sourceOnlyConfiguration(compileOnlyClasspath);
+                task.sourceOnlyConfiguration(annotationProcessorClasspath);
 
                 // this is liberally applied to ease the Java8 -> 11 transition
                 task.ignore("javax.annotation", "javax.annotation-api");
@@ -78,7 +84,7 @@ public final class BaselineExactDependencies implements Plugin<Project> {
             project.getTasks().create("checkImplicitDependencies", CheckImplicitDependenciesTask.class, task -> {
                 task.dependsOn(JavaPlugin.CLASSES_TASK_NAME);
                 task.setSourceClasses(mainSourceSet.getOutput().getClassesDirs());
-                task.dependenciesConfiguration(runtimeClasspath);
+                task.dependenciesConfiguration(compileClasspath);
 
                 task.ignore("org.slf4j", "slf4j-api");
             });
