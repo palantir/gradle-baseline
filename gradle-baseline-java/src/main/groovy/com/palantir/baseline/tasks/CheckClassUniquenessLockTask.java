316168e559c54afb47a4ca5a983f55a77207af39
Callum Rogers <callumr@palantir.com>
2020-03-03 18:13:56 +0000

    Fix imports

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/tasks/CheckClassUniquenessLockTask.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/tasks/CheckClassUniquenessLockTask.java
@@ -18,7 +18,12 @@ package com.palantir.baseline.tasks;
 
 import com.google.common.collect.ImmutableList;
 import java.io.File;
-import java.util.*;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.gradle.api.DefaultTask;
