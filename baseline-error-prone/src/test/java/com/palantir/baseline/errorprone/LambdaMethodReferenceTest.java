484423e70b5532ce36c0bfe9046d7fe745f877ae
Carter Kozak <ckozak@ckozak.net>
2020-05-28 16:48:57 -0400

    Fix for method references with type parameters
    `Optional<String>::toString` does not compile

--- baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/LambdaMethodReferenceTest.java
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/LambdaMethodReferenceTest.java
@@ -154,6 +154,28 @@ public class LambdaMethodReferenceTest {
     }
 
     @Test
+    void testAutoFix_specificInstanceMethod_withTypeParameters() {
+        refactoringValidator
+                .addInputLines(
+                        "Test.java",
+                        "import " + Optional.class.getName() + ';',
+                        "class Test {",
+                        "  public Optional<String> foo(Optional<Optional<Test>> optional) {",
+                        "    return optional.map(v -> v.toString());",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + Optional.class.getName() + ';',
+                        "class Test {",
+                        "  public Optional<String> foo(Optional<Optional<Test>> optional) {",
+                        "    return optional.map(Optional::toString);",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
     public void testNegative_block_localMethod() {
         compilationHelper
                 .addSourceLines(
