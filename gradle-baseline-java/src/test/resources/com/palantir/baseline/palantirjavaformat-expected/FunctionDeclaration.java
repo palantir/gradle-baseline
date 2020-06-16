326fd011ce47510ac076e97eb49e534efcc363bb
Erik Hesselink <ehesselink@palantir.com>
2020-06-16 13:49:37 +0200

    Don't insert breaks in function declarations.

---
+++ gradle-baseline-java/src/test/resources/com/palantir/baseline/palantirjavaformat-expected/FunctionDeclaration.java
@@ -0,0 +1,9 @@
+package test;
+
+class FunctionDeclaration {
+
+    public void onMessage(
+            UnifiedSearchRequestMessage message, EventContext<DeprecatedUnifiedSearchResponse> eventContext) {
+        // The body
+    }
+}
