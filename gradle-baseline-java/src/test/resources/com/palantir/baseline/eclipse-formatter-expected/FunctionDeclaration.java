c4fd4b2d8018e1b7ad1e15a63322e03f07293a2b
Erik Hesselink <hesselink@gmail.com>
2020-06-16 20:42:35 +0200

    Don't wrap method declaration (#1416)
    Wrap method declarations on parameters, not on modifiers and return type.

---
+++ gradle-baseline-java/src/test/resources/com/palantir/baseline/eclipse-formatter-expected/FunctionDeclaration.java
@@ -0,0 +1,11 @@
+package test;
+
+class FunctionDeclaration {
+
+    public void onMessage(
+            UnifiedSearchRequestMessage message,
+            EventContext<DeprecatedUnifiedSearchResponse> eventContext) {
+        // The body
+    }
+
+}
