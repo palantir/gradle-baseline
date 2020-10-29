7ccfc8dc60710b54e88d75df6f9a4de44aa887a9
Willi Raschkowski <willi@raschkowski.com>
2020-10-29 13:39:35 +0000

    Always set '-target:jvm-8'

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineScalastyle.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineScalastyle.java
@@ -16,6 +16,7 @@
 
 package com.palantir.baseline.plugins;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import groovy.util.Node;
 import groovy.xml.QName;
@@ -29,14 +30,20 @@ import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.ScalaSourceSet;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskCollection;
+import org.gradle.api.tasks.scala.ScalaCompile;
 import org.gradle.plugins.ide.idea.model.IdeaModel;
 
 public final class BaselineScalastyle extends AbstractBaselinePlugin {
+    private static final String SCALA_TARGET_VERSION = "jvm-8";
+
     @Override
     public void apply(Project project) {
         this.project = project;
         project.getPluginManager().withPlugin("scala", plugin -> {
             JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
+            project.getTasks().withType(ScalaCompile.class).configureEach(scalaCompile -> scalaCompile
+                    .getScalaCompileOptions()
+                    .setAdditionalParameters(ImmutableList.of("-target:" + SCALA_TARGET_VERSION)));
             project.getRootProject().getPluginManager().withPlugin("idea", ideaPlugin -> project.getRootProject()
                     .getExtensions()
                     .configure(
@@ -46,8 +53,7 @@ public final class BaselineScalastyle extends AbstractBaselinePlugin {
                                     javaConvention
                                             .getSourceSets()
                                             .named(SourceSet.MAIN_SOURCE_SET_NAME)
-                                            .get(),
-                                    javaConvention.getTargetCompatibility().toString())));
+                                            .get())));
             project.getPluginManager().apply(ScalaStylePlugin.class);
             TaskCollection<ScalaStyleTask> scalaStyleTasks = project.getTasks().withType(ScalaStyleTask.class);
             scalaStyleTasks.configureEach(scalaStyleTask -> {
@@ -66,7 +72,7 @@ public final class BaselineScalastyle extends AbstractBaselinePlugin {
         });
     }
 
-    private void configureIdeaPlugin(IdeaModel ideaModel, SourceSet mainSourceSet, String javaVersion) {
+    private void configureIdeaPlugin(IdeaModel ideaModel, SourceSet mainSourceSet) {
         Convention scalaConvention = (Convention) InvokerHelper.getProperty(mainSourceSet, "convention");
         ScalaSourceSet scalaSourceSet = scalaConvention.getPlugin(ScalaSourceSet.class);
         // If scala source directory doesn't contain java files use "JavaThenScala" compilation mode
@@ -77,6 +83,8 @@ public final class BaselineScalastyle extends AbstractBaselinePlugin {
                 ? "JavaThenScala"
                 : "Mixed";
         ideaModel.getProject().getIpr().withXml(xmlProvider -> {
+            // configure target jvm mode
+            String targetJvmVersion = "-target:" + SCALA_TARGET_VERSION;
             Node rootNode = xmlProvider.asNode();
             Node scalaCompilerConf = (Node) rootNode.getAt(new QName("component")).stream()
                     .filter(o -> ((Node) o).attributes().get("name").equals("ScalaCompilerConfiguration"))
@@ -90,6 +98,14 @@ public final class BaselineScalastyle extends AbstractBaselinePlugin {
                     .orElseGet(() -> scalaCompilerConf.appendNode("option"));
             compilerOrder.attributes().put("name", "compileOrder");
             compilerOrder.attributes().put("value", compilerMode);
+            Node parametersNode = (Node) scalaCompilerConf.getAt(new QName("parameters")).stream()
+                    .findFirst()
+                    .orElseGet(() -> scalaCompilerConf.appendNode("parameters"));
+            Node parameter = (Node) parametersNode.getAt(new QName("parameter")).stream()
+                    .filter(o -> ((Node) o).attributes().get("value").equals(targetJvmVersion))
+                    .findFirst()
+                    .orElseGet(() -> parametersNode.appendNode("parameter"));
+            parameter.attributes().put("value", targetJvmVersion);
         });
     }
 }
--- gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineScalastyleTest.groovy
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/BaselineScalastyleTest.groovy
@@ -21,6 +21,7 @@ import org.github.ngbinh.scalastyle.ScalaStylePlugin
 import org.github.ngbinh.scalastyle.ScalaStyleTask
 import org.gradle.api.Project
 import org.gradle.api.XmlProvider
+import org.gradle.api.tasks.scala.ScalaCompile
 import org.gradle.plugins.ide.idea.model.IdeaModel
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
@@ -65,6 +66,14 @@ final class BaselineScalastyleTest extends Specification {
         def tasks = project.tasks.withType(ScalaStyleTask.class)
         for (ScalaStyleTask task : tasks) {
             assert task.getSource().getFiles().contains(file)
+        }
+    }
+
+    def configuresTargetJvmVersion() {
+        expect:
+        def tasks = project.tasks.withType(ScalaCompile.class)
+        for (ScalaCompile task : tasks) {
+            assert task.getScalaCompileOptions().getAdditionalParameters().contains("-target:jvm-8")
         }
     }
 
