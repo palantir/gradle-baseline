39de5a3d02681eb8bd2a5d1959db722dc10360b7
Patrick Koenig <pkoenig10@gmail.com>
2020-11-03 15:37:43 -0800

    Add error-prone check for system time zone usage

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/JavaTimeSystemDefaultTimeZone.java
@@ -0,0 +1,71 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "JavaTimeSystemDefaultTimeZone",
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = BugPattern.SeverityLevel.WARNING,
+        summary = "The system default time zone should not be used, since the behavior is system dependent. "
+                + "Instead, UTC should always be used.")
+public final class JavaTimeSystemDefaultTimeZone extends BugChecker implements BugChecker.MethodInvocationTreeMatcher {
+
+    private static final Matcher<ExpressionTree> CLOCK_SYSTEM_DEFAULT_ZONE_MATCHER = Matchers.staticMethod()
+            .onClass("java.time.Clock")
+            .named("systemDefaultZone")
+            .withParameters();
+    private static final Matcher<ExpressionTree> ZONE_ID_SYSTEM_DEFAULT_MATCHER = Matchers.staticMethod()
+            .onClass("java.time.ZoneId")
+            .named("systemDefault")
+            .withParameters();
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        if (CLOCK_SYSTEM_DEFAULT_ZONE_MATCHER.matches(tree, state)) {
+            return buildDescription(tree)
+                    .addFix(SuggestedFix.builder()
+                            .replace(tree, "Clock.systemUTC()")
+                            .addImport("java.time.Clock")
+                            .build())
+                    .build();
+        }
+
+        if (ZONE_ID_SYSTEM_DEFAULT_MATCHER.matches(tree, state)) {
+            return buildDescription(tree)
+                    .addFix(SuggestedFix.builder()
+                            .replace(tree, "ZoneOffset.UTC")
+                            .addImport("java.time.ZoneOffset")
+                            .build())
+                    .build();
+        }
+
+        return Description.NO_MATCH;
+    }
+}
---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/JavaTimeSystemDefaultTimeZoneTest.java
@@ -0,0 +1,68 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.baseline.errorprone;
+
+import org.junit.jupiter.api.Test;
+
+final class JavaTimeSystemDefaultTimeZoneTest {
+
+    @Test
+    void clockSystemDefaultZone() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import java.time.Clock;",
+                        "class Test {",
+                        "  static void f() {",
+                        "    Clock clock = Clock.systemDefaultZone();",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import java.time.Clock;",
+                        "class Test {",
+                        "  static void f() {",
+                        "    Clock clock = Clock.systemUTC();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    void zoneIdSystemDefault() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import java.time.ZoneId;",
+                        "class Test {",
+                        "  static void f() {",
+                        "    ZoneId zoneId = ZoneId.systemDefault();",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import java.time.ZoneId;",
+                        "import java.time.ZoneOffset;",
+                        "class Test {",
+                        "  static void f() {",
+                        "    ZoneId zoneId = ZoneOffset.UTC;",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    private RefactoringValidator fix() {
+        return RefactoringValidator.of(new JavaTimeSystemDefaultTimeZone(), getClass());
+    }
+}
