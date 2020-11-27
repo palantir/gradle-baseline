d06d3c5cf6bb06047b9c30ed8ee2746109c273d2
Dan Fox <dfox@palantir.com>
2020-11-27 15:53:55 +0000

    New 'checkExplicitSourceCompatibility' task

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineReproducibility.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineReproducibility.java
@@ -16,10 +16,17 @@
 
 package com.palantir.baseline.plugins;
 
+import com.palantir.baseline.tasks.CheckExplicitSourceCompatibilityTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.file.DuplicatesStrategy;
+import org.gradle.api.plugins.JavaBasePlugin;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.TaskProvider;
 import org.gradle.api.tasks.bundling.AbstractArchiveTask;
+import org.gradle.language.base.plugins.LifecycleBasePlugin;
+import org.gradle.util.GradleVersion;
 
 /** Sensible defaults so that all Jar, Tar, Zip tasks can be deterministically reproduced. */
 public final class BaselineReproducibility implements Plugin<Project> {
@@ -38,6 +45,33 @@ public final class BaselineReproducibility implements Plugin<Project> {
                             "Please remove the 'nebula.info' plugin from {} as it breaks "
                                     + "reproducibility of jars by adding a 'Build-Date' entry to the MANIFEST.MF",
                             project);
+        });
+
+        project.getPlugins().withType(JavaBasePlugin.class, _plugin -> {
+            JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
+
+            // just being a bit defensive because we need to cast to an internal gradle class... don't wanna block
+            // upgrades
+            String clazz = javaConvention.getClass().getCanonicalName();
+            String expected = "org.gradle.api.plugins.internal.DefaultJavaPluginConvention";
+            if (!clazz.equals(expected)) {
+                project.getLogger()
+                        .error(
+                                "BaselineReproducibility unable to check sourceCompatibility - please report this to "
+                                        + "https://github.com/palantir/gradle-baseline/issues and mention the current"
+                                        + " Gradle version ({}). Expected '{}' found '{}'",
+                                GradleVersion.current(),
+                                expected,
+                                clazz);
+                return;
+            }
+
+            TaskProvider<? extends Task> checkExplicitSourceCompatibility = project.getTasks()
+                    .register("checkExplicitSourceCompatibility", CheckExplicitSourceCompatibilityTask.class);
+
+            project.getTasks().named(LifecycleBasePlugin.CHECK_TASK_NAME).configure(check -> {
+                check.dependsOn(checkExplicitSourceCompatibility);
+            });
         });
     }
 }
