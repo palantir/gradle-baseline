56aba7826b340e364ec57c1eb5241b3b4327ef6c
Fabian Windheuser <fwindheuser@palantir.com>
2021-07-16 16:26:22 +0200

    Enforce 6.7 as minimum Gradle version

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/Baseline.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/Baseline.java
@@ -16,14 +16,23 @@
 
 package com.palantir.baseline.plugins;
 
+import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.util.GradleVersion;
 
 /** A Plugin that configures a project with all Baseline settings. */
 public final class Baseline implements Plugin<Project> {
+    public static final GradleVersion MIN_GRADLE_VERSION = GradleVersion.version("6.7");
 
     @Override
     public void apply(Project project) {
+        if (GradleVersion.current().compareTo(MIN_GRADLE_VERSION) >= 0) {
+            throw new GradleException(String.format(
+                    "The minimum supported Gradle version is version %s but got version %s",
+                    MIN_GRADLE_VERSION, GradleVersion.current()));
+        }
+
         Project rootProject = project.getRootProject();
         if (!project.equals(rootProject)) {
             project.getLogger()
--- gradle-baseline-java/src/test/groovy/com/palantir/baseline/GradleTestVersions.java
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/GradleTestVersions.java
@@ -17,9 +17,11 @@
 package com.palantir.baseline;
 
 import com.google.common.collect.ImmutableList;
+import com.palantir.baseline.plugins.Baseline;
 
 public final class GradleTestVersions {
-    public static final ImmutableList<String> VERSIONS = ImmutableList.of("6.9", "7.1.1");
+    public static final ImmutableList<String> VERSIONS =
+            ImmutableList.of(Baseline.MIN_GRADLE_VERSION.getVersion(), "7.1.1");
 
     private GradleTestVersions() {}
 }
