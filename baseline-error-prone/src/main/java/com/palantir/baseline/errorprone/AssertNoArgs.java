29984658832aa34a59442f40217fdb1b25996788
Patrick Koenig <pkoenig10@gmail.com>
2021-11-09 06:21:46 -0800

    Add error-prone check to prefer hasNoArgs

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/AssertNoArgs.java
@@ -0,0 +1,59 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.fixes.SuggestedFixes;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "AssertNoArgs",
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = BugPattern.SeverityLevel.WARNING,
+        summary = "Prefer hasNoArgs when asserting that an exception has no args.")
+public final class AssertNoArgs extends BugChecker implements BugChecker.MethodInvocationTreeMatcher {
+
+    private static final Matcher<ExpressionTree> MATCHER = Matchers.instanceMethod()
+            .onExactClassAny(
+                    "com.palantir.conjure.java.api.testing.ServiceExceptionAssert",
+                    "com.palantir.logsafe.testing.LoggableExceptionAssert")
+            .namedAnyOf("containsArgs", "hasArgs", "hasExactlyArgs");
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        if (!MATCHER.matches(tree, state)) {
+            return Description.NO_MATCH;
+        }
+
+        if (!tree.getArguments().isEmpty()) {
+            return Description.NO_MATCH;
+        }
+
+        return buildDescription(tree)
+                .addFix(SuggestedFixes.renameMethodInvocation(tree, "hasNoArgs", state))
+                .build();
+    }
+}
