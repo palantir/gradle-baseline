c589fd703e5683f47ab30b16063e68459de85c6b
rzpt <@>
2022-03-28 12:09:55 -0700

    move logic to PreferSafeLoggingPreconditions

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineErrorProneExtension.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineErrorProneExtension.java
@@ -53,7 +53,6 @@ public class BaselineErrorProneExtension {
             "ObjectsHashCodeUnnecessaryVarargs",
             "OptionalFlatMapOfNullable",
             "OptionalOrElseMethodInvocation",
-            "PreconditionsArgument",
             "PreferBuiltInConcurrentKeySet",
             "PreferCollectionConstructors",
             "PreferCollectionTransform",
