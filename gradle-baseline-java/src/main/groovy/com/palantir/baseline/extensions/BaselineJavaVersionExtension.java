40e7ad723f4d2cef88dd0fae431aa8a83c36ffcd
Callum Rogers <callumr@palantir.com>
2022-04-04 14:04:32 +0100

    Add jvm versions to extensions

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineJavaVersionExtension.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineJavaVersionExtension.java
@@ -16,8 +16,13 @@
 
 package com.palantir.baseline.extensions;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.inject.Inject;
 import org.gradle.api.Project;
+import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 
@@ -26,19 +31,33 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion;
  * target and runtime java versions used for a single project.
  */
 public class BaselineJavaVersionExtension {
+    private static final Pattern ZULU_VERSION_PATTERN = Pattern.compile("ZULU_([\\d.]+_VERSION)");
+    private static final Pattern JAVA_VERSION_PATTERN = Pattern.compile("JAVA_([\\d.]+_VERSION)");
 
     private final Property<JavaLanguageVersion> target;
     private final Property<JavaLanguageVersion> runtime;
     private final Property<Boolean> overrideLibraryAutoDetection;
+    private final MapProperty<JavaLanguageVersion, String> zuluVersions;
+    private final MapProperty<JavaLanguageVersion, String> javaVersions;
 
     @Inject
     public BaselineJavaVersionExtension(Project project) {
         target = project.getObjects().property(JavaLanguageVersion.class);
-        runtime = project.getObjects().property(JavaLanguageVersion.class);
-        overrideLibraryAutoDetection = project.getObjects().property(Boolean.class);
         target.finalizeValueOnRead();
+
+        runtime = project.getObjects().property(JavaLanguageVersion.class);
         runtime.finalizeValueOnRead();
+
+        overrideLibraryAutoDetection = project.getObjects().property(Boolean.class);
         overrideLibraryAutoDetection.finalizeValueOnRead();
+
+        zuluVersions = project.getObjects().mapProperty(JavaLanguageVersion.class, String.class);
+        zuluVersions.putAll(project.provider(() -> parseOutVersions(project, ZULU_VERSION_PATTERN)));
+        zuluVersions.finalizeValueOnRead();
+
+        javaVersions = project.getObjects().mapProperty(JavaLanguageVersion.class, String.class);
+        javaVersions.putAll(project.provider(() -> parseOutVersions(project, JAVA_VERSION_PATTERN)));
+        javaVersions.finalizeValueOnRead();
     }
 
     /** Target {@link JavaLanguageVersion} for compilation. */
@@ -69,5 +88,29 @@ public class BaselineJavaVersionExtension {
 
     public final void library() {
         overrideLibraryAutoDetection.set(true);
+    }
+
+    public final MapProperty<JavaLanguageVersion, String> zuluVersions() {
+        return zuluVersions;
+    }
+
+    public final MapProperty<JavaLanguageVersion, String> javaVersions() {
+        return javaVersions;
+    }
+
+    private static Map<JavaLanguageVersion, String> parseOutVersions(Project project, Pattern propertyPattern) {
+        Map<JavaLanguageVersion, String> ret = new HashMap<>();
+
+        project.getProperties().forEach((key, value) -> {
+            Matcher matcher = propertyPattern.matcher(key);
+
+            if (!matcher.matches()) {
+                return;
+            }
+
+            ret.put(JavaLanguageVersion.of(Integer.parseInt(matcher.group(1))), (String) value);
+        });
+
+        return ret;
     }
 }
