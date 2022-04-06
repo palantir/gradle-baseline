60475dfe0ebd1cb8f9a0ffa8e4270b78176a828d
rzpt <@>
2022-04-06 15:41:14 -0700

    remove name from BugPattern

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/LogsafeArgument.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/LogsafeArgument.java
@@ -31,7 +31,6 @@ import java.util.List;
 
 @AutoService(BugChecker.class)
 @BugPattern(
-        name = "LogsafeArgument",
         link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
         linkType = BugPattern.LinkType.CUSTOM,
         severity = SeverityLevel.WARNING,
