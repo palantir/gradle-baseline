6e6d0efdd69302b7cce7c2632c96fee73c9061ad
Callum Rogers <callumr@palantir.com>
2022-04-07 14:28:25 +0100

    Get test working for jdk downloader

---
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/javaversions/AzulJdkDownloader.java
@@ -0,0 +1,58 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.&#10;&#10;Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);&#10;you may not use this file except in compliance with the License.&#10;You may obtain a copy of the License at&#10;&#10;    http://www.apache.org/licenses/LICENSE-2.0&#10;&#10;Unless required by applicable law or agreed to in writing, software&#10;distributed under the License is distributed on an &quot;AS IS&quot; BASIS,&#10;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.&#10;See the License for the specific language governing permissions and&#10;limitations under the License.
+ */
+
+package com.palantir.baseline.plugins.javaversions;
+
+import com.google.common.collect.Iterables;
+import java.nio.file.Path;
+import java.util.Optional;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+
+final class AzulJdkDownloader {
+    private static final String AZUL_JDK = "azul-jdk";
+
+    private final Project rootProject;
+
+    AzulJdkDownloader(Project rootProject) {
+        this.rootProject = rootProject;
+
+        if (rootProject != rootProject.getRootProject()) {
+            throw new IllegalArgumentException("Must pass in the root project");
+        }
+
+        String jdkBaseUrl = property("base-url").orElse("https://cdn.azul.com/zulu/bin/");
+
+        rootProject.getRepositories().ivy(ivy -> {
+            ivy.setName(AZUL_JDK);
+            ivy.setUrl(jdkBaseUrl);
+            ivy.patternLayout(patternLayout -> patternLayout.artifact("[module].[ext]"));
+            ivy.metadataSources(metadataSources -> metadataSources.artifact());
+            ivy.content(repositoryContentDescriptor -> {
+                repositoryContentDescriptor.includeGroup(AZUL_JDK);
+            });
+        });
+
+        rootProject
+                .getRepositories()
+                .matching(repo -> !repo.getName().equals(AZUL_JDK))
+                .configureEach(artifactRepository -> {
+                    artifactRepository.content(content -> content.excludeGroup(AZUL_JDK));
+                });
+    }
+
+    public Path downloadJdkFor(String javaVersion, String zuluVersion, String os, String arch) {
+        Configuration configuration = rootProject
+                .getConfigurations()
+                .detachedConfiguration(rootProject
+                        .getDependencies()
+                        .create(String.format(
+                                "%s:zulu%s-ca-jdk%s-%s_%s:@tar.gz", AZUL_JDK, zuluVersion, javaVersion, os, arch)));
+        return Iterables.getOnlyElement(configuration.resolve()).toPath();
+    }
+
+    private Optional<String> property(String name) {
+        return Optional.ofNullable((String) rootProject.findProperty("com.palantir.baseline-java-versions." + name));
+    }
+}
--- gradle-baseline-java/src/test/groovy/com/palantir/baseline/plugins/javaversions/JdkDownloaderTest.groovy
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/plugins/javaversions/JdkDownloaderTest.groovy
@@ -2,25 +2,20 @@
  * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.&#10;&#10;Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);&#10;you may not use this file except in compliance with the License.&#10;You may obtain a copy of the License at&#10;&#10;    http://www.apache.org/licenses/LICENSE-2.0&#10;&#10;Unless required by applicable law or agreed to in writing, software&#10;distributed under the License is distributed on an &quot;AS IS&quot; BASIS,&#10;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.&#10;See the License for the specific language governing permissions and&#10;limitations under the License.
  */
 
-package com.palantir.baseline.plugins.javaversions;
+package com.palantir.baseline.plugins.javaversions
 
-import java.nio.file.Path;
-import org.gradle.api.Project;
-import org.gradle.jvm.toolchain.JavaLanguageVersion;
+import nebula.test.ProjectSpec
 
-final class JdkDownloader {
-    private final Project rootProject;
+class JdkDownloaderSpec extends ProjectSpec {
+    def 'can download a jdk from an Azul CDN'() {
+        AzulJdkDownloader jdkDownloader = new AzulJdkDownloader(project)
 
-    JdkDownloader(Project rootProject) {
-        this.rootProject = rootProject;
+        when:
+        def path = jdkDownloader.downloadJdkFor('11.0.9.1', '11.43.1021', 'macosx', 'aarch64')
 
-        if (rootProject != rootProject.getRootProject()) {
-            throw new IllegalArgumentException("Must pass in the root project");
-        }
-    }
-
-    public Path downloadJdkFor(JavaLanguageVersion javaLanguageVersion) {
-
-        return null;
+        then:
+        println path
+        path.toFile().exists()
+        path.endsWith(".tar.gz")
     }
 }
