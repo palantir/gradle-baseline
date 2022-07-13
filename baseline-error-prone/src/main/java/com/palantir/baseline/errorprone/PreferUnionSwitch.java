4ce9cd61a3f41bec52df18b17c415482a8b96b79
Eduard Kalinicenko <ekalinicenko@palantir.com>
2022-07-13 17:49:22 +0200

    Work

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferUnionSwitch.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/PreferUnionSwitch.java
@@ -17,17 +17,22 @@
 package com.palantir.baseline.errorprone;
 
 import com.google.auto.service.AutoService;
+import com.google.common.collect.Iterables;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.SeverityLevel;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
-import com.google.errorprone.fixes.SuggestedFixes;
+import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.matchers.Matchers;
 import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.LambdaExpressionTree;
+import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.VariableTree;
 import java.util.List;
 
 /**
@@ -66,13 +71,83 @@ public final class PreferUnionSwitch extends BugChecker implements MethodInvocat
         if (arguments.size() != 1) {
             return Description.NO_MATCH;
         }
-        // ExpressionTree onlyArgument = Iterables.getOnlyElement(arguments);
+        ExpressionTree onlyArgument = Iterables.getOnlyElement(arguments);
+        if (!(onlyArgument instanceof MethodInvocationTree)) {
+            return Description.NO_MATCH;
+        }
+        MethodInvocationTree visitorBuilder = (MethodInvocationTree) onlyArgument;
+
+        String[] parts = state.getSourceForNode(visitorBuilder).split("\\.");
+        if (parts.length == 0) {
+            return Description.NO_MATCH;
+        }
+        String unionName = parts[0];
+
+        SuggestedFix.Builder fix = SuggestedFix.builder();
+        StringBuilder replacementBuilder = new StringBuilder();
 
         String methodInvocation = state.getSourceForNode(tree.getMethodSelect());
         String receiverOnly = methodInvocation.replaceAll("\\.accept", "");
+        replacementBuilder.append(String.format("switch (%s) {", receiverOnly));
+
+        if (!iterateChain(state, replacementBuilder, unionName, visitorBuilder)) {
+            return Description.NO_MATCH;
+        }
+
+        replacementBuilder.append("}");
+        fix.replace(tree, replacementBuilder.toString());
 
         return buildDescription(tree)
-                .addFix(SuggestedFixes.replaceIncludingComments(state.getPath(), receiverOnly, state))
+                .setMessage("Prefer Java 17 switch")
+                .addFix(fix.build())
                 .build();
+    }
+
+    private static boolean iterateChain(
+            VisitorState state, StringBuilder replacementBuilder, String unionName, MethodInvocationTree tree) {
+        if (!(tree.getMethodSelect() instanceof MemberSelectTree)) {
+            return false;
+        }
+        MemberSelectTree memberSelect = (MemberSelectTree) tree.getMethodSelect();
+        if (memberSelect.getIdentifier().toString().equals("builder")) {
+            return true;
+        }
+        if (memberSelect.getExpression() instanceof MethodInvocationTree) {
+            if (!iterateChain(
+                    state, replacementBuilder, unionName, (MethodInvocationTree) memberSelect.getExpression())) {
+                return false;
+            }
+        }
+        if (!memberSelect.getIdentifier().toString().equals("build")) {
+            if (tree.getArguments().size() == 1) {
+                ExpressionTree argument = Iterables.getOnlyElement(tree.getArguments());
+
+                if (argument instanceof LambdaExpressionTree) {
+                    LambdaExpressionTree lambda = (LambdaExpressionTree) argument;
+
+                    if (lambda.getParameters().size() == 1) {
+                        VariableTree variable = Iterables.getOnlyElement(lambda.getParameters());
+                        Tree statement = lambda.getBody();
+
+                        replacementBuilder.append(String.format(
+                                "case %s.%s %s -> %s;",
+                                unionName,
+                                uppercase(memberSelect.getIdentifier().toString()),
+                                state.getSourceForNode(variable),
+                                state.getSourceForNode(statement)));
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String uppercase(String name) {
+        String cleanName = name.endsWith("_") ? name.substring(0, name.length() - 1) : name;
+        return cleanName.substring(0, 1).toUpperCase() + cleanName.substring(1);
     }
 }
