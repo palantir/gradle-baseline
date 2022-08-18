9f2d15811d18d245c0bca9a3e28964b1b3766b78
Alex Landau <alandau@cs.stanford.edu>
2022-08-18 13:07:32 -0700

    Apply more task config laziness to gradle-junit-reports (#2364)
    Make task initialization lazier in the `junit-reports` plugin.

--- gradle-junit-reports/src/main/java/com/palantir/gradle/junit/JunitReportsPlugin.java
+++ gradle-junit-reports/src/main/java/com/palantir/gradle/junit/JunitReportsPlugin.java
@@ -43,12 +43,12 @@ public final class JunitReportsPlugin implements Plugin<Project> {
                     .fileProvider(junitPath(rootExt.getReportsDirectory(), test.getPath()));
         });
 
-        project.getTasks().withType(Checkstyle.class, checkstyle -> {
+        project.getTasks().withType(Checkstyle.class).configureEach(checkstyle -> {
             ext.registerTask(
                     checkstyle.getName(), XmlReportFailuresSupplier.create(checkstyle, new CheckstyleReportHandler()));
         });
 
-        project.getTasks().withType(JavaCompile.class, javac -> {
+        project.getTasks().withType(JavaCompile.class).configureEach(javac -> {
             ext.registerTask(javac.getName(), JavacFailuresSupplier.create(javac));
         });
 
