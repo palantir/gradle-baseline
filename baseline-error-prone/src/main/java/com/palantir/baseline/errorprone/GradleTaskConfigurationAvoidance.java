cdbc7144aa55559f00c8e2c3d03f620d2433b0b0
rzpt <@>
2022-09-06 17:13:44 -0700

    create GradleTaskConfigurationAvoidance error prone check
    Do not use gradle apis that eagerly create tasks.

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/GradleTaskConfigurationAvoidance.java
@@ -0,0 +1,97 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = SeverityLevel.ERROR,
+        summary = "Forbid gradle apis that eagerly create tasks.")
+@SuppressWarnings("deprecation")
+public final class GradleTaskConfigurationAvoidance extends BugChecker
+        implements BugChecker.MethodInvocationTreeMatcher {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String MESSAGE = "Avoid creating tasks eagerly. Learn more at"
+            + " https://docs.gradle.org/current/userguide/task_configuration_avoidance.html";
+
+    private static final Matcher<ExpressionTree> TASK_METHOD = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.gradle.api.Project")
+            .namedAnyOf("task");
+
+    private static final Matcher<ExpressionTree> TASK_CONTAINER_METHODS = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.gradle.api.tasks.TaskContainer")
+            .namedAnyOf("create", "getByPath", "findByPath");
+
+    private static final Matcher<ExpressionTree> TASK_COLLECTION_METHODS = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.gradle.api.tasks.TaskCollection")
+            .namedAnyOf("getByName", "whenTaskAdded", "getAt");
+
+    // These methods are defined for DomainObjectCollection, but we only care about Tasks.
+    // Match based on arguments. withType(Class) is fine.
+    private static final Matcher<ExpressionTree> TASKS_WITH_TYPE_METHOD_ACTION = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.gradle.api.tasks.TaskCollection")
+            .namedAnyOf("withType")
+            .withParameters("java.lang.Class", "org.gradle.api.Action");
+    private static final Matcher<ExpressionTree> TASKS_WITH_TYPE_METHOD_CLOSURE = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.gradle.api.tasks.TaskCollection")
+            .namedAnyOf("withType")
+            .withParameters("java.lang.Class", "groovy.lang.Closure");
+
+    // These methods are defined for DomainObjectCollection, but we only care about Tasks.
+    private static final Matcher<ExpressionTree> DOMAIN_OBJECT_COLLECTION_METHODS = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.gradle.api.tasks.TaskCollection")
+            .namedAnyOf("all", "whenObjectAdded");
+
+    // These methods are defined for NamedDomainObjectCollection, but we only care about Tasks.
+    private static final Matcher<ExpressionTree> NAMED_DOMAIN_OBJECT_COLLECTION_METHODS =
+            MethodMatchers.instanceMethod()
+                    .onDescendantOf("org.gradle.api.tasks.TaskCollection")
+                    .namedAnyOf("findByName");
+
+    private static final Matcher<ExpressionTree> MATCHER = Matchers.anyOf(
+            TASK_METHOD,
+            TASK_CONTAINER_METHODS,
+            TASK_COLLECTION_METHODS,
+            TASKS_WITH_TYPE_METHOD_ACTION,
+            TASKS_WITH_TYPE_METHOD_CLOSURE,
+            DOMAIN_OBJECT_COLLECTION_METHODS,
+            NAMED_DOMAIN_OBJECT_COLLECTION_METHODS);
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        if (MATCHER.matches(tree, state)) {
+            return buildDescription(tree).setMessage(MESSAGE).build();
+        }
+
+        return Description.NO_MATCH;
+    }
+}
---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/GradleTaskConfigurationAvoidanceTest.java
@@ -0,0 +1,253 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GradleTaskConfigurationAvoidanceTest {
+
+    private static final String ERROR = "BUG: Diagnostic contains: " + GradleTaskConfigurationAvoidance.MESSAGE;
+
+    private CompilationTestHelper compilationHelper;
+
+    @BeforeEach
+    public void before() {
+        compilationHelper = CompilationTestHelper.newInstance(GradleTaskConfigurationAvoidance.class, getClass());
+    }
+
+    @Test
+    public void fails_for_task() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.task(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_create() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().create(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_getByPath() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().getByPath(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_findByPath() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().findByPath(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void succeeds_for_register() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    project.getTasks().register(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_getByName() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().getByName(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_whenTaskAdded() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().whenTaskAdded(t -> {});",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_getAt() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().getAt(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_withType_action() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "import org.gradle.api.Task;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().withType(Task.class, t -> {",
+                        "    });",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_withType_closure() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import groovy.lang.Closure;",
+                        "import org.gradle.api.Project;",
+                        "import org.gradle.api.Task;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().withType(Task.class, new Closure<Void>(null) {",
+                        "      @Override",
+                        "      public Void call() {",
+                        "        return null;",
+                        "      }",
+                        "    });",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void succeeds_for_withType_class() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "import org.gradle.api.Task;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    project.getTasks().withType(Task.class).configureEach(t -> {",
+                        "    });",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_all() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().all(t -> {});",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_whenObjectAdded() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().whenObjectAdded(t -> {});",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void fails_for_findByName() {
+        compilationHelper
+                .addSourceLines(
+                        "Foo.java",
+                        "import org.gradle.api.Project;",
+                        "class Foo {",
+                        "  public final void apply(Project project) {",
+                        "    // " + ERROR,
+                        "    project.getTasks().findByName(\"name\");",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+}
