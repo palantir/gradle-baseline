c1a346d5a073ee0a9689a6718c6069412fb27983
Carter Kozak <ckozak@palantir.com>
2022-09-20 16:51:26 -0400

    Allow projects which don't use jdk-15 to resolve latest nullaway

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineNullAway.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/plugins/BaselineNullAway.java
@@ -16,10 +16,13 @@
 
 package com.palantir.baseline.plugins;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.palantir.baseline.plugins.javaversions.BaselineJavaVersions;
 import java.util.Optional;
 import net.ltgt.gradle.errorprone.ErrorProneOptions;
 import org.gradle.api.Action;
+import org.gradle.api.JavaVersion;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
@@ -73,6 +76,7 @@ public final class BaselineNullAway implements Plugin<Project> {
                 options.option("NullAway:CheckOptionalEmptiness", "true");
             }
         });
+        newerNullAwayInNonJdk15Projects(project);
     }
 
     private static void configureErrorProneOptions(Project proj, Action<ErrorProneOptions> action) {
@@ -89,5 +93,48 @@ public final class BaselineNullAway implements Plugin<Project> {
                 });
             }
         });
+    }
+
+    // Workaround for nullaway bugs in the last release to support jdk-15. Projects that don't use jdk-15
+    // resolve a newer nullaway version with bug-fixes. This may be deleted after we've rolled everything
+    // off jdk-15 MTS.
+    private static void newerNullAwayInNonJdk15Projects(Project project) {
+        project.getRootProject().getPlugins().hasPlugin(BaselineJavaVersions.class);
+
+        project.getConfigurations()
+                .matching(new Spec<Configuration>() {
+                    @Override
+                    public boolean isSatisfiedBy(Configuration config) {
+                        return "errorprone".equals(config.getName());
+                    }
+                })
+                .configureEach(new Action<Configuration>() {
+                    @Override
+                    public void execute(Configuration _files) {
+                        project.getDependencies().addProvider("errorprone", project.provider(() -> {
+                            if (anyProjectUsesJava15(project)) {
+                                // Cannot upgrade dependencies in this case because newer nullaway/checkerframework
+                                // are not compatible with java 15, but fully support jdk11 and jdk17.
+                                return ImmutableList.of();
+                            }
+                            return ImmutableList.of(
+                                    "com.uber.nullaway:nullaway:0.10.1",
+                                    // align 'org.checkerframework' dependency versions on
+                                    // the current latest version.
+                                    "org.checkerframework:dataflow-errorprone:3.25.0",
+                                    "org.checkerframework:dataflow-nullaway:3.25.0",
+                                    "org.checkerframework:checker-qual:3.25.0");
+                        }));
+                    }
+                });
+    }
+
+    private static boolean anyProjectUsesJava15(Project proj) {
+        return proj.getAllprojects().stream()
+                .anyMatch(project -> project.getTasks().withType(JavaCompile.class).stream()
+                        .anyMatch(comp -> {
+                            JavaVersion javaVersion = JavaVersion.toVersion(comp.getTargetCompatibility());
+                            return javaVersion == JavaVersion.VERSION_15;
+                        }));
     }
 }
