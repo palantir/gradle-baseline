3b68928ad966dbf1713795b9d865f268cce052d2
Pritham Marupaka <pmarupaka@palantir.com>
2023-03-30 15:33:48 -0400

    review changes

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/CardinalityEqualsZero.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/CardinalityEqualsZero.java
@@ -24,13 +24,14 @@ import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
 import com.google.errorprone.matchers.method.MethodMatchers;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.BinaryTree;
 import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.LiteralTree;
-import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.Tree.Kind;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.tools.javac.tree.JCTree;
 import java.util.Collection;
 import java.util.Optional;
 import org.immutables.value.Value.Immutable;
@@ -39,35 +40,57 @@ import org.immutables.value.Value.Immutable;
 @BugPattern(
         link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
         linkType = BugPattern.LinkType.CUSTOM,
-        severity = SeverityLevel.WARNING,
+        severity = SeverityLevel.SUGGESTION,
         summary = "Use the isEmpty method instead of checking collection size")
-public final class CollectionsIsEmpty extends BugChecker implements BugChecker.BinaryTreeMatcher {
+public final class CardinalityEqualsZero extends BugChecker implements BugChecker.BinaryTreeMatcher {
     private static final Matcher<ExpressionTree> COLLECTION_SIZE_METHOD_MATCHER = MethodMatchers.instanceMethod()
             .onDescendantOf(Collection.class.getName())
-            .named("size");
+            .named("size")
+            .withNoParameters();
+
+    private static final Matcher<ExpressionTree> INT_ZERO = Matchers.intLiteral(0);
 
     @Override
     public Description matchBinary(BinaryTree tree, VisitorState state) {
-        Optional<EqualsZeroExpression> maybeEqualsZeroExpression = getEqualsZeroExpression(tree);
+        Optional<EqualsZeroExpression> maybeEqualsZeroExpression = getEqualsZeroExpression(tree, state);
         if (maybeEqualsZeroExpression.isEmpty()) {
             return Description.NO_MATCH;
         }
+
         EqualsZeroExpression equalsZeroExpression = maybeEqualsZeroExpression.get();
         ExpressionTree operand = equalsZeroExpression.operand();
+        ExpressionTree collectionInstance = ASTHelpers.getReceiver(operand);
 
-        if (!(operand instanceof MethodInvocationTree) || !COLLECTION_SIZE_METHOD_MATCHER.matches(operand, state)) {
+        if (isInIsEmptyDefinition(state) && (collectionInstance == null || isExpressionThis(collectionInstance))) {
             return Description.NO_MATCH;
         }
 
-        return describeMatch(
-                tree,
-                SuggestedFix.replace(
-                        tree,
-                        (equalsZeroExpression.type() == ExpressionType.NEQ ? "!" : "")
-                                + state.getSourceForNode(ASTHelpers.getReceiver(operand)) + ".isEmpty()"));
+        if (COLLECTION_SIZE_METHOD_MATCHER.matches(operand, state)) {
+            return describeMatch(
+                    tree,
+                    SuggestedFix.replace(
+                            tree,
+                            (equalsZeroExpression.type() == ExpressionType.NEQ ? "!" : "")
+                                    + (collectionInstance != null
+                                            ? state.getSourceForNode(collectionInstance) + "."
+                                            : "")
+                                    + "isEmpty()"));
+        }
+
+        return Description.NO_MATCH;
     }
 
-    private static Optional<EqualsZeroExpression> getEqualsZeroExpression(BinaryTree tree) {
+    private static boolean isInIsEmptyDefinition(VisitorState state) {
+        MethodTree methodTree = ASTHelpers.findEnclosingNode(state.getPath(), MethodTree.class);
+        return methodTree != null && methodTree.getName().contentEquals("isEmpty");
+    }
+
+    private static boolean isExpressionThis(ExpressionTree expressionTree) {
+        return ((expressionTree instanceof IdentifierTree)
+                && ((JCTree.JCIdent) expressionTree).getName().contentEquals("this"));
+    }
+
+    private static Optional<EqualsZeroExpression> getEqualsZeroExpression(BinaryTree tree, VisitorState state) {
         ExpressionType ret;
         switch (tree.getKind()) {
             case EQUAL_TO:
@@ -82,15 +105,12 @@ public final class CollectionsIsEmpty extends BugChecker implements BugChecker.B
         ExpressionTree leftOperand = tree.getLeftOperand();
         ExpressionTree rightOperand = tree.getRightOperand();
 
-        if (leftOperand.getKind() == Kind.INT_LITERAL
-                && ((LiteralTree) leftOperand).getValue().equals(0)) {
+        if (INT_ZERO.matches(leftOperand, state)) {
             return Optional.of(EqualsZeroExpression.builder()
                     .type(ret)
                     .operand(rightOperand)
                     .build());
-
-        } else if (rightOperand.getKind() == Kind.INT_LITERAL
-                && ((LiteralTree) rightOperand).getValue().equals(0)) {
+        } else if (INT_ZERO.matches(rightOperand, state)) {
             return Optional.of(EqualsZeroExpression.builder()
                     .type(ret)
                     .operand(leftOperand)
