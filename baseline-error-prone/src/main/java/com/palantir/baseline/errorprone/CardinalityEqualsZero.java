11ef5b51f9495a2dd6db9773e6f04d18bc4846c7
Pritham Marupaka <mpritham@users.noreply.github.com>
2023-03-31 14:38:13 -0400

    Add check: Use Collection.isEmpty() instead of comparing size() with 0 (#2530)
    Add check: Use Collection.isEmpty() instead of comparing size() with 0

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/CardinalityEqualsZero.java
@@ -0,0 +1,138 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.MemberSelectTree;
+import java.util.Collection;
+import java.util.Optional;
+import org.immutables.value.Value.Immutable;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = SeverityLevel.SUGGESTION,
+        summary = "Use the isEmpty method instead of checking collection size")
+public final class CardinalityEqualsZero extends BugChecker implements BugChecker.BinaryTreeMatcher {
+    private static final Matcher<ExpressionTree> COLLECTION_SIZE_METHOD_MATCHER = MethodMatchers.instanceMethod()
+            .onDescendantOf(Collection.class.getName())
+            .named("size")
+            .withNoParameters();
+
+    private static final Matcher<ExpressionTree> INT_ZERO = Matchers.intLiteral(0);
+
+    @Override
+    public Description matchBinary(BinaryTree tree, VisitorState state) {
+        Optional<EqualsZeroExpression> maybeEqualsZeroExpression = getEqualsZeroExpression(tree, state);
+        if (maybeEqualsZeroExpression.isEmpty()) {
+            return Description.NO_MATCH;
+        }
+
+        EqualsZeroExpression equalsZeroExpression = maybeEqualsZeroExpression.get();
+        ExpressionTree operand = equalsZeroExpression.operand();
+        ExpressionTree collectionInstance = ASTHelpers.getReceiver(operand);
+
+        if (collectionInstance == null || isExpressionThis(collectionInstance)) {
+            return Description.NO_MATCH;
+        }
+
+        if (COLLECTION_SIZE_METHOD_MATCHER.matches(operand, state)) {
+            return describeMatch(
+                    tree,
+                    SuggestedFix.replace(
+                            tree,
+                            (equalsZeroExpression.type() == ExpressionType.NEQ ? "!" : "")
+                                    + state.getSourceForNode(collectionInstance)
+                                    + ".isEmpty()"));
+        }
+
+        return Description.NO_MATCH;
+    }
+
+    private static boolean isExpressionThis(ExpressionTree tree) {
+        switch (tree.getKind()) {
+            case IDENTIFIER:
+                return ((IdentifierTree) tree).getName().contentEquals("this");
+            case MEMBER_SELECT:
+                return ((MemberSelectTree) tree).getIdentifier().contentEquals("this");
+            default:
+                return false;
+        }
+    }
+
+    private static Optional<EqualsZeroExpression> getEqualsZeroExpression(BinaryTree tree, VisitorState state) {
+        ExpressionType ret;
+        switch (tree.getKind()) {
+            case EQUAL_TO:
+                ret = ExpressionType.EQ;
+                break;
+            case NOT_EQUAL_TO:
+                ret = ExpressionType.NEQ;
+                break;
+            default:
+                return Optional.empty();
+        }
+        ExpressionTree leftOperand = tree.getLeftOperand();
+        ExpressionTree rightOperand = tree.getRightOperand();
+
+        if (INT_ZERO.matches(leftOperand, state)) {
+            return Optional.of(EqualsZeroExpression.builder()
+                    .type(ret)
+                    .operand(rightOperand)
+                    .build());
+        } else if (INT_ZERO.matches(rightOperand, state)) {
+            return Optional.of(EqualsZeroExpression.builder()
+                    .type(ret)
+                    .operand(leftOperand)
+                    .build());
+        }
+
+        return Optional.empty();
+    }
+
+    enum ExpressionType {
+        EQ,
+        NEQ
+    }
+
+    @Immutable
+    interface EqualsZeroExpression {
+        ExpressionType type();
+
+        ExpressionTree operand();
+
+        class Builder extends ImmutableEqualsZeroExpression.Builder {}
+
+        static Builder builder() {
+            return new Builder();
+        }
+    }
+}
---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/CardinalityEqualsZeroTest.java
@@ -0,0 +1,311 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+public class CardinalityEqualsZeroTest {
+    @Test
+    public void test_size_equals_zero() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return foo.size() == 0;",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return foo.isEmpty();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_size_does_not_equal_zero() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return foo.size() != 0;",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return !foo.isEmpty();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_zero_equals_size() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return 0 == foo.size();",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return foo.isEmpty();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_zero_does_not_equal_size() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return 0 != foo.size();",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return !foo.isEmpty();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_other_collection_types() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + Set.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(Set<String> foo) {",
+                        "    return foo.size() != 0;",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + Set.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(Set<String> foo) {",
+                        "    return !foo.isEmpty();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_conjunction() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo, List<String> bar) {",
+                        "    return 0 != foo.size() && 0 != bar.size();",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo, List<String> bar) {",
+                        "    return !foo.isEmpty() && !bar.isEmpty();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_not_in_method() {
+        // Ensure instances of `size() == 0` are matched when they're enclosed in a function call, or a
+        // variable assignment.
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  private static boolean H = List.of(\"Hello\").size() == 0;",
+                        "  static boolean g(boolean x) { return x; }",
+                        "  static boolean f(List<String> foo) {",
+                        "    return g(foo.size() == 0);",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  private static boolean H = List.of(\"Hello\").isEmpty();",
+                        "  static boolean g(boolean x) { return x; }",
+                        "  static boolean f(List<String> foo) {",
+                        "    return g(foo.isEmpty());",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_no_match() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class Test {",
+                        "  static boolean f(List<String> foo) {",
+                        "    return foo.size() == 1 && foo.size() != 1 && 1 == foo.size() && 1 != foo.size();",
+                        "  }",
+                        "}")
+                .expectUnchanged()
+                .doTest();
+    }
+
+    @Test
+    public void test_implementation_of_isEmpty() {
+        fix().addInputLines(
+                        "TestCollection.java",
+                        "import " + ArrayList.class.getCanonicalName() + ";",
+                        "class TestCollection extends ArrayList<String> {",
+                        "  @Override",
+                        "  public boolean isEmpty() {",
+                        "    return size() == 0;",
+                        "  }",
+                        "  public boolean customIsEmpty() {",
+                        "    return size() == 0;",
+                        "  }",
+                        "}")
+                .expectUnchanged()
+                .doTest();
+    }
+
+    @Test
+    public void test_implementation_of_isEmpty_using_this() {
+        fix().addInputLines(
+                        "TestCollection.java",
+                        "import " + ArrayList.class.getCanonicalName() + ";",
+                        "class TestCollection extends ArrayList<String> {",
+                        "  @Override",
+                        "  public boolean isEmpty() {",
+                        "    return this.size() == 0;",
+                        "  }",
+                        "}")
+                .expectUnchanged()
+                .doTest();
+    }
+
+    @Test
+    public void test_implementation_of_isEmpty_with_other_collection() {
+        fix().addInputLines(
+                        "TestCollection.java",
+                        "import " + ArrayList.class.getCanonicalName() + ";",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class TestCollection extends ArrayList<String> {",
+                        "  @Override",
+                        "  public boolean isEmpty() {",
+                        "    return this.size() == 0 && List.of(\"test\").size() == 0;",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "TestCollection.java",
+                        "import " + ArrayList.class.getCanonicalName() + ";",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class TestCollection extends ArrayList<String> {",
+                        "  @Override",
+                        "  public boolean isEmpty() {",
+                        "    return this.size() == 0 && List.of(\"test\").isEmpty();",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    @Test
+    public void test_size_on_non_collection() {
+        fix().addInputLines(
+                        "TestNonCollection.java",
+                        "class TestNonCollection {",
+                        "  public int size() {",
+                        "    return 0;",
+                        "  }",
+                        "  public boolean myIsEmpty() {",
+                        "    return size() == 0;",
+                        "  }",
+                        "  public boolean anotherIsEmpty() {",
+                        "    return this.size() == 0;",
+                        "  }",
+                        "}")
+                .expectUnchanged()
+                .doTest();
+    }
+
+    @Test
+    public void test_qualified_this() {
+        fix().addInputLines(
+                        "TestQualifiedThis.java",
+                        "import " + ArrayList.class.getCanonicalName() + ";",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class TestQualifiedThis extends ArrayList<String> {",
+                        "    boolean myIsEmpty() {",
+                        "        return this.size() == 0;",
+                        "    }",
+                        "    class Inner extends ArrayList<String> {",
+                        "        boolean myIsEmpty() {",
+                        "            return TestQualifiedThis.this.size() == 0;",
+                        "        }",
+                        "        boolean anotherIsEmpty() {",
+                        "            return List.of().size() == 0 && this.size() == 0;",
+                        "        }",
+                        "    }",
+                        "}")
+                .addOutputLines(
+                        "TestQualifiedThis.java",
+                        "import " + ArrayList.class.getCanonicalName() + ";",
+                        "import " + List.class.getCanonicalName() + ";",
+                        "class TestQualifiedThis extends ArrayList<String> {",
+                        "    boolean myIsEmpty() {",
+                        "        return this.size() == 0;",
+                        "    }",
+                        "    class Inner extends ArrayList<String> {",
+                        "        boolean myIsEmpty() {",
+                        "            return TestQualifiedThis.this.size() == 0;",
+                        "        }",
+                        "        boolean anotherIsEmpty() {",
+                        "            return List.of().isEmpty() && this.size() == 0;",
+                        "        }",
+                        "    }",
+                        "}")
+                .doTest();
+    }
+
+    private RefactoringValidator fix() {
+        return RefactoringValidator.of(CardinalityEqualsZero.class, getClass());
+    }
+}
