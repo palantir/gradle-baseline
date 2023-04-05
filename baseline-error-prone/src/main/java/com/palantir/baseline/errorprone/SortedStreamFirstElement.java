4a0f0c8ac1d53f05ba5a8ba8967450edb189e99d
Pritham Marupaka <pmarupaka@palantir.com>
2023-04-05 18:04:39 -0400

    wip

---
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/SortedStreamFirstElement.java
@@ -0,0 +1,97 @@
+/*
+ * (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+@AutoService(BugChecker.class)
+@BugPattern(
+        link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
+        linkType = BugPattern.LinkType.CUSTOM,
+        severity = SeverityLevel.SUGGESTION,
+        summary = "TODO")
+public final class SortedStreamFirstElement extends BugChecker implements BugChecker.MethodInvocationTreeMatcher {
+
+    private static final Matcher<ExpressionTree> STREAM_SORTED_FIND_FIRST_MATCHER = MethodMatchers.instanceMethod()
+            .onDescendantOf(Stream.class.getName())
+            .namedAnyOf("findFirst")
+            .withNoParameters();
+
+    private static final Matcher<ExpressionTree> STREAM_SORTED_NO_PARAMS_MATCHER = MethodMatchers.instanceMethod()
+            .onDescendantOf(Stream.class.getName())
+            .named("sorted")
+            .withNoParameters();
+
+    private static final Matcher<ExpressionTree> STREAM_SORTED_WITH_COMPARATOR_MATCHER = MethodMatchers.instanceMethod()
+            .onDescendantOf(Stream.class.getName())
+            .named("sorted")
+            .withParameters(Comparator.class.getName());
+
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        if (!STREAM_SORTED_FIND_FIRST_MATCHER.matches(tree, state)) {
+            return Description.NO_MATCH;
+        }
+
+        ExpressionTree sorted = ASTHelpers.getReceiver(tree);
+        if (sorted == null) {
+            return Description.NO_MATCH;
+        }
+        MethodInvocationTree sortedTree = (MethodInvocationTree) sorted;
+        ExpressionTree stream = ASTHelpers.getReceiver(sorted);
+        if (stream == null) {
+            return Description.NO_MATCH;
+        }
+
+        if (Matchers.receiverOfInvocation(STREAM_SORTED_NO_PARAMS_MATCHER).matches(tree, state)) {
+            return describeMatch(
+                    tree,
+                    SuggestedFix.builder()
+                            .replace(tree, state.getSourceForNode(stream) + ".min(Comparator.naturalOrder())")
+                            .addImport(Comparator.class.getCanonicalName())
+                            .build());
+        } else if (Matchers.receiverOfInvocation(STREAM_SORTED_WITH_COMPARATOR_MATCHER)
+                .matches(tree, state)) {
+            return describeMatch(
+                    tree,
+                    SuggestedFix.builder()
+                            .replace(
+                                    tree,
+                                    state.getSourceForNode(stream) + ".min("
+                                            + state.getSourceForNode(
+                                                    sortedTree.getArguments().get(0)) + ")")
+                            .addImport(Comparator.class.getCanonicalName())
+                            .build());
+        }
+
+        return Description.NO_MATCH;
+    }
+}
