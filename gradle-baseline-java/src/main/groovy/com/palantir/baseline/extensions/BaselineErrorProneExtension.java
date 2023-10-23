b309f8047d932608bef1306ade1a25290eefd036
Carter Kozak <ckozak@palantir.com>
2023-10-23 14:24:31 -0400

    baseline error-prone no longer auto-fixes PreferJavaTimeOverload
    PreferJavaTimeOverload is not enabled by default, so updates are
    not enforced. Unfortunatley in many places we prefer not to make
    the proposed change because it introduces allocation into
    potentially performance sensitive paths (e.g. timer metrics).

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineErrorProneExtension.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineErrorProneExtension.java
@@ -93,7 +93,6 @@ public class BaselineErrorProneExtension {
             "MissingOverride",
             "NarrowCalculation",
             "ObjectsHashCodePrimitive",
-            "PreferJavaTimeOverload",
             "ProtectedMembersInFinalClass",
             "UnnecessaryParentheses",
             "ZoneIdOfZ");
