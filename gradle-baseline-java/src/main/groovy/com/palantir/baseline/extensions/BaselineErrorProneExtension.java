2abb6d1c8eb059999030530b6b57d65d20a02f66
Lucas Meireles <lmeireles@palantir.com>
2024-04-26 16:59:12 +0100

    add annotation by default

--- gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineErrorProneExtension.java
+++ gradle-baseline-java/src/main/groovy/com/palantir/baseline/extensions/BaselineErrorProneExtension.java
@@ -43,6 +43,7 @@ public class BaselineErrorProneExtension {
             "ExtendsErrorOrThrowable",
             "FinalClass",
             "IllegalSafeLoggingArgument",
+            "ImmutableDefaultShouldHaveAnnotations",
             "ImmutableMapDuplicateKeyStrategy",
             "ImmutablesStyle",
             "ImplicitPublicBuilderConstructor",
