8156d1811cc9a883b39b782662a26d0fad1ad8a2
Callum Rogers <callumr@palantir.com>
2024-08-22 15:55:04 +0100

    Start writing test

---
+++ gradle-baseline-java/src/test/groovy/com/palantir/baseline/plugins/suppressible/SuppressibleErrorPronePluginIntegrationTest.groovy
@@ -0,0 +1,55 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.plugins.suppressible
+
+import nebula.test.IntegrationSpec
+import nebula.test.functional.ExecutionResult
+
+class SuppressibleErrorPronePluginIntegrationTest extends IntegrationSpec {
+    def setup() {
+        // language=Gradle
+        buildFile << '''
+            apply plugin: 'com.palantir.suppressible-error-prone'
+            apply plugin: 'java'
+        '''.stripIndent(true)
+    }
+
+    def 'reports a failing error prone'() {
+        // language=Java
+        writeJavaSourceFile '''
+            package app;
+            public final class App {
+                public static void main(String[] args) {
+                    new int[3].toString();
+                }
+            }
+        '''.stripIndent(true)
+
+        when:
+        def stderr = runTasksWithFailure('compileJava').standardError
+
+        then:
+        stderr.contains('[ArrayToString]')
+    }
+
+    @Override
+    ExecutionResult runTasks(String... tasks) {
+        def projectVersion = Optional.ofNullable(System.getProperty('projectVersion')).orElseThrow()
+        String[] strings = tasks + ["-PbaselineErrorProneVersion=${projectVersion}".toString()]
+        return super.runTasks(strings)
+    }
+}
