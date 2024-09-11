862694c50ba334c76c8b3b5d9be3abd7d874e665
Eric Sword <esword@palantir.com>
2024-09-11 11:04:31 -0400

    compiles

--- gradle-baseline-java/src/main/java/com/palantir/baseline/plugins/AbstractBaselinePlugin.java
+++ gradle-baseline-java/src/main/java/com/palantir/baseline/plugins/AbstractBaselinePlugin.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2015 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,13 @@
  * limitations under the License.
  */
 
-package com.palantir.baseline.plugins
+package com.palantir.baseline.plugins;
 
-import com.palantir.baseline.BaselineParameters
-import java.nio.file.Paths
-import org.gradle.api.Plugin
-import org.gradle.api.Project
+import com.palantir.baseline.BaselineParameters;
+import groovy.lang.GroovyObjectSupport;
+import java.nio.file.Paths;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
 
 /**
  * The super class of all Baseline plugins.
@@ -28,14 +29,16 @@ import org.gradle.api.Project
  * because Baseline is now using Gradle 7 and thus Groovy 3.x. Otherwise, Groovy plugins (i.e. {@link BaselineIdea})
  * fail when setting properties. For more info, see https://github.com/palantir/gradle-baseline/pull/1884.
  */
-abstract class AbstractBaselinePlugin extends GroovyObjectSupport implements Plugin<Project> {
+public abstract class AbstractBaselinePlugin extends GroovyObjectSupport implements Plugin<Project> {
 
     /** The {@link Project} that this plugin has been applied to; must be set in the {@link Project#apply} method. */
-    protected Project project
+    @SuppressWarnings("checkstyle:VisibilityModifier")
+    protected Project project;
 
     /** Returns the absolute path of the Baseline configuration, i.e., the directory '.baseline' in the root directory
      * of this project. */
     protected final String getConfigDir() {
-        return Paths.get(project.rootDir.toString(), BaselineParameters.DEFAULT_CONFIG_DIR).toString()
+        return Paths.get(project.getRootDir().toString(), BaselineParameters.DEFAULT_CONFIG_DIR)
+                .toString();
     }
 }
