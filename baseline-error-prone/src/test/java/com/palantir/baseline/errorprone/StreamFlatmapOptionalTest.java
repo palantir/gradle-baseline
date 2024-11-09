09d424943790f1585882527a0ce1d6727c23216d
David Schlosnagle <davids@palantir.com>
2024-11-08 20:52:38 -0500

    Add StreamFlatMapOptional error-prone check (#2946)
    If one has a `Stream<Optional<T>> stream` of size `N` and does `stream.flatMap(Optional::stream)`, you’ll end up allocating `N` extra streams — one for each `Optional` input element. When `N` is large, those allocations can cause extra GC cycles and pauses if allocation rate is high enough leading to issues with latency, throughput, and allocation sensitive code paths.
    
    `Stream.filter(Optional::isPresent).map(Optional::get)` is more efficient than `Stream.flatMap(Optional::stream)` as it does not allocate a new `Stream` for every element in the stream.

---
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/StreamFlatmapOptionalTest.java
@@ -0,0 +1,72 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.errorprone;
+
+import org.junit.jupiter.api.Test;
+
+class StreamFlatmapOptionalTest {
+    @Test
+    public void test() {
+        fix().addInputLines(
+                        "Test.java",
+                        "import java.util.Collection;",
+                        "import java.util.Optional;",
+                        "import java.util.stream.Stream;",
+                        "public class Test {",
+                        "  Stream<String> f1(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(Collection::stream).flatMap(Optional::stream);",
+                        "  }",
+                        "  Stream<String> f2(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(list -> list.stream().flatMap(Optional::stream));",
+                        "  }",
+                        "  Stream<String> f3(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(list -> list.stream()).flatMap(Optional::stream);",
+                        "  }",
+                        "  Stream<String> f4(Stream<Optional<Optional<String>>> in) {",
+                        "      return in.flatMap(Optional::stream).flatMap(Optional::stream);",
+                        "  }",
+                        "}")
+                .addOutputLines(
+                        "Test.java",
+                        "import java.util.Collection;",
+                        "import java.util.Optional;",
+                        "import java.util.stream.Stream;",
+                        "public class Test {",
+                        "  Stream<String> f1(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(Collection::stream)"
+                                + ".filter(Optional::isPresent).map(Optional::get);",
+                        "  }",
+                        "  Stream<String> f2(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(list -> list.stream()"
+                                + ".filter(Optional::isPresent).map(Optional::get));",
+                        "  }",
+                        "  Stream<String> f3(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(list -> list.stream())"
+                                + ".filter(Optional::isPresent).map(Optional::get);",
+                        "  }",
+                        "  Stream<String> f4(Stream<Optional<Optional<String>>> in) {",
+                        "      return in.filter(Optional::isPresent).map(Optional::get)"
+                                + ".filter(Optional::isPresent).map(Optional::get);",
+                        "  }",
+                        "}")
+                .doTest();
+    }
+
+    private RefactoringValidator fix() {
+        return RefactoringValidator.of(StreamFlatMapOptional.class, getClass());
+    }
+}
