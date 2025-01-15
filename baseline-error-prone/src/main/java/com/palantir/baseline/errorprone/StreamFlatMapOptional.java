09c5689c53924e9ff59bce8b7b2053b33ad528bb
David Schlosnagle <davids@palantir.com>
2025-01-15 14:52:49 -0500

    s/flatMap(Optional::stream)/mapMulti(Optional::ifPresent)/

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/StreamFlatMapOptional.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/StreamFlatMapOptional.java
@@ -18,8 +18,10 @@ package com.palantir.baseline.errorprone;
 
 import com.google.auto.service.AutoService;
 import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.SeverityLevel;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
 import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.matchers.ChildMultiMatcher.MatchType;
@@ -30,50 +32,132 @@ import com.google.errorprone.matchers.method.MethodMatchers;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
+import com.sun.tools.javac.code.Type;
+import java.io.Serial;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @AutoService(BugChecker.class)
 @BugPattern(
         link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
         linkType = BugPattern.LinkType.CUSTOM,
-        severity = BugPattern.SeverityLevel.WARNING,
-        summary = "`Stream.filter(Optional::isPresent).map(Optional::orElseThrow)` is more efficient than "
-                + "`Stream.flatMap(Optional::stream)`")
-public final class StreamFlatMapOptional extends BugChecker implements BugChecker.MethodInvocationTreeMatcher {
+        severity = SeverityLevel.SUGGESTION,
+        summary = "`Stream.mapMulti(Optional::ifPresent)` is more efficient than `Stream.flatMap(Optional::stream)`")
+public final class StreamFlatMapOptional extends BugChecker implements MethodInvocationTreeMatcher {
+    @Serial
     private static final long serialVersionUID = 1L;
 
-    private static final Matcher<ExpressionTree> STREAM_FLAT_MAP = MethodMatchers.instanceMethod()
-            .onDescendantOf(Stream.class.getName())
-            .namedAnyOf("flatMap")
-            .withParameters(Function.class.getName());
-
-    private static final Matcher<ExpressionTree> OPTIONAL_STREAM = MethodMatchers.instanceMethod()
-            .onDescendantOf(Optional.class.getName())
-            .named("stream")
-            .withNoParameters();
-
     private static final Matcher<ExpressionTree> STREAM_FLATMAP_OPTIONAL_STREAM = Matchers.methodInvocation(
-            STREAM_FLAT_MAP,
+            MethodMatchers.instanceMethod()
+                    .onDescendantOf(Stream.class.getCanonicalName())
+                    .namedAnyOf("flatMap")
+                    .withParameters(Function.class.getName()),
             // Any of the three MatchTypes are reasonable in this case, given a single arg
-            MatchType.LAST,
-            OPTIONAL_STREAM);
+            MatchType.AT_LEAST_ONE,
+            MethodMatchers.instanceMethod()
+                    .onDescendantOf(Optional.class.getCanonicalName())
+                    .named("stream")
+                    .withNoParameters());
+
+    private static final Matcher<ExpressionTree> STREAM_FILTER_IS_PRESENT = Matchers.methodInvocation(
+            MethodMatchers.instanceMethod()
+                    .onDescendantOf(Stream.class.getCanonicalName())
+                    .named("filter")
+                    .withParameters(Predicate.class.getName()),
+            // Any of the three MatchTypes are reasonable in this case, given a single arg
+            MatchType.AT_LEAST_ONE,
+            MethodMatchers.instanceMethod()
+                    .onDescendantOf(Optional.class.getCanonicalName())
+                    .named("isPresent")
+                    .withNoParameters());
+
+    private static final Matcher<ExpressionTree> STREAM_MAP_GET = Matchers.methodInvocation(
+            MethodMatchers.instanceMethod()
+                    .onDescendantOf(Stream.class.getCanonicalName())
+                    .named("map")
+                    .withParameters(Function.class.getName()),
+            // Any of the three MatchTypes are reasonable in this case, given a single arg
+            MatchType.AT_LEAST_ONE,
+            MethodMatchers.instanceMethod()
+                    .onDescendantOf(Optional.class.getCanonicalName())
+                    .namedAnyOf("get", "orElseThrow")
+                    .withNoParameters());
 
     @Override
     public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
         if (STREAM_FLATMAP_OPTIONAL_STREAM.matches(tree, state)) {
-            ExpressionTree receiver = ASTHelpers.getReceiver(tree.getMethodSelect());
-            if (receiver != null) {
-                SuggestedFix.Builder fix = SuggestedFix.builder();
-                String optionalType = SuggestedFixes.qualifyType(state, fix, Optional.class.getCanonicalName());
-                String replacement = ".filter(" + optionalType + "::isPresent).map(" + optionalType + "::orElseThrow)";
-                return buildDescription(tree)
-                        .addFix(fix.replace(state.getEndPosition(receiver), state.getEndPosition(tree), replacement)
-                                .build())
-                        .build();
+            ExpressionTree methodSelect = tree.getMethodSelect();
+            ExpressionTree receiver = ASTHelpers.getReceiver(methodSelect);
+            return fix(tree, state, receiver, receiver);
+        }
+
+        if (STREAM_MAP_GET.matches(tree, state)) {
+            ExpressionTree mapTree = ASTHelpers.getReceiver(tree.getMethodSelect());
+            if (mapTree != null && STREAM_FILTER_IS_PRESENT.matches(mapTree, state)) {
+                ExpressionTree filterTree = ASTHelpers.getReceiver(mapTree);
+                return fix(tree, state, tree.getMethodSelect(), filterTree);
             }
         }
+
         return Description.NO_MATCH;
+    }
+
+    private Description fix(
+            MethodInvocationTree tree, VisitorState state, ExpressionTree receiver, ExpressionTree expressionTree) {
+        if (receiver == null) {
+            return Description.NO_MATCH;
+        }
+
+        Type elementType = ASTHelpers.getType(tree);
+        if (elementType == null) {
+            return Description.NO_MATCH;
+        }
+
+        Type receiverType = ASTHelpers.getType(receiver);
+        if (receiverType == null) {
+            return Description.NO_MATCH;
+        }
+
+        SuggestedFix.Builder fix = SuggestedFix.builder();
+
+        long receiverCount = args(ASTHelpers.getReceiverType(receiver)).count();
+        Stream<Type> args = args(ASTHelpers.getReceiverType(expressionTree));
+        long argCount = args.count();
+        boolean shouldQualifyType =
+                args(elementType).findAny().isPresent() || receiverCount > 1 || (receiverCount == 0 && argCount > 0);
+
+        String qualifiedType = "";
+        if (shouldQualifyType) {
+            qualifiedType = qualifyType(state, fix, receiverType.getTypeArguments());
+            if (qualifiedType.isEmpty()) {
+                qualifiedType = SuggestedFixes.qualifyType(state, fix, elementType);
+            }
+        }
+
+        String replacement = "." + qualifiedType + "mapMulti("
+                + SuggestedFixes.qualifyType(state, fix, Optional.class.getCanonicalName()) + "::ifPresent)";
+        return buildDescription(tree)
+                .addFix(fix.replace(state.getEndPosition(expressionTree), state.getEndPosition(tree), replacement)
+                        .build())
+                .build();
+    }
+
+    private static String qualifyType(
+            VisitorState state, SuggestedFix.Builder fix, Collection<Type> receiverTypeArguments) {
+        return args(receiverTypeArguments)
+                .map(type -> SuggestedFixes.qualifyType(state, fix, type))
+                .collect(Collectors.joining(", ", "<", ">"));
+    }
+
+    private static Stream<Type> args(Type type) {
+        return args(type.getTypeArguments());
+    }
+
+    private static Stream<Type> args(Collection<Type> types) {
+        return types.stream().flatMap(type -> type.getTypeArguments().stream());
     }
 }
--- baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/StreamFlatmapOptionalTest.java
+++ baseline-error-prone/src/test/java/com/palantir/baseline/errorprone/StreamFlatmapOptionalTest.java
@@ -19,48 +19,99 @@ package com.palantir.baseline.errorprone;
 import org.junit.jupiter.api.Test;
 
 class StreamFlatmapOptionalTest {
+
     @Test
     public void test() {
         fix().addInputLines(
                         "Test.java",
                         "import java.util.Collection;",
+                        "import java.util.Map;",
                         "import java.util.Optional;",
                         "import java.util.stream.Stream;",
                         "public class Test {",
-                        "  Stream<String> f1(Stream<Collection<Optional<String>>> in) {",
+                        "  Stream<String> filter_empty(Stream<Optional<String>> in) {",
+                        "    return in.filter(Optional::isEmpty).map(o -> o.orElse(\"\"));",
+                        "  }",
+                        "  Stream<String> flatMap_simple(Stream<Optional<String>> in) {",
+                        "    return in.flatMap(Optional::stream);",
+                        "  }",
+                        "  Stream<String> filter_map_get(Stream<Optional<String>> in) {",
+                        "    return in.filter(Optional::isPresent).map(Optional::get);",
+                        "  }",
+                        "  Stream<Integer> filter_map_length(Stream<Optional<String>> in) {",
+                        "    return in.filter(Optional::isPresent).map(o -> o.get().length());",
+                        "  }",
+                        "  Stream<String> filter_orElseThrow(Stream<Optional<String>> in) {",
+                        "    return in.filter(Optional::isPresent).map(Optional::orElseThrow);",
+                        "  }",
+                        "  Stream<String> flatMap_collection(Stream<Collection<Optional<String>>> in) {",
                         "    return in.flatMap(Collection::stream).flatMap(Optional::stream);",
                         "  }",
-                        "  Stream<String> f2(Stream<Collection<Optional<String>>> in) {",
+                        "  Stream<String> flatMap_lambda_collection(Stream<Collection<Optional<String>>> in) {",
                         "    return in.flatMap(list -> list.stream().flatMap(Optional::stream));",
                         "  }",
-                        "  Stream<String> f3(Stream<Collection<Optional<String>>> in) {",
-                        "    return in.flatMap(list -> list.stream()).flatMap(Optional::stream);",
+                        "  Stream<String> flatMap_lambda_optional_collection("
+                                + "Stream<Optional<Collection<Optional<String>>>> in) {",
+                        "    return in.flatMap(o -> o.stream().flatMap(Collection::stream).flatMap(Optional::stream));",
                         "  }",
-                        "  Stream<String> f4(Stream<Optional<Optional<String>>> in) {",
-                        "      return in.flatMap(Optional::stream).flatMap(Optional::stream);",
+                        "  Stream<String> flatMap_simple_flatMap_collection(Stream<Optional<Collection<String>>> in) {",
+                        "    return in.flatMap(Optional::stream).flatMap(Collection::stream);",
+                        "  }",
+                        "  Stream<String> flatMap_optional_flatMap_optional(Stream<Optional<Optional<String>>> in) {",
+                        "    return in.flatMap(Optional::stream).flatMap(Optional::stream);",
+                        "  }",
+                        "  Collection<String> flatMap_optional_map_flatMap_optional_toList("
+                                + "Stream<Optional<Map.Entry<Integer, Optional<String>>>> in) {",
+                        "    return in.flatMap(Optional::stream).map(Map.Entry::getValue)"
+                                + ".flatMap(Optional::stream).toList();",
                         "  }",
                         "}")
                 .addOutputLines(
                         "Test.java",
                         "import java.util.Collection;",
+                        "import java.util.Map;",
                         "import java.util.Optional;",
                         "import java.util.stream.Stream;",
                         "public class Test {",
-                        "  Stream<String> f1(Stream<Collection<Optional<String>>> in) {",
-                        "    return in.flatMap(Collection::stream)"
-                                + ".filter(Optional::isPresent).map(Optional::orElseThrow);",
+                        "  Stream<String> filter_empty(Stream<Optional<String>> in) {",
+                        "    return in.filter(Optional::isEmpty).map(o -> o.orElse(\"\"));",
                         "  }",
-                        "  Stream<String> f2(Stream<Collection<Optional<String>>> in) {",
-                        "    return in.flatMap(list -> list.stream()"
-                                + ".filter(Optional::isPresent).map(Optional::orElseThrow));",
+                        "  Stream<String> flatMap_simple(Stream<Optional<String>> in) {",
+                        "    return in.mapMulti(Optional::ifPresent);",
                         "  }",
-                        "  Stream<String> f3(Stream<Collection<Optional<String>>> in) {",
-                        "    return in.flatMap(list -> list.stream())"
-                                + ".filter(Optional::isPresent).map(Optional::orElseThrow);",
+                        "  Stream<String> filter_map_get(Stream<Optional<String>> in) {",
+                        "    return in.mapMulti(Optional::ifPresent);",
                         "  }",
-                        "  Stream<String> f4(Stream<Optional<Optional<String>>> in) {",
-                        "      return in.filter(Optional::isPresent).map(Optional::orElseThrow)"
-                                + ".filter(Optional::isPresent).map(Optional::orElseThrow);",
+                        "  Stream<Integer> filter_map_length(Stream<Optional<String>> in) {",
+                        "    return in.filter(Optional::isPresent).map(o -> o.get().length());",
+                        "  }",
+                        "  Stream<String> filter_orElseThrow(Stream<Optional<String>> in) {",
+                        "    return in.mapMulti(Optional::ifPresent);",
+                        "  }",
+                        "  Stream<String> flatMap_collection(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(Collection::stream).mapMulti(Optional::ifPresent);",
+                        "  }",
+                        "  Stream<String> flatMap_lambda_collection(Stream<Collection<Optional<String>>> in) {",
+                        "    return in.flatMap(list -> list.stream().mapMulti(Optional::ifPresent));",
+                        "  }",
+                        "  Stream<String> flatMap_lambda_optional_collection("
+                                + "Stream<Optional<Collection<Optional<String>>>> in) {",
+                        "    return in.flatMap(o -> "
+                                + "o.stream().flatMap(Collection::stream).mapMulti(Optional::ifPresent));",
+                        "  }",
+                        "  Stream<String> flatMap_simple_flatMap_collection(Stream<Optional<Collection<String>>> in) {",
+                        "    return in.<Collection<String>>mapMulti(Optional::ifPresent).flatMap(Collection::stream);",
+                        "  }",
+                        "  Stream<String> flatMap_optional_flatMap_optional(Stream<Optional<Optional<String>>> in) {",
+                        "    return in.<Optional<String>>mapMulti(Optional::ifPresent)"
+                                + ".mapMulti(Optional::ifPresent);",
+                        "  }",
+                        "  Collection<String> flatMap_optional_map_flatMap_optional_toList("
+                                + "Stream<Optional<Map.Entry<Integer, Optional<String>>>> in) {",
+                        "    return in.<Map.Entry<Integer,Optional<String>>>mapMulti(Optional::ifPresent)"
+                                + ".map(Map.Entry::getValue)"
+                                + ".<String>mapMulti(Optional::ifPresent)"
+                                + ".toList();",
                         "  }",
                         "}")
                 .doTest();
