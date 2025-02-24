0ca60bb0498d1d06083880aad1b491fd10d95018
Alexis Le Dantec <aledantec@palantir.com>
2025-02-24 14:26:52 +0100

    Refactor out lambda-specific method

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/safety/SafetyPropagationTransfer.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/safety/SafetyPropagationTransfer.java
@@ -442,37 +442,45 @@ public final class SafetyPropagationTransfer implements ForwardTransferFunction<
         if (parameters == null) {
             return AccessPathStore.empty();
         }
-        AccessPathStore.Builder<Safety> result = AccessPathStore.<Safety>empty().toBuilder();
 
         if (underlyingAst.getKind() == UnderlyingAST.Kind.LAMBDA) {
             // Special-case lambda types as the parameter annotations are not propagated in
             //   ForwardAnalysisImpl#getParameters, which is passed as the argument here
-            LambdaExpressionTree lambda = ((UnderlyingAST.CFGLambda) underlyingAst).getLambdaTree();
-
-            // We grab the inferred type of the lambda, then the argument types of the functional interface
-            //  and match them one by one with the actual parameters
-            com.sun.tools.javac.util.List<Type> parameterTypes = state.getTypes()
-                    .findDescriptorType(ASTHelpers.getType(lambda))
-                    .getParameterTypes();
-
-            // This is expected to be true, but let's be defensive here
-            if (parameterTypes.size() == parameters.size()) {
-                for (int i = 0; i < parameters.size(); i++) {
-                    Type type = parameterTypes.get(i);
-                    LocalVariableNode param = parameters.get(i);
-
-                    // If the lambda itself has a safety annotation, which doesn't match the expected one from the
-                    //   type it is used as, combine both
-                    Safety declared = SafetyAnnotations.getSafety(param.getTree(), state);
-                    Safety safety = SafetyAnnotations.getSafety(type, state);
-                    result.setInformation(
-                            AccessPath.fromLocalVariable(param), Safety.mergeAssumingUnknownIsSame(declared, safety));
-                }
-            }
+            return initialStoreLambda(underlyingAst, parameters);
         } else {
+            AccessPathStore.Builder<Safety> result = AccessPathStore.<Safety>empty().toBuilder();
+
             for (LocalVariableNode param : parameters) {
                 Safety declared = SafetyAnnotations.getSafety(param.getTree(), state);
                 result.setInformation(AccessPath.fromLocalVariable(param), declared);
+            }
+
+            return result.build();
+        }
+    }
+
+    private AccessPathStore<Safety> initialStoreLambda(
+            UnderlyingAST underlyingAst, List<LocalVariableNode> parameters) {
+        AccessPathStore.Builder<Safety> result = AccessPathStore.<Safety>empty().toBuilder();
+        LambdaExpressionTree lambda = ((UnderlyingAST.CFGLambda) underlyingAst).getLambdaTree();
+
+        // We grab the inferred type of the lambda, then the argument types of the functional interface
+        //  and match them one by one with the actual parameters
+        com.sun.tools.javac.util.List<Type> parameterTypes =
+                state.getTypes().findDescriptorType(ASTHelpers.getType(lambda)).getParameterTypes();
+
+        // This is expected to be true, but let's be defensive here
+        if (parameterTypes.size() == parameters.size()) {
+            for (int i = 0; i < parameters.size(); i++) {
+                Type type = parameterTypes.get(i);
+                LocalVariableNode param = parameters.get(i);
+
+                // If the lambda itself has a safety annotation, which doesn't match the expected one from the
+                //   type it is used as, combine both
+                Safety declared = SafetyAnnotations.getSafety(param.getTree(), state);
+                Safety safety = SafetyAnnotations.getSafety(type, state);
+                result.setInformation(
+                        AccessPath.fromLocalVariable(param), Safety.mergeAssumingUnknownIsSame(declared, safety));
             }
         }
 
