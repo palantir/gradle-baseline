c749360a09714f27f9ee8b673154fb6b9f58abb0
Callum Rogers <CRogers@users.noreply.github.com>
2025-03-21 17:42:45 +0000

    baseline-java-versions: `explainJavaVersions` task (#3079)
    baseline-java-versions: Provide an `explainJavaVersions` task that makes it easier to work out which `target` and `runtime` java versions have been chosen by the plugin and why.

---
+++ gradle-baseline-java/src/main/java/com/palantir/baseline/plugins/javaversions/ExplainJavaVersions.java
@@ -0,0 +1,63 @@
+/*
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.baseline.plugins.javaversions;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+
+public abstract class ExplainJavaVersions extends DefaultTask {
+    @Input
+    public abstract Property<ChosenJavaVersion> getTarget();
+
+    @Input
+    public abstract Property<ChosenJavaVersion> getDefaultTarget();
+
+    @Input
+    public abstract Property<ChosenJavaVersion> getRuntime();
+
+    @Input
+    public abstract Property<ChosenJavaVersion> getDefaultRuntime();
+
+    @Input
+    public abstract Property<String> getReasoning();
+
+    @TaskAction
+    public final void action() {
+        getLogger()
+                .lifecycle(
+                        "target  = {} {}",
+                        getTarget().get().toString(),
+                        defaultValueChanged(getTarget(), getDefaultTarget()));
+
+        getLogger()
+                .lifecycle(
+                        "runtime = {} {}",
+                        getRuntime().get().toString(),
+                        defaultValueChanged(getRuntime(), getDefaultRuntime()));
+
+        getLogger().lifecycle("Reason: {}", getReasoning().get());
+    }
+
+    private static String defaultValueChanged(
+            Property<ChosenJavaVersion> actualValue, Property<ChosenJavaVersion> defaultValue) {
+        return actualValue.get().equals(defaultValue.get())
+                ? "(default value)"
+                : String.format("(default value was %s - changed by a Gradle script or plugin)", defaultValue.get());
+    }
+}
