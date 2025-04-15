c194b61f37b9557e693a31c24c5ead75cafc5e96
aldexis <53899003+aldexis@users.noreply.github.com>
2025-04-15 15:08:22 +0200

    Move Slf4jLogsafeArgs to ERROR level (#3092)
    Move Slf4jLogsafeArgs to ERROR level

--- baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/Slf4jLogsafeArgs.java
+++ baseline-error-prone/src/main/java/com/palantir/baseline/errorprone/Slf4jLogsafeArgs.java
@@ -44,7 +44,7 @@ import java.util.regex.Pattern;
 @BugPattern(
         link = "https://github.com/palantir/gradle-baseline#baseline-error-prone-checks",
         linkType = BugPattern.LinkType.CUSTOM,
-        severity = SeverityLevel.WARNING,
+        severity = SeverityLevel.ERROR,
         summary = "Allow only com.palantir.logsafe.Arg types, or vararg arrays, as parameter inputs to slf4j log "
                 + "messages.")
 public final class Slf4jLogsafeArgs extends BugChecker implements MethodInvocationTreeMatcher {
