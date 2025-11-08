fe91c2e7aa576ab85f04873247494c1e224e9208
Callum Rogers <callumr@palantir.com>
2025-11-08 00:07:52 +0000

    paren

--- gradle-baseline-java/src/main/java/com/palantir/baseline/plugins/javaversions/BaselineJavaVersion.java
+++ gradle-baseline-java/src/main/java/com/palantir/baseline/plugins/javaversions/BaselineJavaVersion.java
@@ -158,7 +158,7 @@ public final class BaselineJavaVersion implements Plugin<Project> {
             // sourceCompatibility/targetCompatibility/getOptions().setRelease(...) do nothing for
             // Groovy (poor API sharing of JavaCompile's CompileOptions with GroovyCompile). In order
             // to output classfiles with bytecode version of $target, we have to use a $target JDK to
-            // run Groovy (in contrast to Java's use of `--release`.
+            // run Groovy (in contrast to Java's use of `--release`).
             groovyCompileTask
                     .getJavaLauncher()
                     .set(getJavaLauncher(
