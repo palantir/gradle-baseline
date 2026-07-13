e41e0d515644e351593421f54c75ef87d5261022
Callum Rogers <callumr@palantir.com>
2026-07-13 14:57:30 +0100

    Wrong configuration cache annotation

--- gradle-baseline-java/src/test/java/com/palantir/baseline/BaselineModuleJvmArgsIntegrationTest.java
+++ gradle-baseline-java/src/test/java/com/palantir/baseline/BaselineModuleJvmArgsIntegrationTest.java
@@ -22,7 +22,6 @@ import com.palantir.gradle.jdks.testing.WithJdkAutomanagement;
 import com.palantir.gradle.testing.execution.GradleInvoker;
 import com.palantir.gradle.testing.execution.InvocationResult;
 import com.palantir.gradle.testing.execution.TaskOutcome;
-import com.palantir.gradle.testing.junit.DisabledConfigurationCache;
 import com.palantir.gradle.testing.junit.GradlePluginTests;
 import com.palantir.gradle.testing.project.RootProject;
 import com.palantir.gradle.testing.project.SubProject;
@@ -41,7 +40,6 @@ import org.junit.jupiter.api.Test;
 
 @GradlePluginTests
 @WithJdkAutomanagement
-@DisabledConfigurationCache
 class BaselineModuleJvmArgsIntegrationTest {
 
     @BeforeEach
